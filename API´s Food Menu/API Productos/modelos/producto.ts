3f46450d76f35d1c11f39d7b28d9f92bf223d3b2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-26 18:21:44 -0700

    Creación de Clase Producto, ProductoAlimento - FMA API Productos
    Creación de la clase y atributos para la representación de producto y relación de producto alimento.

--- API´s Food Menu/API Productos/modelos/producto.ts
+++ API´s Food Menu/API Productos/modelos/producto.ts
@@ -1,3 +1,7 @@
-class Producto{
-    
+import {Categoria} from "../modelos/categoria"
+export class Producto{
+  private productoID: Number
+  private nombre: String;
+  private categoria: Categoria;
+  private productoUUID: String
 }
--- API´s Food Menu/API Productos/modelos/productoAlimento.ts
+++ API´s Food Menu/API Productos/modelos/productoAlimento.ts
@@ -1,3 +1,10 @@
-class ProductoAlimento{
-    
+import { Producto } from "../modelos/producto"
+import { Alimento } from "../modelos/alimento"
+class ProductoAlimento {
+    private productoAlimentoID: Number;
+    private productoAlimentoUUID: String;
+    private cantidadMinima: Number;
+    private cantidadMaxima: Number;
+    private producto: Producto;
+    private alimento: Alimento;
 }
