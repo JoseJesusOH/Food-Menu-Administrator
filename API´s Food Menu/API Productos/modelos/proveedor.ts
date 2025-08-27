96a743cafa06dc40a1f6207270baaeff0ec6a9e7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-26 18:22:38 -0700

    Creación de clase Proveedor, Sucursal - FMA API Productos
    Creación de la clase y atributos de la representación de proveedor y sucursal.

---
+++ API´s Food Menu/API Productos/modelos/proveedor.ts
@@ -0,0 +1,7 @@
+import {Sucursal} from "../modelos/sucursal"
+export class Proveedor{
+    private proveedorID: Number;
+    private proveedorUUID: Number;
+    private nombre: String;
+    private sucursal: Sucursal[];
+}
---
+++ API´s Food Menu/API Productos/modelos/sucursal.ts
@@ -0,0 +1,7 @@
+export class Sucursal{
+    private sucursalID: Number;
+    private sucursalUUID: String;
+    private nombre: String
+    private direccion: String
+    private telefono: String
+}
