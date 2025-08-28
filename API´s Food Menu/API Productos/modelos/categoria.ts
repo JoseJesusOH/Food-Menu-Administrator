53b7ac2da14ad0010866adbc867a26402fe57ac7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:21:42 -0700

    Clase Categoria - FMA API Productos
    Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/categoria.ts
+++ API´s Food Menu/API Productos/modelos/categoria.ts
@@ -1,5 +1,24 @@
-export class Categoria{
+export class Categoria {
     private categoriaID: Number;
     private nombre: String;
-    private categoriaUUID: Number;
+    private categoriaUUID: String;
+
+    getCategoriaID(): Number {
+        return this.categoriaID;
+    }
+    setCategoriaID(categoriaID: Number): void {
+        this.categoriaID = categoriaID;
+    }
+    getNombre(): String {
+        return this.nombre;
+    }
+    setNombre(nombre: String): void {
+        this.nombre = nombre;
+    }
+    getCategoriaUUID(): String {
+        return this.categoriaUUID;
+    }
+    setCategoriaUUID(categoriaUUID: String): void {
+        this.categoriaUUID = categoriaUUID;
+    }
 }
