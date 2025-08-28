7890fa3afc01f924f61c3a8d3e603878b391b8e2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:22:55 -0700

    Clase Proveedor - FMA API Productos
    Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/proveedor.ts
+++ API´s Food Menu/API Productos/modelos/proveedor.ts
@@ -1,7 +1,32 @@
 import {Sucursal} from "../modelos/sucursal"
 export class Proveedor{
     private proveedorID: Number;
-    private proveedorUUID: Number;
+    private proveedorUUID: String;
     private nombre: String;
-    private sucursal: Sucursal[];
+    private sucursales: Sucursal[];
+    getProveedorID():Number{
+        return this.proveedorID;
+    }
+    setProveedorID(proveedorID:Number):void{
+        this.proveedorID=proveedorID;
+    }
+    getProveedorUUID():String{
+        return this.proveedorUUID;
+    }
+    setProveedorUUID(proveedorUUID:String):void{
+        this.proveedorUUID=proveedorUUID;
+    }
+    getNombre():String{
+        return this.nombre;
+    }
+setNombre(nombre:String):void{
+    this.nombre=nombre;
+}
+setScurusales(sucursales:Sucursal[]):void{
+  this.sucursales=sucursales;
+}
+    getSucursales(){
+ return this.sucursales;
+    }
+
 }
