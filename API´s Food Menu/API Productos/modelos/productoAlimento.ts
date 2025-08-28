94826647cc28d1b9b7598af307b1d6f5b85f6ddb
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:22:34 -0700

    Clase ProductoAlimento- FMA API Productos
    Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/productoAlimento.ts
+++ API´s Food Menu/API Productos/modelos/productoAlimento.ts
@@ -1,10 +1,58 @@
 import { Producto } from "../modelos/producto"
 import { Alimento } from "../modelos/alimento"
-class ProductoAlimento {
+import { UnidadMedida } from "../modelos/unidadMedida"
+export class ProductoAlimento {
     private productoAlimentoID: Number;
     private productoAlimentoUUID: String;
     private cantidadMinima: Number;
     private cantidadMaxima: Number;
     private producto: Producto;
     private alimento: Alimento;
+    private unidadMedida: UnidadMedida
+    
+    getUuidadMedida():UnidadMedida{
+        return this.unidadMedida;
+    }
+ setUnidadMedida(unidadMedida:UnidadMedida):void{
+    this.unidadMedida=unidadMedida;
+ }
+
+    getProductoAlimentoID():Number{
+        return this.productoAlimentoID;
+    }
+    setProductoAlimentoID(productoAlimentoID:Number):void{
+        this.productoAlimentoID=productoAlimentoID;
+    }
+    getProductoAlimentoUUID():String{
+        return this.productoAlimentoUUID;
+    }
+    setProductoAlimentoUUID(productoAlimentoUUID:String):void{
+        this.productoAlimentoUUID=productoAlimentoUUID;
+    }
+    getCantidadMinima():Number{
+        return this.cantidadMinima;
+    }
+    setCantidadMinima(cantidadMinima:Number):void{
+        this.cantidadMinima=cantidadMinima;
+    }
+
+    getCantidadMaxima():Number{
+        return this.cantidadMaxima;
+    }
+    setCantidadMaxima(cantidadMaxima:Number):void{
+        this.cantidadMaxima=cantidadMaxima;
+    }
+    getProducto():Producto{
+        return this.producto;
+    }
+    setProducto(producto:Producto):void{
+        this.producto=producto;
+    }
+    getAlimento():Alimento{
+        return this.alimento;
+    }
+    setAlimento(alimento:Alimento):void{
+        this.alimento=alimento;
+    }
+    
 }
