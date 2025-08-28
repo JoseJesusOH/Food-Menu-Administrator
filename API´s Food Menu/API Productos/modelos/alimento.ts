7c570751050c951e3e470c10c4639445b85b5a79
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:19:04 -0700

    Clase Alimento - FMA API Productos
    Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/alimento.ts
+++ API´s Food Menu/API Productos/modelos/alimento.ts
@@ -1,7 +1,32 @@
-import {Categoria} from "../modelos/categoria"
-export class Alimento{
- private alimentoID: Number;
- private nombre:String;   
- private categoria: Categoria;
- private alumentoUUID: String;
+import { Categoria } from "../modelos/categoria"
+export class Alimento {
+    private alimentoID: Number;
+    private nombre: String;
+    private categoria: Categoria;
+    private alimentoUUID: String;
+    getAlimentoID(): Number {
+        return this.alimentoID;
+    }
+    setAlimentID(alimentoID: Number): void {
+        this.alimentoID = alimentoID;
+    }
+    getNombe(): String {
+        return this.nombre;
+    }
+    setNombre(nombre: String): void {
+        this.nombre = nombre;
+    }
+    getCategoria(): Categoria {
+        return this.categoria;
+    }
+    setCategoria(categoria:Categoria):void{
+        this.categoria=categoria;
+    }
+    getAlimentoUUID():String{
+        return this.alimentoUUID;
+    }
+    setAlimentoUUID(alimentoUUID:String):void{
+        this.alimentoUUID=alimentoUUID;
+    }
+
 }
