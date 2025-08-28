85dc9e8cc12629e0f4df175542096746420b8901
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:22:02 -0700

    Clase Productos- FMA API Productos
    Clase Productos- FMA API Productos Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/producto.ts
+++ API´s Food Menu/API Productos/modelos/producto.ts
@@ -4,4 +4,28 @@ export class Producto{
   private nombre: String;
   private categoria: Categoria;
   private productoUUID: String
+  getProductoID():Number{
+    return this.productoID;
+  }
+  setProductoID(productoID:Number):void{
+    this.productoID=productoID;
+  }
+  getNombre():String{
+    return this.nombre;
+  }
+  setNombre(nombre:String):void{
+    this.nombre=nombre;
+  }
+  getCategoria():Categoria{
+    return this.categoria;
+  }
+  setCategoria(categoria:Categoria):void{
+    this.categoria=categoria;
+  }
+  getProductoUUID():String{
+    return this.productoUUID;
+  }
+  setProductoUUID(productoUUID:String):void{
+    this.productoUUID=productoUUID;
+  }
 }
