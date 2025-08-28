7e1de1e75b8ee3e33f771ac45b07de5fab2044cf
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-27 18:23:26 -0700

    Clase Sucursal- FMA API Productos
    Actualización de la clase se agregan métodos get y set correspondientes.

--- API´s Food Menu/API Productos/modelos/sucursal.ts
+++ API´s Food Menu/API Productos/modelos/sucursal.ts
@@ -4,4 +4,34 @@ export class Sucursal{
     private nombre: String
     private direccion: String
     private telefono: String
+      getSucursalID(): Number{
+        return this.sucursalID;
+      }
+     getSucursalUUID(): String{
+      return this.sucursalUUID;
+     }
+     getNombre(): String{
+        return this.nombre;
+     }
+      getDireccion(): String{
+return this.direccion;
+      }
+      getTelefono(): String{
+        return this.telefono;
+      }
+      setSucursalID(sucursalID: Number): void{
+        this.sucursalID=sucursalID;
+      }
+     setSucursalUUID(sucursalUUID :String):void{
+       this.sucursalUUID=sucursalUUID;
+     }
+     setNombre(nombre: String):void{
+        this.nombre=nombre;
+     }
+     setDireccion(direccion: String):void{
+        this.direccion=direccion;
+     }
+      setTelefono(telefono: String):void{
+       this.telefono=telefono;
+      }
 }
