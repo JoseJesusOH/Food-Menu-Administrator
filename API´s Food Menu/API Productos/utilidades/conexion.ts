2c9fe07a9487be20a4ed785076f54f890b89a5ed
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-29 15:17:19 -0700

    Configuraciones Iniciales TypeORM - API Productos
    Creación de conexión y configuraciones iniciales para el uso de TypeORM, en la API Productos.

--- API´s Food Menu/API Productos/utilidades/conexion.ts
+++ API´s Food Menu/API Productos/utilidades/conexion.ts
@@ -1,3 +1,18 @@
-class Conexion{
-    
-}
+
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+export const Conexion = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "api_productos",  
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+})
+
