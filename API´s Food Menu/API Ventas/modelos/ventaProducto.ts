f83a7c75dff4b83d15514be73e018a253fc03985
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-30 15:19:21 -0700

    Creación de atributos modelos VentaUsuario, VentaProducto - FMA API Ventas
    Creación de los atributos de los modelos de VentaUsuario, VentaProducto de la API Venta.

---
+++ API´s Food Menu/API Ventas/modelos/ventaProducto.ts
@@ -0,0 +1,10 @@
+import {Venta} from "./venta"
+import {Producto} from "./producto"
+export class VentaProducto{
+    private ventaProductoID: Number;
+    private ventaProductoUUID: String;
+   private cantidad: Number;
+   private subtotal:Number;
+   private iva: Number;
+   private total: Number;    
+}
---
+++ API´s Food Menu/API Ventas/modelos/ventaUsuario.ts
@@ -0,0 +1,8 @@
+import { Venta } from "./venta"
+import { Usuario } from "./usuario"
+export class VentaUsuario {
+    private ventaUsuarioID: Number;
+    private ventaUsuarioUUID: String;
+    private venta: Venta;
+    private usuario: Usuario;
+}
