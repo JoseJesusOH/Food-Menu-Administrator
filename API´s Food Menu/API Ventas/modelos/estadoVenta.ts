ab7ee42bf1924aa6f500a3d92fbda7952ffc45e3
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-30 15:21:09 -0700

    Creación de los atributos de modelos Venta, Usuario - FMA API Ventas
    Creación de los atributos para los modelos de venta e usuario para la API Ventas.

---
+++ API´s Food Menu/API Ventas/modelos/estadoVenta.ts
@@ -0,0 +1,5 @@
+export enum EstadoVenta{
+    EN_PROCESo="En Proceso",
+    CANCELADAP="Cancelada",
+    VENDIDA="Vendida"
+}
---
+++ API´s Food Menu/API Ventas/modelos/venta.ts
@@ -0,0 +1,7 @@
+export class Venta{
+    private ventaID: Number;
+    private ventaUUID: String;
+    private fecha: Date;
+    private total: Number;
+    private hora: String;
+}
