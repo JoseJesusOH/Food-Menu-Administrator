34f08435951380c76eec4fc4bc987f34fba5f599
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-31 10:17:47 -0700

    Actualización interface productoIDAO - FMA API Ventas
    Actualización de la interfaz estableciendo los métodos a requerir en el acceso a datos correspondiente.

---
+++ API´s Food Menu/API Ventas/accesoDatos/daosInterfaces/productoIDAO.ts
@@ -0,0 +1,10 @@
+import {Producto} from "../../modelos/producto";
+
+export interface ProductoIDAO{
+  getProductos():Producto[];
+  getProductoByID(productoID:Number):Producto;
+  getProductoByUUID(productoUUID:String):Producto;
+  agregarProducto(producto:Producto):Boolean;
+  actualizarProducto(producto:Producto):Boolean;
+  eliminarProductoByID(productoID:Number):Boolean;
+}
