0fa342571ef23bd45d80c91365acaddbf421ca99
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-31 10:20:23 -0700

    Actualización  modelo Sucursal - FMA API Ventas
    Actualización del modelo sucursal de la API Ventas estableciendo los atributos requeridos.

---
+++ API´s Food Menu/API Menus/modelos/sucursal.ts
@@ -0,0 +1,8 @@
+export class Sucursal{
+    private sucursalID:Number;
+    private sucursalUUID:String;
+    private nombre:String;
+    private telefono: String;
+    private correo: String;
+    private slogan: String;
+}
