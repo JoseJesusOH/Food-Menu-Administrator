b6ce686d3751888c26973de7b53a84122cfc3e2a
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-31 10:17:21 -0700

    Actualización interface usuarioIDAO - FMA API Ventas
    Actualización de la interfaz estableciendo los métodos a requerir en el acceso a datos correspondiente.

---
+++ API´s Food Menu/API Ventas/accesoDatos/daosInterfaces/usuarioIDAO.ts
@@ -0,0 +1,9 @@
+import {Usuario} from "../../modelos/usuario";
+export interface UsuarioIDAO{
+    getUsuarios():Usuario[];
+    getUsuarioByID(usuarioID:Number):Usuario;
+    getUsuarioByUUID(usuarioUUID:String):Usuario;
+    agregarUsuario(usuario:Usuario):Boolean;
+    actualizarUsuario(usuario:Usuario):Boolean;
+    eliminarUsuarioByID(usuarioID:Number):Boolean;
+}
