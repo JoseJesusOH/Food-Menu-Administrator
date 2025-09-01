a3d882e149228e477c0d09587359e8d57b454a02
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-01 00:21:48 -0700

    Actualización Conexión - FMA API Productos
    Actualización de la conexón de la base de datos, colocando las entidades correspondientes.

--- API´s Food Menu/API Productos/utilidades/conexion.ts
+++ API´s Food Menu/API Productos/utilidades/conexion.ts
@@ -2,16 +2,24 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Categoria } from "../modelos/categoria";
-export  const Conexion = new DataSource({
+import { Alimento } from "../modelos/alimento";
+import { Producto } from "../modelos/producto";
+import { ProductoAlimento } from "../modelos/productoAlimento";
+import { ProductoProveedor } from "../modelos/productoProveedor";
+import { Proveedor } from "../modelos/proveedor";
+import { Sucursal } from "../modelos/sucursal";
+
+
+export const Conexion = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
     password: "root",
-    database: "api_productos",  
+    database: "api_productos",
     synchronize: true,
     logging: true,
-entities: [Categoria],
+    entities: [Categoria, Alimento, Producto, ProductoAlimento, ProductoProveedor, Proveedor, Sucursal],
     subscribers: [],
     migrations: [],
 })
