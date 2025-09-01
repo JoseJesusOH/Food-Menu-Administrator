ede52a24a2a71ee4d146d8cf43f5b3dfb5e691a1
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-01 00:26:20 -0700

    Actualización del modelo ProductoAlimento - FMA API Productos
    Actualización del modelo producto alimento, colocando los decoradores requeridos para el mapeo idoneo.

--- API´s Food Menu/API Productos/modelos/productoAlimento.ts
+++ API´s Food Menu/API Productos/modelos/productoAlimento.ts
@@ -1,58 +1,87 @@
 import { Producto } from "../modelos/producto"
 import { Alimento } from "../modelos/alimento"
 import { UnidadMedida } from "../modelos/unidadMedida"
+import { Categoria } from "./categoria"
+import "reflect-metadata"
+import {
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne,
+    JoinColumn,ManyToOne
+} from "typeorm"
+
+@Entity("producto_alimentos")
 export class ProductoAlimento {
+    @PrimaryGeneratedColumn()
     private productoAlimentoID: Number;
+    @Column()
+    @Generated("uuid")
     private productoAlimentoUUID: String;
+
+    @Column({
+        type: "double"
+    })
     private cantidadMinima: Number;
+
+    @Column({
+        type: "double"
+    })
     private cantidadMaxima: Number;
+
+      @ManyToOne(() => Producto, (Producto) => Producto.getProductoAlimentos)
     private producto: Producto;
+
+      @ManyToOne(() => Alimento, (Alimento) => Alimento.getProductoAlimentos)
     private alimento: Alimento;
+
+    @Column({
+        type: "enum",
+        enum: UnidadMedida,
+        default: UnidadMedida.SIN,
+    })
     private unidadMedida: UnidadMedida
-    
-    getUuidadMedida():UnidadMedida{
+
+    getUuidadMedida(): UnidadMedida {
         return this.unidadMedida;
     }
- setUnidadMedida(unidadMedida:UnidadMedida):void{
-    this.unidadMedida=unidadMedida;
- }
+    setUnidadMedida(unidadMedida: UnidadMedida): void {
+        this.unidadMedida = unidadMedida;
+    }
 
-    getProductoAlimentoID():Number{
+    getProductoAlimentoID(): Number {
         return this.productoAlimentoID;
     }
-    setProductoAlimentoID(productoAlimentoID:Number):void{
-        this.productoAlimentoID=productoAlimentoID;
+    setProductoAlimentoID(productoAlimentoID: Number): void {
+        this.productoAlimentoID = productoAlimentoID;
     }
-    getProductoAlimentoUUID():String{
+    getProductoAlimentoUUID(): String {
         return this.productoAlimentoUUID;
     }
-    setProductoAlimentoUUID(productoAlimentoUUID:String):void{
-        this.productoAlimentoUUID=productoAlimentoUUID;
+    setProductoAlimentoUUID(productoAlimentoUUID: String): void {
+        this.productoAlimentoUUID = productoAlimentoUUID;
     }
-    getCantidadMinima():Number{
+    getCantidadMinima(): Number {
         return this.cantidadMinima;
     }
-    setCantidadMinima(cantidadMinima:Number):void{
-        this.cantidadMinima=cantidadMinima;
+    setCantidadMinima(cantidadMinima: Number): void {
+        this.cantidadMinima = cantidadMinima;
     }
 
-    getCantidadMaxima():Number{
+    getCantidadMaxima(): Number {
         return this.cantidadMaxima;
     }
-    setCantidadMaxima(cantidadMaxima:Number):void{
-        this.cantidadMaxima=cantidadMaxima;
+    setCantidadMaxima(cantidadMaxima: Number): void {
+        this.cantidadMaxima = cantidadMaxima;
     }
-    getProducto():Producto{
+    getProducto(): Producto {
         return this.producto;
     }
-    setProducto(producto:Producto):void{
-        this.producto=producto;
+    setProducto(producto: Producto): void {
+        this.producto = producto;
     }
-    getAlimento():Alimento{
+    getAlimento(): Alimento {
         return this.alimento;
     }
-    setAlimento(alimento:Alimento):void{
-        this.alimento=alimento;
+    setAlimento(alimento: Alimento): void {
+        this.alimento = alimento;
     }
-    
+
 }
