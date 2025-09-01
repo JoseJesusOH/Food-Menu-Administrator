cb7829cdb6c9b2e8f8764ba547102f1d0b28cf39
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-31 22:22:39 -0700

    Actualización modelo sucursal - FMA APUI Productos
    Actualización del modelo de sucursal , se establecieron las variables y decoradores correspondientes para la relación entre modelos.

--- API´s Food Menu/API Productos/modelos/sucursal.ts
+++ API´s Food Menu/API Productos/modelos/sucursal.ts
@@ -1,37 +1,35 @@
-export class Sucursal{
-    private sucursalID: Number;
-    private sucursalUUID: String;
-    private nombre: String
-    private direccion: String
-    private telefono: String
-      getSucursalID(): Number{
-        return this.sucursalID;
-      }
-     getSucursalUUID(): String{
-      return this.sucursalUUID;
-     }
-     getNombre(): String{
-        return this.nombre;
-     }
-      getDireccion(): String{
-return this.direccion;
-      }
-      getTelefono(): String{
-        return this.telefono;
-      }
-      setSucursalID(sucursalID: Number): void{
-        this.sucursalID=sucursalID;
-      }
-     setSucursalUUID(sucursalUUID :String):void{
-       this.sucursalUUID=sucursalUUID;
-     }
-     setNombre(nombre: String):void{
-        this.nombre=nombre;
-     }
-     setDireccion(direccion: String):void{
-        this.direccion=direccion;
-     }
-      setTelefono(telefono: String):void{
-       this.telefono=telefono;
-      }
+import { Proveedor } from "../modelos/proveedor"
+import {
+  Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne,
+  JoinColumn, ManyToOne, OneToMany
+} from "typeorm"
+
+@Entity("sucursales")
+export class Sucursal {
+
+  @PrimaryGeneratedColumn()
+  private sucursalID: Number;
+
+  @Column()
+  @Generated("uuid")
+  private sucursalUUID: String;
+
+  @Column({
+    length: 100
+  })
+  private nombre: String
+
+  @Column({
+    length: 100
+  })
+  private direccion: String
+
+  @Column({
+    length: 100
+  })
+  private telefono: String
+
+  @ManyToOne(() => Proveedor, (Proveedor) => Proveedor.getSucursales)
+  private proveedor: Proveedor;
+
 }
