6e7b1cb1d6bed6786e02953f775784bd3fdc86d2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-01 01:05:00 -0700

    Actualización de modelo ProductoProveedor - FMA API Productos
    Actualización del modelo ProductoProveedor de la API Productos, estableciendo los nombres de las relaciones y columnas de la base de datos.

--- API´s Food Menu/API Productos/modelos/productoProveedor.ts
+++ API´s Food Menu/API Productos/modelos/productoProveedor.ts
@@ -8,17 +8,23 @@ import {
 @Entity("producto_proveedores")
 export class ProductoProveedor {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({
+        name:"producto_proveedor_id"
+    })
     private productoProveedorID: Number;
 
-    @Column()
+    @Column({
+        name:"producto_proveedor_uuid"
+    })
     @Generated("uuid")
     private productoProveedorUUID: String;
 
     @ManyToOne(() => Proveedor, (Proveedor) => Proveedor.getProductoProveedores)
+    @JoinColumn({ name: "proveedor_id", foreignKeyConstraintName: "producto_proveedor_proveedor_idfk" })
     private proveedor: Proveedor;
 
     @ManyToOne(() => Producto, (Producto) => Producto.getProductoProveedores)
+    @JoinColumn({ name: "producto_id", foreignKeyConstraintName: "producto_proveedor_producto_idfk" })
     private producto: Producto;
 
     setProveedor(proveedor: Proveedor): void {
