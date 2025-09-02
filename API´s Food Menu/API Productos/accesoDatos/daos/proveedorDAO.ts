625e1679799812611695585646e7e861bb8b4d0b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-01 20:59:38 -0700

    Actualización del acceso a datos ProveedorDAO - FMA API Productos
    Actualización del acceso a datos ProveedorDAO de la API Productos, se escribe el extends correspondiente a la interface e implementación vacía de los metodos.

--- API´s Food Menu/API Productos/accesoDatos/daos/proveedorDAO.ts
+++ API´s Food Menu/API Productos/accesoDatos/daos/proveedorDAO.ts
@@ -1,2 +1,23 @@
-class ProveedorDAO{
+import { Proveedor } from "../../modelos/proveedor";
+import { ProveedorIDAO } from "../daoInterfaces/proveedorIDAO";
+export class ProveedorDAO implements ProveedorIDAO{
+    getProveedores(): Proveedor[] {
+        throw new Error("Method not implemented.");
+    }
+    getProveedorByID(proveedorID: Number): Proveedor {
+        throw new Error("Method not implemented.");
+    }
+    getProveedorByUUID(proveedorUUID: String): Proveedor {
+        throw new Error("Method not implemented.");
+    }
+    agregarProveedor(proveedor: Proveedor): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    actualizarProveedor(proveedor: Proveedor): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProveedorByID(proveedorID: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+
 }
