ad1a9dfc68fd3256c833fd21bc01987fcec9e44d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-01 20:58:34 -0700

    Actualización del acceso a datos ProductoAlimentoDAO - FMA API Productos
    Actualización del acceso a datos ProductoAlimentoDAO de la API Productos, se escribe el extends correspondiente a la interface e implementación vacía de los metodos.

---
+++ API´s Food Menu/API Productos/accesoDatos/daos/productoAlimentoDAO.ts
@@ -0,0 +1,17 @@
+import { ProductoAlimento } from "../../modelos/productoAlimento";
+import { ProductoAlimentoIDAO } from "../daoInterfaces/productoAlimentoIDAO";
+export class ProductoAlimentoDAO implements ProductoAlimentoIDAO{
+    getProductoAlimentosByIDAlimento(alimentoID: Number): ProductoAlimento[] {
+        throw new Error("Method not implemented.");
+    }
+    agregarProductoAlimento(productoAlimento: ProductoAlimento): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    actualizarProductoAlimento(productoAlimento: ProductoAlimento): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProductoAlimentoByID(productoAlimentoID: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    
+}
