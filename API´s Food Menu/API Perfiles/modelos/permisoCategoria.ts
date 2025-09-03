9f73d425356e79ea2400bcb558a66cd643704904
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-02 17:24:39 -0700

    Actualización modelo PermisoCategoria - FMA API Perfiles
    Actualización del modelo PermisoCategoria de la API Perfiles, en el se escriben los atributos y relaciones establecidas.

---
+++ API´s Food Menu/API Perfiles/modelos/permisoCategoria.ts
@@ -0,0 +1,8 @@
+import {Categoria} from "./categoria"
+import {Permiso} from "./permiso"
+export class PermisoCategoria{
+    private permisoCategoriaID: Number;
+    private permisoCategoriaUUID:String;
+    private categoria:Categoria;
+    private permiso:Permiso;
+}
