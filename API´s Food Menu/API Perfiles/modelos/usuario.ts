dc66570e2c1fcb1ea7c3bd01152536cc57109c48
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-02 17:20:05 -0700

    Actualización modelo Usuario - FMA API Perfiles
    Actualización del modelo usuario colocando los atributos de usuario, y asociaciones.

---
+++ API´s Food Menu/API Perfiles/modelos/usuario.ts
@@ -0,0 +1,9 @@
+import {Persona} from "./persona"
+export class Usuario{
+    private usuarioID:Number;
+    private usuarioUUID: String;
+    private usuario: String;
+    private contraseña: String;
+    private correo: String;
+    private persona: Persona;
+}
