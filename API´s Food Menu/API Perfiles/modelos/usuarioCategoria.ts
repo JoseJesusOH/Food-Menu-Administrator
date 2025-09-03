a62b8eb2fe6835afa9d8e2cbec43a879a7724359
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-02 17:14:24 -0700

    Actualización modelo UsuarioCategoria - FMA API Perfiles
    Actualización del modelo de Usuario Categoria de la API Perfiles donde se crean los atributos idóneos planteados.

---
+++ API´s Food Menu/API Perfiles/modelos/usuarioCategoria.ts
@@ -0,0 +1,8 @@
+import {Categoria} from "./categoria"
+import {Usuario} from "./usuario"
+export class UsuarioCategoria{
+    private usuarioCategoriaID:Number;
+    private usuarioCategoriaUUID:String;
+    private usuario: Usuario;
+    private categoria: Categoria;
+}
