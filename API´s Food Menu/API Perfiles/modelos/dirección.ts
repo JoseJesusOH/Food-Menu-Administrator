f566d225b7d0603f895ac8f7f102e5e33c2dcf03
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-02 17:27:16 -0700

    Actualización modelo Dirección - FMA API Perfiles
    Actualización del modelo Dirección de la API Perfiles, en el se escriben los atributos y relaciones establecidas.

---
+++ API´s Food Menu/API Perfiles/modelos/dirección.ts
@@ -0,0 +1,9 @@
+import {Persona} from "./persona"
+export class Direccion{
+    private direccionID:Number;
+    private direccionUUID:String;
+    private calle:String;
+    private numero: String;
+    private asentamiento: String;
+    private persona:Persona;
+}
