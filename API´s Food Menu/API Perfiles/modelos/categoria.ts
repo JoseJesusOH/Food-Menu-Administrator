d07c6bcf5d851fe0e01d83124e76696e4079431b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-02 17:30:04 -0700

    Actualización modelo Categoría - FMA API Perfiles
    Actualización del modelo Categoría de la API Perfiles, se establecen las relaciones y atributos requeridos.

---
+++ API´s Food Menu/API Perfiles/modelos/categoria.ts
@@ -0,0 +1,8 @@
+import {PermisoCategoria} from "./permisoCategoria"
+
+export class Categoria{
+    private categoriaID:Number;
+    private categoriaUUID:String;
+    private nombre: String;
+    private permisoCategorias: PermisoCategoria[];
+}
