5a3e193f25f15cbb2edad5816b7db7bc69caf019
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:32:26 -0700

    Actualización modelo PermisoCategoria, métodos set y get - FMA API Perfiles
    Actualización del modelo PermisoCategoría de la API Perfiles , donde se crean los métodos set y get correspondientes.

--- API´s Food Menu/API Perfiles/modelos/permisoCategoria.ts
+++ API´s Food Menu/API Perfiles/modelos/permisoCategoria.ts
@@ -5,4 +5,29 @@ export class PermisoCategoria{
     private permisoCategoriaUUID:String;
     private categoria:Categoria;
     private permiso:Permiso;
+    setPermisoCategoriaID(permisoCategoriaID:Number):void{
+        this.permisoCategoriaID=permisoCategoriaID
+    }
+    getPermisoCategoriaID():Number{
+        return this.permisoCategoriaID
+    }
+    setPermisoCategoriaUUID(permisoCategoriaUUID:String):void{
+        this.permisoCategoriaUUID=permisoCategoriaUUID
+    }
+    getPermisoCategoriaUUID():String{
+        return this.permisoCategoriaUUID
+    }
+    setCategoria(categoria:Categoria):void{
+        this.categoria=categoria
+    }
+    getCategoria():Categoria{
+        return this.categoria
+    }
+    setPermiso(permiso:Permiso):void{
+        this.permiso=permiso
+    }
+    getPermiso():Permiso{
+        return this.permiso
+    }
+
 }
