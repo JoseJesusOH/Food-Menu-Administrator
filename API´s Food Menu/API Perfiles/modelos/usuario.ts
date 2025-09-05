666b8d35978533d0051e564d6cc93a37b072f1cd
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:54:19 -0700

    Actualización modelo Usuario decoradores - FMA API Perfiles
    Se realiza el primer avance de los decoradores planteados para los atributos del modelo Usuario de la API Perfiles, el siguiente avance contendrá las demás relaciones y decoradores con respecto a las relaciones 1 : N, 1:1, etc.

--- API´s Food Menu/API Perfiles/modelos/usuario.ts
+++ API´s Food Menu/API Perfiles/modelos/usuario.ts
@@ -1,46 +1,80 @@
-import {Persona} from "./persona"
-export class Usuario{
-    private usuarioID:Number;
+import { Persona } from "./persona"
+
+import "reflect-metadata"
+import {
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
+    JoinColumn,
+    ForeignKey
+} from "typeorm"
+
+@Entity("usuarios")
+export class Usuario {
+
+    @PrimaryGeneratedColumn({
+        name: "usuario_id"
+    })
+    private usuarioID: Number;
+
+    @Column({
+        name: "usuario_uuid"
+    })
+    @Generated("uuid")
     private usuarioUUID: String;
+
+    @Column({
+        name: "usuario",
+        length: 100
+    })
     private usuario: String;
+
+    @Column({
+        name: "password",
+        length: 100
+    })
     private password: String;
+
+    @Column({
+        name: "correo",
+        length: 100
+    })
     private correo: String;
+
     private persona: Persona;
 
-    setUsuarioID(usuarioID:Number):void{
-        this.usuarioID=usuarioID
+    setUsuarioID(usuarioID: Number): void {
+        this.usuarioID = usuarioID
     }
-    getUsuarioID():Number{
+    getUsuarioID(): Number {
         return this.usuarioID
     }
-    setUsuarioUUID(usuarioUUID:String):void{
-        this.usuarioUUID=usuarioUUID
+    setUsuarioUUID(usuarioUUID: String): void {
+        this.usuarioUUID = usuarioUUID
     }
-    getUsuarioUUID():String{
+    getUsuarioUUID(): String {
         return this.usuarioUUID
     }
-    setUsuario(usuario:String):void{
-        this.usuario=usuario
+    setUsuario(usuario: String): void {
+        this.usuario = usuario
     }
-    getUsuario():String{
+    getUsuario(): String {
         return this.usuario
     }
-    setPassword(password:String):void{
-        this.password=password
+    setPassword(password: String): void {
+        this.password = password
     }
-    getPassword():String{
+    getPassword(): String {
         return this.password
     }
-    setCorreo(correo:String):void{
-        this.correo=correo
+    setCorreo(correo: String): void {
+        this.correo = correo
     }
-    getCorreo():String{
+    getCorreo(): String {
         return this.correo
     }
-    setPersona(persona:Persona):void{
-        this.persona=persona
+    setPersona(persona: Persona): void {
+        this.persona = persona
     }
-    getPersona():Persona{
+    getPersona(): Persona {
         return this.persona
     }
 }
