b29e9aeb78f3bada8f56f7c42eb5c35a6c9e84d4
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:31:29 -0700

    Actualización modelo Direccion , métodos set y get - FMA API Perfiles
     Actualización del modelo Dirección de la API Perfiles , donde se crean los métodos set y get correspondientes.

--- API´s Food Menu/API Perfiles/modelos/dirección.ts
+++ API´s Food Menu/API Perfiles/modelos/dirección.ts
@@ -6,4 +6,40 @@ export class Direccion{
     private numero: String;
     private asentamiento: String;
     private persona:Persona;
+    setDireccionID(direccionID:Number):void{
+        this.direccionID=direccionID
+    }
+    getDireccionID():Number{
+        return this.direccionID
+    }
+    setDireccionUUID(direccionUUID:String):void{
+        this.direccionUUID=direccionUUID
+    }
+    getDireccionUUID():String{
+        return this.direccionUUID
+    }
+    setCalle(calle:String):void{
+        this.calle=calle
+    }
+    getCalle():String{
+        return this.calle
+    }
+    setNumero(numero:String):void{
+        this.numero=numero
+    }
+    getNumero():String{
+        return this.numero
+    }
+    setAsentamiento(asentamiento:String):void{
+        this.asentamiento=asentamiento
+    }
+    getAsentamiento():String{
+        return this.asentamiento
+    }
+    setPersona(persona:Persona):void{
+        this.persona=persona
+    }
+    getPersona():Persona{
+        return this.persona
+    }
 }
