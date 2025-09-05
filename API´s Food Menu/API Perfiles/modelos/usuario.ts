c6c50ee213d5293ab414050eae1249d27b692c92
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:33:29 -0700

    Actualización modelo Usuario, métodos set y get - FMA API Perfiles
    Actualización del modelo Usuario de la API Perfiles , donde se crean los métodos set y get correspondientes.

--- API´s Food Menu/API Perfiles/modelos/usuario.ts
+++ API´s Food Menu/API Perfiles/modelos/usuario.ts
@@ -3,7 +3,44 @@ export class Usuario{
     private usuarioID:Number;
     private usuarioUUID: String;
     private usuario: String;
-    private contraseña: String;
+    private password: String;
     private correo: String;
     private persona: Persona;
+
+    setUsuarioID(usuarioID:Number):void{
+        this.usuarioID=usuarioID
+    }
+    getUsuarioID():Number{
+        return this.usuarioID
+    }
+    setUsuarioUUID(usuarioUUID:String):void{
+        this.usuarioUUID=usuarioUUID
+    }
+    getUsuarioUUID():String{
+        return this.usuarioUUID
+    }
+    setUsuario(usuario:String):void{
+        this.usuario=usuario
+    }
+    getUsuario():String{
+        return this.usuario
+    }
+    setPassword(password:String):void{
+        this.password=password
+    }
+    getPassword():String{
+        return this.password
+    }
+    setCorreo(correo:String):void{
+        this.correo=correo
+    }
+    getCorreo():String{
+        return this.correo
+    }
+    setPersona(persona:Persona):void{
+        this.persona=persona
+    }
+    getPersona():Persona{
+        return this.persona
+    }
 }
