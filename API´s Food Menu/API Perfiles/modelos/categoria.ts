58d778c2023d440b85990b7c148b02042e57c6ba
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:30:50 -0700

    Actualización modelo Categoria , métodos set y get - FMA API Perfiles
    Actualización del modelo Categoría de la API Perfiles , donde se crean los métodos set y get correspondientes.

--- API´s Food Menu/API Perfiles/modelos/categoria.ts
+++ API´s Food Menu/API Perfiles/modelos/categoria.ts
@@ -5,4 +5,31 @@ export class Categoria{
     private categoriaUUID:String;
     private nombre: String;
     private permisoCategorias: PermisoCategoria[];
+    
+    setCategoriaID(categoriaID: Number):void{
+        this.categoriaID=categoriaID
+    }
+    
+    getCategoriaID():Number{
+        return this.categoriaID
+    }
+
+    setCategoriaUUID(categoriaUUID:String):void{
+       this.categoriaUUID=categoriaUUID
+    }
+    getCategoriaUUID():String{
+     return this.categoriaUUID
+    }
+    setNombre(nombre:String):void{
+        this.nombre=nombre
+    }
+    getNombre():String{
+        return this.nombre
+    }
+    setPermisoCategorias(permisoCategorias:PermisoCategoria[]):void{
+        this.permisoCategorias=permisoCategorias
+    }
+    getPermisoCategorias():PermisoCategoria[]{
+        return this.permisoCategorias
+    }
 }
