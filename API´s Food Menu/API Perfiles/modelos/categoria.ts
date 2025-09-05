f505eea501b5383bbee257a8bebd71512df8dfe2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:54:49 -0700

    Actualización modelo Categoria decoradores - FMA API Perfiles
    Se realiza el primer avance de los decoradores planteados para los atributos del modelo Categoria de la API Perfiles, el siguiente avance contendrá las demás relaciones y decoradores con respecto a las relaciones 1 : N, 1:1, etc.

--- API´s Food Menu/API Perfiles/modelos/categoria.ts
+++ API´s Food Menu/API Perfiles/modelos/categoria.ts
@@ -1,35 +1,57 @@
-import {PermisoCategoria} from "./permisoCategoria"
+import { PermisoCategoria } from "./permisoCategoria"
+import "reflect-metadata"
+import {
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
+    JoinColumn,
+    ForeignKey
+} from "typeorm"
 
-export class Categoria{
-    private categoriaID:Number;
-    private categoriaUUID:String;
+@Entity("categorias")
+export class Categoria {
+
+    @PrimaryGeneratedColumn({
+        name: "categoria_id"
+    })
+    private categoriaID: Number;
+
+    @Column({
+        name: "categoria_uuid"
+    })
+    @Generated("uuid")
+    private categoriaUUID: String;
+
+    @Column({
+        name: "nombre",
+        length: 100
+    })
     private nombre: String;
+
     private permisoCategorias: PermisoCategoria[];
-    
-    setCategoriaID(categoriaID: Number):void{
-        this.categoriaID=categoriaID
+
+    setCategoriaID(categoriaID: Number): void {
+        this.categoriaID = categoriaID
     }
-    
-    getCategoriaID():Number{
+
+    getCategoriaID(): Number {
         return this.categoriaID
     }
 
-    setCategoriaUUID(categoriaUUID:String):void{
-       this.categoriaUUID=categoriaUUID
+    setCategoriaUUID(categoriaUUID: String): void {
+        this.categoriaUUID = categoriaUUID
     }
-    getCategoriaUUID():String{
-     return this.categoriaUUID
+    getCategoriaUUID(): String {
+        return this.categoriaUUID
     }
-    setNombre(nombre:String):void{
-        this.nombre=nombre
+    setNombre(nombre: String): void {
+        this.nombre = nombre
     }
-    getNombre():String{
+    getNombre(): String {
         return this.nombre
     }
-    setPermisoCategorias(permisoCategorias:PermisoCategoria[]):void{
-        this.permisoCategorias=permisoCategorias
+    setPermisoCategorias(permisoCategorias: PermisoCategoria[]): void {
+        this.permisoCategorias = permisoCategorias
     }
-    getPermisoCategorias():PermisoCategoria[]{
+    getPermisoCategorias(): PermisoCategoria[] {
         return this.permisoCategorias
     }
 }
