87805ad2da0b3ddf60ad04fab95800c10ef75dbd
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-04 21:56:22 -0700

    Actualización modelo UsuarioCategoria decoradores - FMA API Perfiles
    Se realiza el primer avance de los decoradores planteados para los atributos del modelo UsuarioCategoria de la API Perfiles, el siguiente avance contendrá las demás relaciones y decoradores con respecto a las relaciones 1 : N, 1:1, etc.

--- API´s Food Menu/API Perfiles/modelos/usuarioCategoria.ts
+++ API´s Food Menu/API Perfiles/modelos/usuarioCategoria.ts
@@ -1,32 +1,51 @@
-import {Categoria} from "./categoria"
-import {Usuario} from "./usuario"
-export class UsuarioCategoria{
-    private usuarioCategoriaID:Number;
-    private usuarioCategoriaUUID:String;
+import { Categoria } from "./categoria"
+import { Usuario } from "./usuario"
+import "reflect-metadata"
+import {
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
+    JoinColumn,
+    ForeignKey
+} from "typeorm"
+
+@Entity("usuario_categorias")
+export class UsuarioCategoria {
+
+    @PrimaryGeneratedColumn({
+        name: "usuario_categoria_id"
+    })
+    private usuarioCategoriaID: Number;
+
+    @Column({
+        name: "usuario_categoria_uuid"
+    })
+    @Generated("uuid")
+    private usuarioCategoriaUUID: String;
+
+
     private usuario: Usuario;
     private categoria: Categoria;
-    setUsuarioCategoriaID(usuarioCategoriaID:Number):void{
-        this.usuarioCategoriaID=usuarioCategoriaID
+    setUsuarioCategoriaID(usuarioCategoriaID: Number): void {
+        this.usuarioCategoriaID = usuarioCategoriaID
     }
-    getUsuarioCategoriaID():Number{
+    getUsuarioCategoriaID(): Number {
         return this.usuarioCategoriaID
     }
-    setUsuarioCategoriaUUID(usuarioCategoriaUUID:String):void{
-        this.usuarioCategoriaUUID=usuarioCategoriaUUID
+    setUsuarioCategoriaUUID(usuarioCategoriaUUID: String): void {
+        this.usuarioCategoriaUUID = usuarioCategoriaUUID
     }
-    getUsuarioCategoriaUUID():String{
+    getUsuarioCategoriaUUID(): String {
         return this.usuarioCategoriaUUID
     }
-    setUsuario(usuario:Usuario):void{
-        this.usuario=usuario
+    setUsuario(usuario: Usuario): void {
+        this.usuario = usuario
     }
-    getUsuario():Usuario{
+    getUsuario(): Usuario {
         return this.usuario
     }
-    setCategoria(categoria:Categoria):void{
-        this.categoria=categoria
+    setCategoria(categoria: Categoria): void {
+        this.categoria = categoria
     }
-    getCategoria():Categoria{
+    getCategoria(): Categoria {
         return this.categoria
     }
 }
