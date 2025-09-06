6ba56a83f9b68069de65041cd05b4c5c7129a17d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-05 20:31:30 -0700

    Actualización conexion - FMA API Perfiles
    Actualización de la conexión de la API Perfiles, colocando las entidades correspondientes.

--- API´s Food Menu/API Perfiles/utilidades/conexion.ts
+++ API´s Food Menu/API Perfiles/utilidades/conexion.ts
@@ -1,6 +1,15 @@
 
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import {Categoria} from "../modelos/categoria"
+import {Direccion} from "../modelos/direccion"
+import {Permiso} from "../modelos/permiso"
+import {PermisoCategoria} from "../modelos/permisoCategoria"
+import {Persona} from "../modelos/persona"
+import {Usuario} from "../modelos/usuario"
+import {UsuarioCategoria} from "../modelos/usuarioCategoria"
+
+
 export const Conexion = new DataSource({
     type: "mysql",
     host: "localhost",
@@ -10,7 +19,7 @@ export const Conexion = new DataSource({
     database: "api_perfiles",
     synchronize: true,
     logging: true,
-    entities: [],
+    entities: [Categoria,Direccion,Permiso,PermisoCategoria,Persona,Usuario, UsuarioCategoria],
     subscribers: [],
     migrations: [],
 })
