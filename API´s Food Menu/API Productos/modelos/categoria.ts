791332b69990bc039fdd215a7377fd3d8ed2534b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-07 01:52:23 -0700

    Actualización modelo Categoría - FMA API Productos
    Actualización del modelo categoría de la API Productos, se establecen los comentarios y actualización de variables para respetar estándares.

--- API´s Food Menu/API Productos/modelos/categoria.ts
+++ API´s Food Menu/API Productos/modelos/categoria.ts
@@ -1,42 +1,80 @@
+/**
+ * Importaciones TypeORM
+ */
 import "reflect-metadata"
-import { Entity, Column, PrimaryGeneratedColumn, Generated,FindManyOptions } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn, Generated } from "typeorm"
 
+/**
+ * Entidad que representa una categoria en la base de datos.
+ */
 @Entity("categorias")
 export class Categoria {
 
+    /** Identificador autoincremental de la categoria en la base de datos */
     @PrimaryGeneratedColumn({
-        name:"categoria_id"
+        name: "categoria_id"
     })
-    private categoriaID: Number;
+    private categoriaId: Number;
 
+    /** Nombre de la categoria */
     @Column({
         length: 100,
-        name:"nombre"
+        name: "nombre"
     })
     private nombre: String;
 
+    /** Identificador único UUID de la categoria */
     @Column({
-        name:"categoria_uuid"
+        name: "categoria_uuid"
     })
     @Generated("uuid")
-    private categoriaUUID: String;
- 
-    getCategoriaID(): Number {
-        return this.categoriaID;
+    private categoriaUuid: String;
+
+    /**
+     * Devuelve el identificador interno de la categoria.
+     * @returns ID de la categoria
+     */
+    getCategoriaId(): Number {
+        return this.categoriaId;
     }
-    setCategoriaID(categoriaID: Number): void {
-        this.categoriaID = categoriaID;
+
+    /**
+     * Asigna el identificador interno de la categoria.
+     * @param alimentoId ID de la categoria
+     */
+    setCategoriaId(categoriaId: Number): void {
+        this.categoriaId = categoriaId;
     }
+
+    /**
+     * Devuelve el nombre de la categoria.
+     * @returns nombre Nombre de la categoria
+     */
     getNombre(): String {
         return this.nombre;
     }
+
+    /**
+     * Asigna el nombre a la categoria.
+     * @param nombre Nombre de la categoria
+     */
     setNombre(nombre: String): void {
         this.nombre = nombre;
     }
-    getCategoriaUUID(): String {
-        return this.categoriaUUID;
+
+    /**
+     * Devuelve el UUID de la categoria.
+     * @returns categoriaUuid UUID de la categoria
+     */
+    getCategoriaUuid(): String {
+        return this.categoriaUuid;
     }
-    setCategoriaUUID(categoriaUUID: String): void {
-        this.categoriaUUID = categoriaUUID;
+
+    /**
+     * Asigna el UUID a la categoria.
+     * @param alimentoUuid UUID de la categoria
+     */
+    setCategoriaUuid(categoriaUuid: String): void {
+        this.categoriaUuid = categoriaUuid;
     }
 }
