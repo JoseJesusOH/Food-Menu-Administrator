60e1e1dd5dd338902233e038b61b31a063c1c832
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-08 22:46:28 -0700

    Actualización de acceso a datos CategoriaDAO - FMA API Productos
    Actualización del acceso a datos correspondiente donde se validad nombres de métodos, parámetros, y estándares cases.

---
+++ API´s Food Menu/API Productos/accesoDatos/daos/categoria.dao.impl.ts
@@ -0,0 +1,22 @@
+import { Categoria } from "../../modelos/categoria";
+import { CategoriaIDAO } from "../daoInterfaces/categoria.dao";
+export class CategoriaDAO implements CategoriaIDAO{
+    getCategorias(): Categoria[] {
+        throw new Error("Method not implemented.");
+    }
+    getCategoriaById(categoriaID: Number): Categoria {
+        throw new Error("Method not implemented.");
+    }
+    getCategoriaByUuid(categoriaUuid: String): Categoria {
+        throw new Error("Method not implemented.");
+    }
+    agregarCategoria(categoria: Categoria): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCategoriaById(categoriaId: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    actualizarCategoria(categoria: Categoria): Boolean {
+        throw new Error("Method not implemented.");
+    }
+}
