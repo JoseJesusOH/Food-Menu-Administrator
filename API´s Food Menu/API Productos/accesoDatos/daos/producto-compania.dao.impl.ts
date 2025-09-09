2c6bf0b0990b80c67780b224974e034453b94072
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-08 22:48:12 -0700

    Actualización de acceso a datos ProductoCompania- FMA API Productos
    Actualización del acceso a datos correspondiente donde se validad nombres de métodos, parámetros, y estándares cases.

---
+++ API´s Food Menu/API Productos/accesoDatos/daos/producto-compania.dao.impl.ts
@@ -0,0 +1,17 @@
+import { ProductoCompania } from "../../modelos/producto-compania";
+import { ProductoCompaniaIDAO } from "../daoInterfaces/producto-compania.dao";
+export class ProductoCompaniaDAO implements ProductoCompaniaIDAO{
+    getProductosCompaniasByIdAlimento(productoCompaniaId: Number): ProductoCompania[] {
+        throw new Error("Method not implemented.");
+    }
+    agregarProductoCompania(productoCompania: ProductoCompania): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    actualizarProductoCompania(productoCompania: ProductoCompania): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProductoCompaniaById(productoCompaniaId: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    
+}
