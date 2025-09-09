82f51e433225a73c37df915d8187866931658056
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-08 22:47:08 -0700

    Actualización de acceso a datos CompaniaDAO- FMA API Productos
    Actualización del acceso a datos correspondiente donde se validad nombres de métodos, parámetros, y estándares cases.

---
+++ API´s Food Menu/API Productos/accesoDatos/daos/compania.dao.impl.ts
@@ -0,0 +1,24 @@
+import { Compania } from "../../modelos/compania";
+import { CompaniaIDAO } from "../daoInterfaces/compania.dao";
+
+export class CompaniaDAO implements CompaniaIDAO{
+    getCompanias(): Compania[] {
+        throw new Error("Method not implemented.");
+    }
+    getCompaniaById(companiaID: Number): Compania {
+        throw new Error("Method not implemented.");
+    }
+    getCompaniaByUuid(companiaUuid: String): Compania {
+        throw new Error("Method not implemented.");
+    }
+    agregarCompania(compania: Compania): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCompaniaById(companiaId: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    actualizarCompania(compania: Compania): Boolean {
+        throw new Error("Method not implemented.");
+    }
+	
+}
