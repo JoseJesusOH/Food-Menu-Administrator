608aae4cdca784bc6e4188a8e7e22202f252f786
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-08 22:44:08 -0700

    Actualización interface ProductoDAO- FMA API Productos
    Actualización de interface, con el fin de validar nombres de metodos, parametros, y estándares cases.

---
+++ API´s Food Menu/API Productos/accesoDatos/daoInterfaces/producto.dao.ts
@@ -0,0 +1,10 @@
+import {Producto} from "../../modelos/producto"
+
+export interface ProductoIDAO{
+    getProductos():Producto[];
+    getProductoById(productoId:Number):Producto;
+    getProductoByUuid(productoUuid:String):Producto;
+    agregarProducto(producto:Producto):Boolean;
+    actualizarProducto(producto:Producto):Boolean;
+    eliminarProductoById(productoId:Number):Boolean;
+}
