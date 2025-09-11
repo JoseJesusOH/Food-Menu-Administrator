36b4df741e3e5c1e062a3198e0c82295b3e16010
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-11 15:52:26 -0700

    Actualización de import de la interface AlimentoDAO - FMA API Productos
    Se actualiza los import correspondientes de la interface de acceso a datos de la API Productos.

---
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/alimento.dao.ts
@@ -0,0 +1,10 @@
+import { Alimento } from "@entity/alimento.entity"
+
+export interface AlimentoIDAO {
+    getAlimentos(): Alimento[];
+    getAlimentoById(alimentoId: Number): Alimento;
+    getAlimentByUuid(alimentoUuid: String): Alimento;
+    eliminarAlimentoById(alimentoId: Number): Boolean;
+    actualizarAlimento(alimento: Alimento): boolean;
+    agregarAlimento(alimento: Alimento): Boolean;
+}
