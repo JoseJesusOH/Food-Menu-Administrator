a736a6a4b9865e532c7b4a3a713c4e4acdd7318d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 18:05:58 -0700

    Actualización Interface Acceso a Datos CompaniaDAO - FMA API Productos
    Se agregan comentarios respectivo de la interface, comentarios de los métodos agregar, eliminar, actualizar, búsqueda , etc.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/compania.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/compania.dao.ts
@@ -1,9 +1,46 @@
+/**
+ * Modelo Compania requerido para la interfaz
+ */
 import { Compania } from "@entity/compania.entity"
+/**
+ * Interface de Acceso a datos para el modelo Compania
+ */
 export interface CompaniaIDAO {
+  /**
+   * Obtiene todas las compañias
+   * @returns Arreglo con todas las compañias
+   */
   getCompanias(): Compania[];
+  /**
+   * Obtiene una compañia por su ID
+   * @param companiaID ID de la compañia a buscar
+   * @return Compania encontrada
+   */
   getCompaniaById(companiaID: Number): Compania;
+  /**
+   * Obtiene una compañia por su UUID
+   * @param companiaUuid UUID de la compañia a buscar
+   * @return Compania encontrada
+   */
   getCompaniaByUuid(companiaUuid: String): Compania;
+  
+  /**
+   * Agrega una nueva compañia a la base de datos
+   * @param compania Compañia a agregar
+   * @returns Boolean indicando si se agrego o no la compañia 
+   */
   agregarCompania(compania: Compania): Boolean;
+  
+  /**
+   * Elimina una compañia de la base de datos
+   * @param companiaId ID de la compañia a eliminar
+   * @returns Boolean indicando si se elimino o no la compañia
+   */
   eliminarCompaniaById(companiaId: Number): Boolean;
+  /**
+   * Actualiza una compañia en la base de datos
+   * @param compania Compañia a actualizar
+   * @returns Boolean indicando si se actualizo o no la compañia
+   */
   actualizarCompania(compania: Compania): Boolean;
 } 
