7bb53c5335d348ced21695ecbe59d2eda16c6de3
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 22:05:55 -0700

    Actualización Implementación Acceso a Datos CategoriaDAO - FMA API Productos
    Se crean los comentarios respectivos para la implementación de los métodos respectivos de cada interface.

--- API´s Food Menu/API Productos/src/data-acces/implementations/categoria.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/categoria.dao.impl.ts
@@ -1,22 +1,62 @@
-// Update the import path to the correct module that exports 'Categoria'
+/**
+ * Implementacion de los metodos de la interfaz CategoriaIDAO
+ */
 import { Categoria } from "@entity/categoria.entity";
 import { CategoriaIDAO } from "@data.dao/categoria.dao";
-export class CategoriaDAO implements CategoriaIDAO{
+/**
+ * Implementacion de los metodos de la interfaz CategoriaIDAO
+ */
+export class CategoriaDAO implements CategoriaIDAO {
+    /**
+     * Metodo para obtener todas las categorias
+     */
     getCategorias(): Categoria[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para obtener una categoria por su ID
+     */
     getCategoriaById(categoriaID: Number): Categoria {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo para obtener una categoria por su UUID
+     */
     getCategoriaByUuid(categoriaUuid: String): Categoria {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo para obtener una categoria por su nombre
+     */
+    getCategoriaByNombre(categoriaNombre: String): Categoria {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Metodo para agregar una nueva categoria
+     */
     agregarCategoria(categoria: Categoria): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para eliminar una categoria por su ID
+     */
     eliminarCategoriaById(categoriaId: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para eliminar una categoria por su UUID
+     */
+    eliminarCategoriaByUuid(categoriaUuid: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    /**
+     * Metodo para actualizar una categoria
+     * 
+     */
     actualizarCategoria(categoria: Categoria): Boolean {
         throw new Error("Method not implemented.");
     }
