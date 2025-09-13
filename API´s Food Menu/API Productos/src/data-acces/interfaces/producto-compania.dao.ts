a6c0c275d923c5d83a338886fbf1695b53de7ab0
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 18:06:24 -0700

    Actualización Interface Acceso a Datos ProductoCompaniaDAO- FMA API Productos
    Se agregan comentarios respectivo de la interface, comentarios de los métodos agregar, eliminar, actualizar, búsqueda , etc.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/producto-compania.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/producto-compania.dao.ts
@@ -1,7 +1,44 @@
-import {ProductoCompania} from "@entity/producto-compania.entity"
-export interface ProductoCompaniaIDAO{
-    getProductosCompaniasByIdAlimento(productoCompaniaId:Number):ProductoCompania[];
-    agregarProductoCompania(productoCompania:ProductoCompania):Boolean;
-    actualizarProductoCompania(productoCompania:ProductoCompania):Boolean;
-    eliminarProductoCompaniaById(productoCompaniaId:Number):Boolean;
+/**
+ * Interface para el DAO de ProductoCompania
+ */
+import { ProductoCompania } from "@entity/producto-compania.entity"
+/**
+ * Interfaz que define los métodos para el acceso a datos de ProductoCompania
+ */
+export interface ProductoCompaniaIDAO {
+
+    /**
+     * Obtiene todos los productos compañias de la base de datos por el ID del producto compañia
+     * @param productoCompaniaId ID del producto compañia
+     * @returns Lista de productos compañias
+     */
+    getProductosCompaniasById(productoCompaniaId: Number): ProductoCompania[];
+
+    /**
+     * Obtiene todos los productos compañias de la base de datos por el ID de la compañia
+     * @param companiaId ID de la compañia
+     * @returns Lista de productos compañias
+     */
+    getProductosCompaniasByIdCompania(companiaId: Number): ProductoCompania[];
+
+    /**
+     * Agrega un nuevo producto compañia a la base de datos
+     * @param productoCompania ProductoCompania a agregar
+     * @returns Boolean indicando si se agrego o no el producto compañia
+     */
+    agregarProductoCompania(productoCompania: ProductoCompania): Boolean;
+
+    /**
+     * Actualiza un producto compañia en la base de datos
+     * @param productoCompania ProductoCompania a actualizar
+     * @returns Boolean indicando si se actualizo o no el producto compañia
+     */
+    actualizarProductoCompania(productoCompania: ProductoCompania): Boolean;
+
+    /**
+     * Elimina un producto compañia de la base de datos
+     * @param productoCompaniaId ID del producto compañia a eliminar
+     * @returns Boolean indicando si se elimino o no el producto compañia
+     */
+    eliminarProductoCompaniaById(productoCompaniaId: Number): Boolean;
 }
