53423e1a05066a21aa03947a71cf6e2f5b42515d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 22:07:53 -0700

    Actualización Implementación Acceso a Datos ProductoCompaniaDAO - FMA API Productos
    Se crean los comentarios respectivos para la implementación de los métodos respectivos de cada interface.

--- API´s Food Menu/API Productos/src/data-acces/implementations/producto-compania.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/producto-compania.dao.impl.ts
@@ -1,17 +1,55 @@
+/**
+ * Importaciones requeridas para la implementación del DAO de ProductoCompania.
+ */
 import { ProductoCompania } from "@entity/producto-compania.entity";
 import { ProductoCompaniaIDAO } from "@data.dao/producto-compania.dao";
-export class ProductoCompaniaDAO implements ProductoCompaniaIDAO{
-    getProductosCompaniasByIdAlimento(productoCompaniaId: Number): ProductoCompania[] {
+/**
+ * Implementación del DAO de ProductoCompania.
+ */
+export class ProductoCompaniaDAO implements ProductoCompaniaIDAO {
+    /**
+     * Metodo que obtiene los ProductoCompania por su ID de producto compañia.
+     */
+    getProductosCompaniasById(productoCompaniaId: Number): ProductoCompania[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que obtiene los ProductoCompania por su UUID de producto compañia.
+     */
+    getProductosCompaniasByIdCompania(companiaId: Number): ProductoCompania[] {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+    * Metodo que obtiene los ProductoCompania por su UUID de producto compañia.
+    */
+    getProductosCompaniasByUuidCompania(companiaUuid: String): ProductoCompania[] {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Metodo que agrega un ProductoCompania.
+     */
+
     agregarProductoCompania(productoCompania: ProductoCompania): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que actualiza un ProductoCompania.
+     */
+
     actualizarProductoCompania(productoCompania: ProductoCompania): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /** 
+     * Metodo que elimina un ProductoCompania por su ID.
+     */
     eliminarProductoCompaniaById(productoCompaniaId: Number): Boolean {
         throw new Error("Method not implemented.");
     }
-    
+
+
 }
