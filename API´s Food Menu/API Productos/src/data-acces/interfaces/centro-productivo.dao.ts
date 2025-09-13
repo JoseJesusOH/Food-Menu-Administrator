21740cf2e3f96024c4d7b61fcc3f61fb1b799717
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 18:07:07 -0700

    Actualización Interface Acceso a Datos CentroProductivoDAO - FMA API Productos
    Se agregan comentarios respectivo de la interface, comentarios de los métodos agregar, eliminar, actualizar, búsqueda , etc.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/centro-productivo.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/centro-productivo.dao.ts
@@ -1,9 +1,46 @@
-import {CentroProductivo} from "@entity/centro-productivo.entity"
-export interface CentroProductivoIDAO{
-    getCentrosProductivos():CentroProductivo[];
-    getCentroProductivoById(centroProductivoId:Number):CentroProductivo;
-    getCentroProductivoByUuid(centroProductivoUuid:String):CentroProductivo;
-    agregarCentroProductivo(centroProductivo:CentroProductivo):Boolean;
-    actualizarCentroProductivo(centroProductivo:CentroProductivo):Boolean;
-    eliminarCentroProductivoById(centroProductivoId:Number):Boolean;
+/**
+ * Interfaz para el acceso a datos de los centros productivos
+ * 
+ */
+import { CentroProductivo } from "@entity/centro-productivo.entity"
+/**
+ * Interface de Acceso a datos para el modelo Centro Productivo
+ */
+export interface CentroProductivoIDAO {
+    /**
+     * Obtiene todos los centros productivos
+     * @returns Arreglo con todos los centros productivos
+     */
+    getCentrosProductivos(): CentroProductivo[];
+    /**
+     * Obtiene un centro productivo por su ID
+     * @param centroProductivoId ID del centro productivo a buscar
+     * @return CentroProductivo encontrado
+     */
+    getCentroProductivoById(centroProductivoId: Number): CentroProductivo;
+    /**
+     * Obtiene un centro productivo por su UUID
+     * @param centroProductivoUuid UUID del centro productivo a buscar
+     * @returns CentroProductivo encontrado
+     */
+    getCentroProductivoByUuid(centroProductivoUuid: String): CentroProductivo;
+   
+    /**
+     * Agrega un nuevo centro productivo a la base de datos
+     * @param centroProductivo Centro productivo a agregar
+     * @returns Boolean indicando si se agrego o no el centro productivo
+     */
+    agregarCentroProductivo(centroProductivo: CentroProductivo): Boolean;
+    /**
+     *  Actualiza un centro productivo en la base de datos
+     * @param centroProductivo Centro productivo a actualizar
+     * @returns Boolean indicando si se actualizo o no el centro productivo
+     */
+    actualizarCentroProductivo(centroProductivo: CentroProductivo): Boolean;
+    /**
+     * Elimina un centro productivo de la base de datos
+     * @param centroProductivoId ID del centro productivo a eliminar
+     * @returns Boolean indicando si se elimino o no el centro productivo   
+     */
+    eliminarCentroProductivoById(centroProductivoId: Number): Boolean;
 }
