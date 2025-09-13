61c7fd2191f51127983fc4dc76dd81cf5a305959
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 22:07:11 -0700

    Actualización Implementación Acceso a Datos CompaniaDAO - FMA API Productos
    Se crean los comentarios respectivos para la implementación de los métodos respectivos de cada interface.

--- API´s Food Menu/API Productos/src/data-acces/implementations/compania.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/compania.dao.impl.ts
@@ -1,24 +1,59 @@
+/**
+ * Importaciones requeridas para la implementación del DAO de Compañía.
+ */
 import { Compania } from "@entity/compania.entity";
 import { CompaniaIDAO } from "@data.dao/compania.dao";
 
-export class CompaniaDAO implements CompaniaIDAO{
+/**
+ * Implementación del DAO de Compañía.
+ */
+export class CompaniaDAO implements CompaniaIDAO {
+    /**
+     * Metodo que obtiene todas las compañias.
+     */
     getCompanias(): Compania[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que obtiene una compañia por su ID.
+     */
     getCompaniaById(companiaID: Number): Compania {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que obtiene una compañia por su UUID.
+     *  
+     */
     getCompaniaByUuid(companiaUuid: String): Compania {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que agrega una nueva compañia.
+     */
     agregarCompania(compania: Compania): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que elimina una compañia por su ID.
+     */
     eliminarCompaniaById(companiaId: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo que elimina una compañia por su UUID.
+     */
+    eliminarCompaniaByUuid(companiaUuid: String): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    /**
+     * Metodo que actualiza una compañia.
+     */
     actualizarCompania(compania: Compania): Boolean {
         throw new Error("Method not implemented.");
     }
-	
+
 }
