4e800fa83e8f288abd0a11a92efe14c76165e99f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 22:08:07 -0700

    Actualización Implementación Acceso a Datos ProductoDAO - FMA API Productos
    Se crean los comentarios respectivos para la implementación de los métodos respectivos de cada interface.

--- API´s Food Menu/API Productos/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/producto.dao.impl.ts
@@ -1,23 +1,62 @@
+/**
+ * Importaciones requeridas para la implementación del DAO de Producto.
+ */
 import { Producto } from "@entity/producto.entity";
 import { ProductoIDAO } from "@data.dao/producto.dao";
+
+/**
+ * Implementación del DAO de Producto.
+ */
 export class ProductoDAO implements ProductoIDAO{
+     
+    /**
+     * Metodo para obtener todos los productos.
+     */ 
     getProductos(): Producto[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para obtener un producto por su ID.
+     */
     getProductoById(productoId: Number): Producto {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para obtener un producto por su nombre.
+     */
+    getProductoByNombre(productoNombre: String): Producto {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * Metodo para obtener un producto por su UUID.
+     */
     getProductoByUuid(productoUuid: String): Producto {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para agregar un producto.
+     */
     agregarProducto(producto: Producto): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para actualizar un producto.
+     */
     actualizarProducto(producto: Producto): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para eliminar un producto por su ID.
+     */
     eliminarProductoById(productoId: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+    
    
 }
