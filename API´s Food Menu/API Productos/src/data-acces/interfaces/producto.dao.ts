7a6ad739ff63e1ac4aa1635f7b968b0c6f9b963d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 18:07:23 -0700

    Actualización Interface Acceso a Datos ProductoDAO- FMA API Productos
    Se agregan comentarios respectivo de la interface, comentarios de los métodos agregar, eliminar, actualizar, búsqueda , etc.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/producto.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/producto.dao.ts
@@ -1,10 +1,51 @@
 import {Producto} from "@entity/producto.entity"
 
 export interface ProductoIDAO{
+    /**
+     * Obtiene todos los productos
+     * @returns Lista de productos
+     */
     getProductos():Producto[];
+    
+    /**
+     * Obtiene un producto por su ID
+     * @param productoId ID del producto a buscar
+     * @returns Producto con el ID especificado
+     */
     getProductoById(productoId:Number):Producto;
+
+    /**
+     *  Obtiene un producto por su nombre
+     * @param productoNombre Nombre del producto a buscar
+     * @returns Producto con el nombre especificado
+     */
+    getProductoByNombre(productoNombre:String):Producto;
+
+    /**
+     * Obtiene un producto por su UUID
+     * @param productoUuid UUID del producto a buscar
+     * @returns Producto con el UUID especificado
+     */
     getProductoByUuid(productoUuid:String):Producto;
+   
+    /**
+     * Agrega un nuevo producto a la base de datos
+     * @param producto Producto a agregar
+     * @returns Boolean indicando si se agrego o no el producto
+     */
     agregarProducto(producto:Producto):Boolean;
+    
+    /**
+     * Actualiza un producto en la base de datos
+     * @param producto Producto a actualizar
+     * @returns Boolean indicando si se actualizo o no el producto
+     */
     actualizarProducto(producto:Producto):Boolean;
+
+    /**
+     * Elimina un producto de la base de datos
+     * @param productoId ID del producto a eliminar
+     * @returns Boolean indicando si se elimino o no el producto
+     */
     eliminarProductoById(productoId:Number):Boolean;
 }
