2b06c62dab74602fd13c9ce50e5fd33502d87ea8
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-13 10:56:42 -0700

    Eliminación de archivos Acceso a Datos - FMA API Perfiles
    Se eliminan los archivos correspondientes al acceso a datos de la API Perfiles, para dar paso al uso de PATHS.

--- API´s Food Menu/API Perfiles/accesoDatos/daosInterfaces/usuarioCategoriaIDAO.ts
+++
@@ -1,8 +0,0 @@
-import {UsuarioCategoria} from "../../modelos/usuarioCategoria"
-export interface UsuarioCategoriaIDAO{
-    getUsuarioCategorias():UsuarioCategoria[];
-    getUsuarioCategoriaByID(usuarioCategoriaID:Number):UsuarioCategoria;
-    agregarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Boolean;
-    eliminarUsuarioCategoriaByID(usuarioCategoria:Number):Boolean;
-    actualizarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Boolean;
-}
