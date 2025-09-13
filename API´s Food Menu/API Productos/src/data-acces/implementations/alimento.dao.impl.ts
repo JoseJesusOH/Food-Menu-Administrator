86d97b7c07b246a1aab8104d03bd44b816446539
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 22:03:40 -0700

    Actualización Implementación AlimentoDAO - FMA API Productos
    Se crean los comentarios respectivos para la implementación de los métodos respectivos de cada interface.

--- API´s Food Menu/API Productos/src/data-acces/implementations/alimento.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/alimento.dao.impl.ts
@@ -1,23 +1,50 @@
+/**
+ * Importaciones de entidades y interfaces necesarias Alimento y AlimentoIDAO
+ */
 import { Alimento } from "@entity/alimento.entity";
 import { AlimentoIDAO } from "@data.dao/alimento.dao";
 export class AlimentoDAO implements AlimentoIDAO{
+    /**
+     * Metodo que retorna un arreglo de alimentos
+     */
     getAlimentos(): Alimento[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que retorna un alimento por su ID
+     */
     getAlimentoById(alimentoId: Number): Alimento {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que retorna un alimento por su UUID
+     */
     getAlimentByUuid(alimentoUuid: String): Alimento {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que elimina un alimento por su ID
+     */
     eliminarAlimentoById(alimentoId: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que actualiza un alimento
+     */
     actualizarAlimento(alimento: Alimento): boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo que agrega un alimento
+     */
     agregarAlimento(alimento: Alimento): Boolean {
         throw new Error("Method not implemented.");
     }
+   
    
 }
