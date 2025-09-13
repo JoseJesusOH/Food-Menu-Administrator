276c4b607ccb700484a80cdb95c646caa1bbca93
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-12 17:07:34 -0700

    Actualización Interface Acceso a Datos CategoriaDAO - FMA API Productos
    Se agregan comentarios respectivo de la interface, comentarios de los métodos agregar, eliminar, actualizar, búsqueda , etc.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/categoria.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/categoria.dao.ts
@@ -1,9 +1,44 @@
+/**
+ * Import correspondiente de la Entidad Categoria requerido para la interfaz CategoriaIDAO.
+ */
 import { Categoria } from "@entity/categoria.entity"
+/**
+ * Interface de Acceso a datos para el modelo Categoria
+ */
 export interface CategoriaIDAO {
+  /**
+   * @returns Arreglo con todas las categorias
+   */
   getCategorias(): Categoria[];
+  /**
+   * Devuelve la categoria encontrada con respecto al ID especificado.
+   * @param categoriaID ID de la categoria a buscar
+   * @returns Categoria Obtenida
+   */
   getCategoriaById(categoriaID: Number): Categoria;
+  /**
+   * Devuelve la categoria encontrada con respecto al UUID proporcionado
+   * @param categoriaUuid UUID de la categoria a buscar
+   * @returns Categoria encontrada
+   */
   getCategoriaByUuid(categoriaUuid: String): Categoria;
+
+  /**
+   *  Agrega una nueva categoria a la base de datos
+   * @param categoria Categoria a agregar
+   * @returns Boolean indicando si se agrego o no la categoria
+   */
   agregarCategoria(categoria: Categoria): Boolean;
+  /**
+   * Elimina una categoria de la base de datos
+   * @param categoriaId ID de la categoria a eliminar
+   * @returns Boolean indicando si se elimino o no la categoria
+   */
   eliminarCategoriaById(categoriaId: Number): Boolean;
+  /**
+   * Actualiza una categoria existente en la base de datos
+   * @param categoria Categoria a actualizar
+   * @returns Boolean indicando si se actualizo o no la categoria
+   */
   actualizarCategoria(categoria: Categoria): Boolean;
 } 
