c88a3f35fcddebd3ffd2421018e843f7e299b0a9
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-15 01:35:23 -0700

    Actualización interface PermisoCategoriaDAO- FMA API Perfiles
    Se actualiza la interface con los comentarios respectivos de cada metodo.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso-categoria.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso-categoria.dao.ts
@@ -1,8 +1,47 @@
-import {PermisoCategoria} from "@entity/permiso-categoria.entity"
+/**
+ * Importacion de entidad PermisoCategoria
+ */
+
+import { PermisoCategoria } from "@entity/permiso-categoria.entity"
+
+/**
+ * Interfaz que define las operaciones de acceso a datos para la entidad PermisoCategoria
+ */
 export interface PermisoCategoriaIDAO {
-    getPermisoCategorias():PermisoCategoria[];
-    getPermisoCategoriaByID(permisoCategoriaID:Number):PermisoCategoria;
-    agregarPermisoCategoria(permisoCategoria: PermisoCategoria):Boolean;
-    eliminarPermisoCategoriaByID(permisoCategoriaID:Number):Boolean;
-    actualizarPermisoCategoria(permisoCategoria:PermisoCategoria): Boolean
+    /**
+     * Método para obtener todos los PermisoCategorias
+     * @return Un arreglo de objetos PermisoCategoria
+     */
+    getPermisoCategorias(): PermisoCategoria[];
+
+    /**
+     * Método para obtener un PermisoCategoria por su ID
+     * @param permisoCategoriaID ID del PermisoCategoria a buscar
+     * @return Un objeto PermisoCategoria si se encuentra, de lo contrario retorna null
+     */
+    getPermisoCategoriaByID(permisoCategoriaID: Number): PermisoCategoria;
+
+    /** 
+     * 
+     * Método para agregar un nuevo PermisoCategoria
+     * @param permisoCategoria Objeto PermisoCategoria a agregar
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    agregarPermisoCategoria(permisoCategoria: PermisoCategoria): Boolean;
+
+    /** 
+     * 
+     * Método para eliminar un PermisoCategoria por su ID
+     * @param permisoCategoriaID ID del PermisoCategoria a eliminar
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    eliminarPermisoCategoriaByID(permisoCategoriaID: Number): Boolean;
+
+    /** 
+     * 
+     * Método para actualizar un PermisoCategoria existente
+     * @param permisoCategoria Objeto PermisoCategoria con los datos actualizados
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    actualizarPermisoCategoria(permisoCategoria: PermisoCategoria): Boolean
 }
