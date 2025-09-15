9aa618e666b7e660d3e45988f5d0cf9b90dcc203
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-14 19:50:57 -0700

    Actualización modelo Usuario - FMA API Perfiles
    Se crean los comentarios respectivos del modelo  además de actualizar nombres de variables, para cumplir estándares case.

--- API´s Food Menu/API Perfiles/src/entities/usuario-categoria.ts
+++ API´s Food Menu/API Perfiles/src/entities/usuario-categoria.ts
@@ -1,54 +1,112 @@
-import { Categoria } from "./categoria.entity"
-import { Usuario } from "./usuario.entity"
+
+/** 
+ * Importaciones TypeORM
+ */
 import "reflect-metadata"
 import {
-    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
-    JoinColumn,
-    ForeignKey
+    Entity, Column, PrimaryGeneratedColumn, Generated,
+     ManyToOne , JoinColumn
 } from "typeorm"
 
+/**
+ * Importaciones de Entidades Usuario y Categoria para las relaciones
+ */
+import { Categoria } from "./categoria.entity"
+import { Usuario } from "./usuario.entity"
+
+/**
+ * Entidad UsuarioCategoria que representa la tabla usuario_categorias en la base de datos
+ */
 @Entity("usuario_categorias")
 export class UsuarioCategoria {
 
+    /**
+     * Identificador único de UsuarioCategoria  
+     */
     @PrimaryGeneratedColumn({
         name: "usuario_categoria_id"
     })
-    private usuarioCategoriaID: Number;
+    private usuarioCategoriaId: Number;
 
+    /**
+     * UUID de UsuarioCategoria
+     */
     @Column({
         name: "usuario_categoria_uuid"
     })
     @Generated("uuid")
-    private usuarioCategoriaUUID: String;
+    private usuarioCategoriaUuid: String;
 
-
-    private usuario: Usuario;
+    /**
+     * Relación muchos a uno con la entidad Usuario
+     */
+          @ManyToOne(() => Categoria, (Categoria) => Categoria.getPermisoCategorias)
+        @JoinColumn({ name: "usuario_id", foreignKeyConstraintName: "usuario_categoria_usuario_idfk" })
+   private usuario: Usuario;
     
+    /**
+     * Relación muchos a uno con la entidad Categoria
+     */
+         @ManyToOne(() => Categoria, (Categoria) => Categoria.getPermisoCategorias)
+        @JoinColumn({ name: "categoria_id", foreignKeyConstraintName: "usuario_categoria_categoria_idfk" })
     private categoria: Categoria;
 
+    /**
+     *  Asigna el id de UsuarioCategoria
+     * @param usuarioCategoriaId Id de UsuarioCategoria
+     */
+    setUsuarioCategoriaId(usuarioCategoriaId: Number): void {
+        this.usuarioCategoriaId = usuarioCategoriaId
+    }
+    /**
+     *  Devuelve el id de UsuarioCategoria
+     * @returns Id de UsuarioCategoria
+     */
+    getUsuarioCategoriaId(): Number {
+        return this.usuarioCategoriaId
+    }
+    /**
+     *  Asigna el UUID de UsuarioCategoria
+     * @param usuarioCategoriaUuid UUID de UsuarioCategoria
+     */
+    setUsuarioCategoriaUuid(usuarioCategoriaUuid: String): void {
+        this.usuarioCategoriaUuid = usuarioCategoriaUuid
+    }
+    /**
+     *  Devuelve el UUID de UsuarioCategoria
+     * @returns UUID de UsuarioCategoria
+     */
+    getUsuarioCategoriaUuid(): String {
+        return this.usuarioCategoriaUuid
+    }
 
-
-    setUsuarioCategoriaID(usuarioCategoriaID: Number): void {
-        this.usuarioCategoriaID = usuarioCategoriaID
-    }
-    getUsuarioCategoriaID(): Number {
-        return this.usuarioCategoriaID
-    }
-    setUsuarioCategoriaUUID(usuarioCategoriaUUID: String): void {
-        this.usuarioCategoriaUUID = usuarioCategoriaUUID
-    }
-    getUsuarioCategoriaUUID(): String {
-        return this.usuarioCategoriaUUID
-    }
+    /** 
+     * Asigna el usuario asociado a la categoría
+     * @param usuario El usuario asociado a la categoría
+     */ 
     setUsuario(usuario: Usuario): void {
         this.usuario = usuario
     }
+
+    /**
+     *  Devuelve el usuario asociado a la categoría
+     * @returns El usuario asociado a la categoría
+     */
     getUsuario(): Usuario {
         return this.usuario
     }
+
+     /**
+      *  Asigna la categoría asociada al usuario
+      * @param categoria La categoría asociada al usuario
+      */
     setCategoria(categoria: Categoria): void {
         this.categoria = categoria
     }
+    /**
+     *  Devuelve la categoría asociada al usuario
+     * @returns  La categoría asociada al usuario
+      */
     getCategoria(): Categoria {
         return this.categoria
     }
