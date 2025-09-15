4f6ace92fb099ff11d702345fabd9eee40f749e8
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-14 19:48:27 -0700

    Actualización modelo Usuario  - FMA API Perfiles
    Se actualiza el modelo Usuario de la API Perfiles se colocan los comentarios con respecto a los atributos y metodos.

--- API´s Food Menu/API Perfiles/src/entities/usuario.entity.ts
+++ API´s Food Menu/API Perfiles/src/entities/usuario.entity.ts
@@ -1,81 +1,167 @@
-import { Persona } from "./persona.entity"
-
+/**
+ * Importaciones TypeORM
+ */
 import "reflect-metadata"
 import {
-    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne,
     JoinColumn,
-    ForeignKey
 } from "typeorm"
 
+/**
+ * Importacion de Entidad Persona requeridas para la relacion
+ */
+import { Persona } from "./persona.entity"
+
+/**
+ * Entidad Usuario que mapea la tabla usuarios de la base de datos
+ */
 @Entity("usuarios")
 export class Usuario {
 
+    /**
+     * ID del usuario
+     */
     @PrimaryGeneratedColumn({
         name: "usuario_id"
     })
-    private usuarioID: Number;
+    private usuarioId: Number;
 
+    /**
+     * UUID del usuario
+     */
     @Column({
         name: "usuario_uuid"
     })
     @Generated("uuid")
-    private usuarioUUID: String;
+    private usuarioUuid: String;
 
+    /**
+     * Nombre del usuario
+     */
     @Column({
         name: "usuario",
         length: 100
     })
     private usuario: String;
 
+    /** 
+     * Contraseña del usuario
+     */
     @Column({
         name: "password",
         length: 100
     })
     private password: String;
 
+    /**
+     * Correo del usuario   
+     */
     @Column({
         name: "correo",
         length: 100
     })
     private correo: String;
 
-      @OneToOne(() => Persona)
-    @JoinColumn({name:"persona_id" ,foreignKeyConstraintName:"usuario_persona_IDFK"})
+    /**
+     * Relacion uno a uno con la entidad Persona
+     */
+    @OneToOne(() => Persona)
+    @JoinColumn({ name: "persona_id", foreignKeyConstraintName: "usuario_persona_IDFK" })
     private persona: Persona;
 
-    setUsuarioID(usuarioID: Number): void {
-        this.usuarioID = usuarioID
+
+    /**
+     *  Asigna un ID al usuario
+     * @param usuarioId ID a asignar al usuario
+     */
+    setUsuarioId(usuarioId: Number): void {
+        this.usuarioId = usuarioId
     }
-    getUsuarioID(): Number {
-        return this.usuarioID
+
+    /**
+     *  Obtiene el ID del usuario
+     * @returns ID del usuario
+     */
+    getUsuarioId(): Number {
+        return this.usuarioId
     }
-    setUsuarioUUID(usuarioUUID: String): void {
-        this.usuarioUUID = usuarioUUID
+
+    /**
+     * Asigna un UUID al usuario
+     * @param usuarioUuid UUID a asignar al usuario
+     */
+    setUsuarioUuid(usuarioUuid: String): void {
+        this.usuarioUuid = usuarioUuid
     }
-    getUsuarioUUID(): String {
-        return this.usuarioUUID
+
+    /**
+     *  Obtiene el UUID del usuario
+     * @returns UUID del usuario
+     */
+    getUsuarioUuid(): String {
+        return this.usuarioUuid
     }
+
+    /**
+     * Asigna un nombre al usuario
+     * @param usuario Nombre a asignar al usuario
+     */
     setUsuario(usuario: String): void {
         this.usuario = usuario
     }
+
+    /**
+     *  Obtiene el nombre del usuario
+     * @returns Nombre del usuario
+     */
     getUsuario(): String {
         return this.usuario
     }
+
+    /**
+     * Asigna una contraseña al usuario
+     * @param password Contraseña a asignar al usuario
+     */
     setPassword(password: String): void {
         this.password = password
     }
+
+    /**
+     *  Obtiene la contraseña del usuario
+     * @returns Contraseña del usuario
+     */
     getPassword(): String {
         return this.password
     }
+
+    /**
+     *  Asigna un correo al usuario
+     * @param correo Correo a asignar al usuario
+     */
     setCorreo(correo: String): void {
         this.correo = correo
     }
+
+    /**
+     * Obtiene el correo del usuario
+     * @returns Correo del usuario
+     */
     getCorreo(): String {
         return this.correo
     }
+
+    /**
+     * Asigna una persona al usuario
+     * @param persona Persona a asignar al usuario
+     */
     setPersona(persona: Persona): void {
         this.persona = persona
     }
+
+    /**
+     *  Obtiene la persona relacionada al usuario
+     * @returns Persona relacionada al usuario
+     */
     getPersona(): Persona {
         return this.persona
     }
