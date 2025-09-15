dede137a7d76b5d2dd70579b84aed56a521c3e3f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-15 01:35:08 -0700

    Actualización interface DireccionDAO - FMA API Perfiles
    Se actualiza la interface con los comentarios respectivos de cada metodo.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/direccion.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/direccion.dao.ts
@@ -1,8 +1,44 @@
-import {Direccion} from "@entity/direccion.entity"
+/**
+ * Importacion de la entidad Direccion
+ */
+
+import { Direccion } from "@entity/direccion.entity"
+/**
+ * Interfaz que define las operaciones de acceso a datos para la entidad Direccion
+ */
 export interface DireccionIDAO {
-    getDirecciones():Direccion[];
-    getDireccionByID(direccionID:Number):Direccion;
-    agregarDireccion(direccion:Direccion):Boolean;
-    eliminarDireccion(direccionID:Number):Boolean;
-    actualizarDireccion(direccion:Direccion):Boolean;
+
+    /** 
+     * Método para obtener todas las direcciones
+     * @return Un arreglo de objetos Direccion
+     */
+    getDirecciones(): Direccion[];
+
+    /** 
+     * Método para obtener una direccion por su ID
+     * @param direccionID ID de la direccion a buscar
+     * @return Un objeto Direccion si se encuentra, de lo contrario retorna null
+     */
+    getDireccionByID(direccionID: Number): Direccion;
+
+    /** 
+     * Método para agregar una nueva direccion
+     * @param direccion Objeto Direccion a agregar
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    agregarDireccion(direccion: Direccion): Boolean;
+
+    /** 
+     * Método para eliminar una direccion por su ID
+     * @param direccionID ID de la direccion a eliminar
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    eliminarDireccion(direccionID: Number): Boolean;
+
+    /** 
+     * Método para actualizar una direccion existente
+     * @param direccion Objeto Direccion con los datos actualizados
+     * @return true si la operación fue exitosa, de lo contrario false
+     */
+    actualizarDireccion(direccion: Direccion): Boolean;
 }
