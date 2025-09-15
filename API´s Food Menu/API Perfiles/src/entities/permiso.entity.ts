b10e48f2694794713c2d31fe1be325435f7ff45a
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-14 19:50:22 -0700

    Actualización modelo Permiso - FMA API Perfiles
    Se crean los comentarios respectivos del modelo  además de actualizar nombres de variables, para cumplir estándares case.

--- API´s Food Menu/API Perfiles/src/entities/permiso.entity.ts
+++ API´s Food Menu/API Perfiles/src/entities/permiso.entity.ts
@@ -1,56 +1,114 @@
-import { PermisoCategoria } from "./permiso-categoria.entity"
+
+/**
+ * Importaciones TypeORM
+ */
 import "reflect-metadata"
 import {
-    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
-    JoinColumn,
-    ForeignKey
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToMany,
 } from "typeorm"
 
+/**
+ * Importacione de Entidad PermisoCategoria
+ */
+import { PermisoCategoria } from "./permiso-categoria.entity"
+
+/**
+ * Entidad Permiso que representa la tabla permisos en la base de datos
+ */
 @Entity("permisos")
 export class Permiso {
 
+    /**
+     * Identificador único del permiso
+     */
     @PrimaryGeneratedColumn({
         name: "permiso_id"
     })
-    private permisoID: Number;
+    private permisoId: Number;
 
 
+    /**
+     * UUID del permiso
+     */
     @Column({
         name: "permiso_uuid"
     })
     @Generated("uuid")
-    private permisoUUID: String;
+    private permisoUuid: String;
 
+    /**
+     * Nombre del permiso
+     */
     @Column({
         name: "nombre",
         length: 100
     })
     private nombre: String;
 
+    /**
+     * Relación uno a muchos con la entidad PermisoCategoria
+     */
     @OneToMany(() => PermisoCategoria, (PermisoCategoria) => PermisoCategoria.getPermiso)
     private permisoCategorias: PermisoCategoria[];
 
-    setPermisoID(permisoID: Number): void {
-        this.permisoID = permisoID
+    /**
+     * Asigna el id del permiso
+     * @param permisoID Identificador único del permiso
+     */
+    setPermisoId(permisoId: Number): void {
+        this.permisoId = permisoId
     }
-    getPermisoID(): Number {
-        return this.permisoID
+
+    /**
+     * Obtiene el id del permiso
+     * @returns El id del permiso
+     */
+    getPermisoId(): Number {
+        return this.permisoId
     }
-    setPermisoUUID(permisoUUID: String): void {
-        this.permisoUUID = permisoUUID
+
+    /** 
+     * Asigna el UUID del permiso
+     * @param permisoUuid UUID del permiso
+     */
+    setPermisoUuid(permisoUuid: String): void {
+        this.permisoUuid = permisoUuid
     }
-    getPermisoUUID(): String {
-        return this.permisoUUID
+
+    /**
+     * Devuelve el UUID del permiso
+     * @returns El UUID del permiso
+     */
+    getPermisoUuid(): String {
+        return this.permisoUuid
     }
+ 
+    /**
+     *  Asigna el nombre del permiso
+     * @param nombre Nombre del permiso
+     */
     setNombre(nombre: String): void {
         this.nombre = nombre
     }
+    /**
+     * Devuelve el nombre del permiso
+     * @returns El nombre del permiso
+     */
     getNombre(): String {
         return this.nombre
     }
+
+    /**
+     * Asigna las categorías de permiso asociadas al permiso
+     * @param permisoCategorias Categorías de permiso asociadas al permiso
+     */
     setPermisoCategorias(permisoCategorias: PermisoCategoria[]): void {
         this.permisoCategorias = permisoCategorias
     }
+    /**
+     * Devuelve las categorías de permiso asociadas al permiso
+     * @returns Las categorías de permiso asociadas al permiso
+     */
     getPermisoCategorias(): PermisoCategoria[] {
         return this.permisoCategorias
     }
