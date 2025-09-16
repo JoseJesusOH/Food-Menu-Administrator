30446bb4a2a2ee59e8756d9561d81c69b1a660fb
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-16 15:37:45 -0700

    Actualización Implementación DAO UsuarioCategoria - FMA API Perfiles
    Se realiza la actualización de los comentarios respectivos de la implementación para cada método e importación.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario-categoria.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario-categoria.dao.impl.ts
@@ -1,18 +1,43 @@
+
+/**
+ * Importaciones de los modelos respectivos e interface UsuarioCategoria e UsuarioCategoriaIDAO
+ */
 import { UsuarioCategoria } from "@entity/usuario-categoria.entity";
 import {UsuarioCategoriaIDAO} from "@data.dao/usuario-categoria.dao"
+
+/**
+ * Implementación de UsuarioCateforiaDAO 
+ */
 export class UsuarioCategoriaDAO implements UsuarioCategoriaIDAO{
+    /**
+     * Metodo para obtener las relaciones de usuario con respecto a categoria
+     */
     getUsuarioCategorias(): UsuarioCategoria[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para obtener la relación con el ID identificador
+     */
     getUsuarioCategoriaByID(usuarioCategoriaID: Number): UsuarioCategoria {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo para agregar una categoria al usuario
+     */
     agregarUsuarioCategoria(usuarioCategoria: UsuarioCategoria): Boolean {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo para quitar una categoria al usuario
+     */
     eliminarUsuarioCategoriaByID(usuarioCategoria: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para actualizar una categoria al usuario
+     */
     actualizarUsuarioCategoria(usuarioCategoria: UsuarioCategoria): Boolean {
         throw new Error("Method not implemented.");
     }
