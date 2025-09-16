20a2569fba99a4209ffeaab535bd34ca3dcb14e3
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-16 15:39:08 -0700

    Actualización Implementación DAO PermisoCategoria - FMA API Perfiles
    Se realiza la actualización de los comentarios respectivos de la implementación para cada método e importación.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/permiso-categoria.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/permiso-categoria.dao.impl.ts
@@ -1,18 +1,43 @@
+/**
+ * Importaciones requeridas el modelo PermisoCategoria e interface PermisoCategoriaIDAO
+ */
 import { PermisoCategoria } from "@entity/permiso-categoria.entity";
 import {PermisoCategoriaIDAO} from "@data.dao/permiso-categoria.dao"
+
+/**
+ * Implementacion del acceso a datos de PermisoCategoriaDAO
+ */
 export class PermisoCategoriaDAO implements PermisoCategoriaIDAO{
+    /**
+     * Metodo para obtener todos las relacionas de permisos con respecto a categoria
+     */
     getPermisoCategorias(): PermisoCategoria[] {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Obtiene las relacion con respecti al id 
+     */
     getPermisoCategoriaByID(permisoCategoriaID: Number): PermisoCategoria {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Agrega una nueva relación de permiso - categoria
+     */
     agregarPermisoCategoria(permisoCategoria: PermisoCategoria): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Elimina un permiso categoria con respecto al id
+     */
     eliminarPermisoCategoriaByID(permisoCategoriaID: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Actualiza la relacion de permiso - categoria
+     */
     actualizarPermisoCategoria(permisoCategoria: PermisoCategoria): Boolean {
         throw new Error("Method not implemented.");
     }
