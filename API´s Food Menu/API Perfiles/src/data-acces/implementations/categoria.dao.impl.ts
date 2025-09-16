d44bcf3888b6147fd2d01c8099da67ac1e71816c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-16 15:38:36 -0700

    Actualización Implementación DAO Categoría - FMA API Perfiles
    Se realiza la actualización de los comentarios respectivos de la implementación para cada método e importación.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/categoria.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/categoria.dao.impl.ts
@@ -1,18 +1,45 @@
+/**
+ * Importaciones requeridas del sistema modelo Categoria e interface CategoriaIDAO
+ */
 import { Categoria } from "@entity/categoria.entity";
 import {CategoriaIDAO} from "@data.dao/categoria.dao"
+
+/**
+ * Implementaccion correspondiente a la categoria
+ */
 export class CategoriaDAO implements CategoriaIDAO{
+
+    /**
+     * Metodo para obtener todas las categorias del sistema
+     */
     getCategorias(): Categoria[] {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para obtener una categoria del sistema por su id
+     */
     getCategoriaByID(categoriaID: Number): Categoria {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para agregar una categoria
+     */
     agregarCategoria(categoria: Categoria): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para eliminar una categoria por id
+     */
     eliminarCategoriaByID(categoriaID: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para actualizar una categoria del sistema. 
+     */
     actualizarCategoria(categoria: Categoria): Boolean {
         throw new Error("Method not implemented.");
     }
