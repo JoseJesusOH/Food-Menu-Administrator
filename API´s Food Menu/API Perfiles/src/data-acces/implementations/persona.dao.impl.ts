1d45b32aced2cf18c555afa59fc7cdecde685460
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-16 15:38:01 -0700

    Actualización Implementación DAO Persona - FMA API Perfiles
    Se realiza la actualización de los comentarios respectivos de la implementación para cada método e importación.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
@@ -1,18 +1,43 @@
+/**
+ * Importaciones requeridas para el metodo el modelo Persona e interface PersonaIDAO
+ */
 import { Persona } from "@entity/persona.entity";
 import {PersonaIDAO} from "@data.dao/persona.dao"
+
+/**
+ * Clase correspondiente al acceso a datos de Persona
+ */
 export class PersonaDAO implements PersonaIDAO{
+    /**
+     * Metodo para obtener todas las personas del sistema
+     */
     getPersonas(): Persona[] {
         throw new Error("Method not implemented.");
     }
+    /**
+     * Metodo para obtener una persona del sistema por id 
+     */
     getPersonaByID(personaID: Number): Persona {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para agregar una persona al sistema
+     */
     agregarPersona(persona: Persona): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para eliminar una persona del sistema
+     */
     eliminarPersonaByID(personaID: Number): Boolean {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Metodo para actualizar una persona del sistema
+     */
     actualizarPersona(persona: Persona): Boolean {
         throw new Error("Method not implemented.");
     }
