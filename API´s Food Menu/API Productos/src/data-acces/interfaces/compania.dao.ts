622f45203d7fc18dd64308b4e41bd43660ca52bc
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-17 22:37:18 -0700

    Actualización interface CompaniaDAO - FMA API Productos
    Se actualizan los return para devolver promesas debido al uso de TypeORM y repositorio correspondiente.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/compania.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/compania.dao.ts
@@ -10,37 +10,37 @@ export interface CompaniaIDAO {
    * Obtiene todas las compañias
    * @returns Arreglo con todas las compañias
    */
-  getCompanias(): Compania[];
+  getCompanias(): Promise<Compania[]>;
   /**
    * Obtiene una compañia por su ID
    * @param companiaID ID de la compañia a buscar
    * @return Compania encontrada
    */
-  getCompaniaById(companiaID: Number): Compania;
+  getCompaniaById(companiaID: Number): Promise<Compania>;
   /**
    * Obtiene una compañia por su UUID
    * @param companiaUuid UUID de la compañia a buscar
    * @return Compania encontrada
    */
-  getCompaniaByUuid(companiaUuid: String): Compania;
+  getCompaniaByUuid(companiaUuid: String): Promise<Compania>;
   
   /**
    * Agrega una nueva compañia a la base de datos
    * @param compania Compañia a agregar
    * @returns Boolean indicando si se agrego o no la compañia 
    */
-  agregarCompania(compania: Compania): Boolean;
+  agregarCompania(compania: Compania): Promise<Boolean>;
   
   /**
    * Elimina una compañia de la base de datos
    * @param companiaId ID de la compañia a eliminar
    * @returns Boolean indicando si se elimino o no la compañia
    */
-  eliminarCompaniaById(companiaId: Number): Boolean;
+  eliminarCompaniaById(companiaId: Number): Promise<Boolean>;
   /**
    * Actualiza una compañia en la base de datos
    * @param compania Compañia a actualizar
    * @returns Boolean indicando si se actualizo o no la compañia
    */
-  actualizarCompania(compania: Compania): Boolean;
+  actualizarCompania(compania: Compania): Promise<Boolean>;
 } 
