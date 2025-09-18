755c5a0a2b9cf10357fc30777f4d885f3145e471
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-17 22:38:46 -0700

    Actualización interface ProductoAlimentoDAO - FMA API Productos
    Se actualizan los return para devolver promesas debido al uso de TypeORM y repositorio correspondiente.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/producto-alimento.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/producto-alimento.dao.ts
@@ -11,46 +11,46 @@ export interface ProductoAlimentoIDAO{
      * @param alimentoId ID del alimento
      * @returns Lista de productos alimentos
      */
-    getProductosAlimentosByIdAlimento(alimentoId:Number):ProductoAlimento[];
+    getProductosAlimentosByIdAlimento(alimentoId:Number):Promise<ProductoAlimento[]>;
 
   /**
    *  Obtiene un producto alimento por su ID
    * @param productoAlimentoId ID del producto alimento a buscar
    * @returns ProductoAlimento con el ID especificado
    */
-    getProductoAlimentoById(productoAlimentoId:Number):ProductoAlimento;
+    getProductoAlimentoById(productoAlimentoId:Number):Promise<ProductoAlimento>;
 
     /**
      * Obtiene un producto alimento por su UUID
      * @param productoAlimentoUuid UUID del producto alimento a buscar
      * @returns ProductoAlimento con el UUID especificado
      */
-    getProductoAlimentoByUuid(productoAlimentoUuid:String):ProductoAlimento;
+    getProductoAlimentoByUuid(productoAlimentoUuid:String):Promise<ProductoAlimento>;
 
     /**
      * Obtiene todos los productos alimentos de la base de datos por el ID del producto
      * @param productoId ID del producto
      * @returns Lista de productos alimentos
      */
-    getProductosAlimentosByIdProducto(productoId:Number):ProductoAlimento[];
+    getProductosAlimentosByIdProducto(productoId:Number):Promise<ProductoAlimento[]>;
     /**
      * Agrega un nuevo producto alimento a la base de datos
      * @param productoAlimento ProductoAlimento a agregar
      * @returns Boolean indicando si se agrego o no el producto alimento
      */
-    agregarProductoAlimento(productoAlimento:ProductoAlimento):Boolean;
+    agregarProductoAlimento(productoAlimento:ProductoAlimento):Promise<Boolean>;
 
     /**
      * Actualiza un producto alimento en la base de datos
      * @param productoAlimento ProductoAlimento a actualizar
      * @returns Boolean indicando si se actualizo o no el producto alimento
      */
-    actualizarProductoAlimento(productoAlimento:ProductoAlimento):Boolean;
+    actualizarProductoAlimento(productoAlimento:ProductoAlimento):Promise<Boolean>;
 
     /**
      * Elimina un producto alimento de la base de datos
      * @param productoAlimentoId ID del producto alimento a eliminar
      * @returns Boolean indicando si se elimino o no el producto alimento
      */
-    eliminarProductoAlimentoById(productoAlimentoId:Number):Boolean;
+    eliminarProductoAlimentoById(productoAlimentoId:Number):Promise<Boolean>;
 }
