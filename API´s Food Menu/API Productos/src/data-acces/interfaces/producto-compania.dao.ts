1eb9ad981cb3ec9014fdb159f6d7a1fb7d3a4a3e
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-17 22:39:08 -0700

    Actualización interface ProductoCompaniaDAO - FMA API Productos
    Se actualizan los return para devolver promesas debido al uso de TypeORM y repositorio correspondiente.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/producto-compania.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/producto-compania.dao.ts
@@ -12,46 +12,46 @@ export interface ProductoCompaniaIDAO {
      * @param productoCompaniaUuid UUID del producto compañia a buscar
      * @returns Producto compañia encontrado
      */
-    getProductoCompaniaByUuid(productoCompaniaUuid: String): ProductoCompania;
+    getProductoCompaniaByUuid(productoCompaniaUuid: String):  Promise<ProductoCompania>;
 
     /**
      * Obtiene un producto compañia por su ID
      * @param productoCompaniaId ID del producto compañia a buscar
      * @returns Producto compañia encontrado
      */
-    getProductoCompaniaById(productoCompaniaId: Number): ProductoCompania;
+    getProductoCompaniaById(productoCompaniaId: Number): Promise<ProductoCompania>;
     /**
      * Obtiene todos los productos compañias de la base de datos por el ID del producto
      * @param productoId ID del producto
      * @returns Lista de productos compañias
      */
-    getProductosCompaniasByIdProducto(productoId: Number): ProductoCompania[];
+    getProductosCompaniasByIdProducto(productoId: Number): Promise<ProductoCompania[]>;
 
     /**
      * Obtiene todos los productos compañias de la base de datos por el ID de la compañia
      * @param companiaId ID de la compañia
      * @returns Lista de productos compañias
      */
-    getProductosCompaniasByIdCompania(companiaId: Number): ProductoCompania[];
+    getProductosCompaniasByIdCompania(companiaId: Number): Promise<ProductoCompania[]>;
 
     /**
      * Agrega un nuevo producto compañia a la base de datos
      * @param productoCompania ProductoCompania a agregar
      * @returns Boolean indicando si se agrego o no el producto compañia
      */
-    agregarProductoCompania(productoCompania: ProductoCompania): Boolean;
+    agregarProductoCompania(productoCompania: ProductoCompania): Promise<Boolean>;
 
     /**
      * Actualiza un producto compañia en la base de datos
      * @param productoCompania ProductoCompania a actualizar
      * @returns Boolean indicando si se actualizo o no el producto compañia
      */
-    actualizarProductoCompania(productoCompania: ProductoCompania): Boolean;
+    actualizarProductoCompania(productoCompania: ProductoCompania): Promise<Boolean>;
 
     /**
      * Elimina un producto compañia de la base de datos
      * @param productoCompaniaId ID del producto compañia a eliminar
      * @returns Boolean indicando si se elimino o no el producto compañia
      */
-    eliminarProductoCompaniaById(productoCompaniaId: Number): Boolean;
+    eliminarProductoCompaniaById(productoCompaniaId: Number): Promise<Boolean>;
 }
