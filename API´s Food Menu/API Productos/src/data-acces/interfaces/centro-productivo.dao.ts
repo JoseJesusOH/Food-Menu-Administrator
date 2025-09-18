9f5c2fe2858aff270dba9c63a4a7e82e92ae3678
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-17 22:38:21 -0700

    Actualización interface CentroProductivoDAO - FMA API Productos
    Se actualizan los return para devolver promesas debido al uso de TypeORM y repositorio correspondiente.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/centro-productivo.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/centro-productivo.dao.ts
@@ -11,36 +11,36 @@ export interface CentroProductivoIDAO {
      * Obtiene todos los centros productivos
      * @returns Arreglo con todos los centros productivos
      */
-    getCentrosProductivos(): CentroProductivo[];
+    getCentrosProductivos(): Promise<CentroProductivo[]>;
     /**
      * Obtiene un centro productivo por su ID
      * @param centroProductivoId ID del centro productivo a buscar
      * @return CentroProductivo encontrado
      */
-    getCentroProductivoById(centroProductivoId: Number): CentroProductivo;
+    getCentroProductivoById(centroProductivoId: Number): Promise<CentroProductivo>;
     /**
      * Obtiene un centro productivo por su UUID
      * @param centroProductivoUuid UUID del centro productivo a buscar
      * @returns CentroProductivo encontrado
      */
-    getCentroProductivoByUuid(centroProductivoUuid: String): CentroProductivo;
+    getCentroProductivoByUuid(centroProductivoUuid: String): Promise<CentroProductivo>;
    
     /**
      * Agrega un nuevo centro productivo a la base de datos
      * @param centroProductivo Centro productivo a agregar
      * @returns Boolean indicando si se agrego o no el centro productivo
      */
-    agregarCentroProductivo(centroProductivo: CentroProductivo): Boolean;
+    agregarCentroProductivo(centroProductivo: CentroProductivo): Promise<Boolean>;
     /**
      *  Actualiza un centro productivo en la base de datos
      * @param centroProductivo Centro productivo a actualizar
      * @returns Boolean indicando si se actualizo o no el centro productivo
      */
-    actualizarCentroProductivo(centroProductivo: CentroProductivo): Boolean;
+    actualizarCentroProductivo(centroProductivo: CentroProductivo): Promise<Boolean>;
     /**
      * Elimina un centro productivo de la base de datos
      * @param centroProductivoId ID del centro productivo a eliminar
      * @returns Boolean indicando si se elimino o no el centro productivo   
      */
-    eliminarCentroProductivoById(centroProductivoId: Number): Boolean;
+    eliminarCentroProductivoById(centroProductivoId: Number): Promise<Boolean>;
 }
