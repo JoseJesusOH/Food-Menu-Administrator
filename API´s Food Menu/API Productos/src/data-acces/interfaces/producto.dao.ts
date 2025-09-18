fe74af90d032fa4ad4f5203e2d262adbbbb67464
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-17 22:35:34 -0700

    Actualización interface ProductoDAO - FMA API Productos
    Se actualizan los return para devolver promesas debido al uso de TypeORM y repositorio correspondiente.

--- API´s Food Menu/API Productos/src/data-acces/interfaces/producto.dao.ts
+++ API´s Food Menu/API Productos/src/data-acces/interfaces/producto.dao.ts
@@ -11,40 +11,40 @@ export interface ProductoIDAO{
      * Obtiene todos los productos
      * @returns Lista de productos
      */
-    getProductos():Producto[];
+    getProductos(): Promise<Producto[]>;
     
     /**
      * Obtiene un producto por su ID
      * @param productoId ID del producto a buscar
      * @returns Producto con el ID especificado
      */
-    getProductoById(productoId:Number):Producto;
+    getProductoById(productoId:Number): Promise<Producto>;
 
     /**
      * Obtiene un producto por su UUID
      * @param productoUuid UUID del producto a buscar
      * @returns Producto con el UUID especificado
      */
-    getProductoByUuid(productoUuid:String):Producto;
+    getProductoByUuid(productoUuid:String): Promise<Producto>;
    
     /**
      * Agrega un nuevo producto a la base de datos
      * @param producto Producto a agregar
      * @returns Boolean indicando si se agrego o no el producto
      */
-    agregarProducto(producto:Producto):Boolean;
+    agregarProducto(producto:Producto): Promise<Boolean>;
     
     /**
      * Actualiza un producto en la base de datos
      * @param producto Producto a actualizar
      * @returns Boolean indicando si se actualizo o no el producto
      */
-    actualizarProducto(producto:Producto):Boolean;
+    actualizarProducto(producto:Producto): Promise<Boolean>;
 
     /**
      * Elimina un producto de la base de datos
      * @param productoId ID del producto a eliminar
      * @returns Boolean indicando si se elimino o no el producto
      */
-    eliminarProductoById(productoId:Number):Boolean;
+    eliminarProductoById(productoId:Number): Promise<Boolean>;
 }
