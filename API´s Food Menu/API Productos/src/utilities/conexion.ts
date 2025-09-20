24c8b39d75d21a15cdbe7b6d9438b514ca44128c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-19 21:26:43 -0700

    Actualización Conexión - FMA API Productos
    Se elimina la inicialización en la clase de conexión para que esta sea ejecutada inicialmente en el archibvo de arranque.

--- API´s Food Menu/API Productos/src/utilities/conexion.ts
+++ API´s Food Menu/API Productos/src/utilities/conexion.ts
@@ -9,7 +9,7 @@ import { ProductoCompania } from "@entity/producto-compania.entity";
 import { Compania } from "@entity/compania.entity";
 import { CentroProductivo } from "@entity/centro-productivo.entity";
 
-  const Conexion = new DataSource({
+const Conexion = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
@@ -17,13 +17,10 @@ import { CentroProductivo } from "@entity/centro-productivo.entity";
     password: "root",
     database: "api_productos",
     synchronize: true,
-    logging: true,
+    logging: false,
     entities: [Categoria, Alimento, Producto, ProductoAlimento, ProductoCompania, Compania, CentroProductivo],
     subscribers: [],
     migrations: [],
 })
-Conexion.initialize()
-  .then(() => console.log("✅ Conexión inicializada"))
-  .catch((err) => console.error("❌ Error al inicializar:", err));
 
 export {Conexion};
