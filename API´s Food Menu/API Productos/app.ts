894d613e3a5a9470e6c1c2231df64cbc7d52f440
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-19 21:59:39 -0700

    Actualización del modulo APP - FMA API Productos
    Se realiza la actualización del modulo de arranque del servidor donde se realiza la importacion del servidor y pruebas, se espera la actualización del mismo para un mejor uso.

--- API´s Food Menu/API Productos/app.ts
+++ API´s Food Menu/API Productos/app.ts
@@ -1,54 +1,5 @@
-/*
-import express, { Request, Response } from "express";
-
-const app = express();
-const PORT = 3000;
-
-// Middleware
-app.use(express.json());
-
-// Ruta ejemplo
-app.get("/", (req: Request, res: Response) => {
-  res.send("Servidor Express con TypeScript funcionando 🚀");
-});
-
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-*/
-import "reflect-metadata"
-import { Conexion } from "./src/utilities/conexion"
-import { Categoria } from "./src/entities/categoria.entity"
-async function pruebas() {
-
-  /*  try {
-  Conexion.initialize().then(() => {
-      console.log("Entities registered:", Conexion.entityMetadatas.map(metadata => metadata.name));
-  }).catch(error => console.log(error));} catch (error) {
-    console.log(error)
-  } 
-  */
-  try {
-    
-    //Creación categoria
-    await Conexion.initialize();
-    const categoria2 = new Categoria();
-    const categoriaRepo = Conexion.getRepository(Categoria);
-    categoria2.setNombre("E");
-    await categoriaRepo.save(categoria2);
-    console.log("Guardado");
-    
-    /*
-    //Obtencion categorias
-    await Conexion.initialize();
-    const categoriaRepo = Conexion.getRepository(Categoria);
-    const categorias = await categoriaRepo.find();
-    console.log("Categorias; ", categorias);
-    */
-  } catch (error) {
-    console.log(error)
-  }
-}
+// app.ts
+import { servidorStart } from "./src/servidor";
+import { pruebas } from "./src/pruebas";
+servidorStart();
 pruebas();
