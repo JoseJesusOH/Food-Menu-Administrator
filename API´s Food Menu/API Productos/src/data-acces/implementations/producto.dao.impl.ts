f9dd681cb2aacf3788ad83c0b69b65e11dcb7896
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-20 15:49:13 -0700

    Actualización del metodo para obtener all productos - FMA API Productos
    Se realiza la actualización del metodo donde se establece el Logger, try catch, y comentarios respectivos.

--- API´s Food Menu/API Productos/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/producto.dao.impl.ts
@@ -4,17 +4,30 @@
 import { Producto } from "@entity/producto.entity";
 import { ProductoIDAO } from "@data.dao/producto.dao";
 import { Conexion } from "@utility/conexion"
+import { LoggerAPI } from "@utility/logger";
 /**
  * Implementación del DAO de Producto.
  */
 export class ProductoDAO implements ProductoIDAO {
-
+    productoRepositorio = Conexion.getRepository(Producto);
     /** 1
      * Metodo para obtener todos los productos.
      */
     async getProductos(): Promise<Producto[]> {
-        const productoRepositorio = Conexion.getRepository(Producto);
-        return productoRepositorio.find()
+        LoggerAPI.info("Se inicia la busqueda de productos del sistema")
+        try {
+            const productos = await this.productoRepositorio.find();
+            if (!productos || productos.length === 0) {
+                LoggerAPI.warn("No se han encontrado productos en el sistema")
+                return []
+            } else {
+                LoggerAPI.info(`Se han enconctreado un total de ${productos.length} productos`)
+                return productos;
+            }
+        } catch (error) {
+            LoggerAPI.warn(`Se produjo un error en la busqueda de productos error ${error}`)
+            throw error;
+        }
     }
     /** 2
      * Metodo para obtener un producto por su ID.
@@ -27,15 +40,15 @@ export class ProductoDAO implements ProductoIDAO {
      * Metodo para obtener un producto por su UUID.
      */
     async getProductoByUuid(productoUuid: String): Promise<Producto> {
-           const productoRepositorio = Conexion.getRepository(Producto);
+        const productoRepositorio = Conexion.getRepository(Producto);
         return productoRepositorio.findOneBy({ productoUuid: productoUuid })
-  }
+    }
     /** 4
      * Metodo para agregar un producto.
      */
     async agregarProducto(producto: Producto): Promise<Boolean> {
-               const productoRepositorio = Conexion.getRepository(Producto);
-        return (await productoRepositorio.insert(producto)).identifiers.length>0
+        const productoRepositorio = Conexion.getRepository(Producto);
+        return (await productoRepositorio.insert(producto)).identifiers.length > 0
     }
     /** 5
      * Metodo para actualizar un producto.
