30b44997161fe6c73b93fc768405705cfc5a1f05
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-19 22:07:48 -0700

    Actualizacion Archivo app.ts - FMA API Productos
    Se realiza la creación de comentarios respectivos para cada metodo, importación ,etc.

--- API´s Food Menu/API Productos/app.ts
+++ API´s Food Menu/API Productos/app.ts
@@ -1,5 +1,15 @@
-// app.ts
+/**
+ * Importaciones Requeridas Servidor y modulo de Pruebas
+ */
 import { servidorStart } from "./src/servidor";
 import { pruebas } from "./src/pruebas";
+
+/**
+ * Llamada al metodo para iniciar los servidores express
+ */
 servidorStart();
+
+/**
+ * Llamada al metodo para ejecucion de pruebas
+ */
 pruebas();
