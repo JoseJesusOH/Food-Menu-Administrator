c54d31977bc706cf0c6b60bf3e3ae84e72c82197
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-21 18:19:56 -0700

    Creación del metodo get  productocompania por id de compania - FMA API Productos
    Se realiza la implementacion del metodo para obtener todos los productocompania por id de la compania,  se manejan errores correspondiente y logs requeridos.

--- API´s Food Menu/API Productos/src/data-acces/implementations/producto-compania.dao.impl.ts
+++ API´s Food Menu/API Productos/src/data-acces/implementations/producto-compania.dao.impl.ts
@@ -5,24 +5,25 @@ import { ProductoCompania } from "@entity/producto-compania.entity";
 import { ProductoCompaniaIDAO } from "@data.dao/producto-compania.dao";
 import { Conexion } from "@utility/conexion";
 import { LoggerAPI } from "@utility/logger";
+import { Compania } from "@entity/compania.entity";
 /**
  * Implementación del DAO de ProductoCompania.
  */
 export class ProductoCompaniaDAO implements ProductoCompaniaIDAO {
-productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
+    productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
     /** 1
      * Metodo que obtiene un ProductoCompania por su UUID.
      */
     async getProductoCompaniaByUuid(productoCompaniaUuid: String): Promise<ProductoCompania> {
         LoggerAPI.info("Se ha iniciado el proceso para obtener un producto compania por id")
         try {
-            const productoCompania=await this.productoCompaniaRepositorio.findOneBy({productoCompaniaUuid})
-            if(productoCompania!==null){
+            const productoCompania = await this.productoCompaniaRepositorio.findOneBy({ productoCompaniaUuid })
+            if (productoCompania !== null) {
                 LoggerAPI.info(`Se ha encontrado el productocompania con uuid ${productoCompaniaUuid}`)
                 return productoCompania;
-            }else{
+            } else {
                 LoggerAPI.warn(`No se ha encontrado el productocompania con uuid ${productoCompaniaUuid}`)
-               return null;
+                return null;
             }
         } catch (error) {
             LoggerAPI.warn(`Se ha producido un error al obtener el producto por UUID error ${error}`)
@@ -35,17 +36,17 @@ productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
     async getProductoCompaniaById(productoCompaniaId: Number): Promise<ProductoCompania> {
         LoggerAPI.info("Se inicia el proceso para obtener un producto compania por id DB")
         try {
-            const resultado= await this.productoCompaniaRepositorio.findOneBy({productoCompaniaId:productoCompaniaId.valueOf()})
-            if(resultado!==null){
-                 LoggerAPI.info(`Se ha encontrado un producto compania con ID ${productoCompaniaId}`)
+            const resultado = await this.productoCompaniaRepositorio.findOneBy({ productoCompaniaId: productoCompaniaId.valueOf() })
+            if (resultado !== null) {
+                LoggerAPI.info(`Se ha encontrado un producto compania con ID ${productoCompaniaId}`)
                 return resultado;
-            }else{
+            } else {
                 LoggerAPI.warn(`No se ha encontrado un producto compania con ID ${productoCompaniaId}`)
                 return null;
             }
         } catch (error) {
-              LoggerAPI.warn(`Se ha producido un error en la busqueda por id de productocompania error; ${error}`)
-              throw error;
+            LoggerAPI.warn(`Se ha producido un error en la busqueda por id de productocompania error; ${error}`)
+            throw error;
         }
     }
     /** 3
@@ -58,7 +59,21 @@ productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
      * Metodo que obtiene los ProductoCompania por su UUID de producto compañia.
      */
     async getProductosCompaniasByIdCompania(companiaId: Number): Promise<ProductoCompania[]> {
-        throw new Error("Method not implemented.");
+        LoggerAPI.info("Se inicia el proceso para obtener todos los productos compania por id de compania")
+        try {
+            const resultado = await this.productoCompaniaRepositorio.findBy({ compania: new Compania(companiaId.valueOf()) })
+            if (resultado.length > 0) {
+                LoggerAPI.info(`Se han encontrado un total de ${resultado.length} productocompanias`)
+                return resultado;
+            } else {
+                LoggerAPI.warn(`No se han encontrado productocompania por el id de la compania ${companiaId}`)
+                return
+                []
+            }
+        } catch (error) {
+            LoggerAPI.warn(`Se ha producido un error al obtener todos los productocompania por id de compania error; ${error}`)
+            throw error;
+        }
     }
     /** 5
      * Metodo que agrega un ProductoCompania.
@@ -66,14 +81,14 @@ productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
     async agregarProductoCompania(productoCompania: ProductoCompania): Promise<Boolean> {
         LoggerAPI.info("Se iniia el metodo para agregar un productoCompania en el sistema")
         try {
-             const resultado=await this.productoCompaniaRepositorio.insert(productoCompania)
-             if(resultado.identifiers.length>0){
+            const resultado = await this.productoCompaniaRepositorio.insert(productoCompania)
+            if (resultado.identifiers.length > 0) {
                 LoggerAPI.info("Se ha agregado el productoCompania exitosamente")
                 return new Boolean(true)
-             }else{
+            } else {
                 LoggerAPI.warn("No se ha agregado el producto compania")
                 return new Boolean(false)
-             }
+            }
         } catch (error) {
             LoggerAPI.warn(`Se ha producido un error al agregar un productocompania error; ${error}`)
             throw error;
@@ -85,15 +100,15 @@ productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
     async actualizarProductoCompania(productoCompania: ProductoCompania): Promise<Boolean> {
         LoggerAPI.info("Se ha iniciado el proceso para actualizar un productoCompania de DB")
         try {
-             const resultado=await this.productoCompaniaRepositorio.update({productoCompaniaId:productoCompania.getProductoCompaniaId()},productoCompania)
-             if(resultado!==null){
+            const resultado = await this.productoCompaniaRepositorio.update({ productoCompaniaId: productoCompania.getProductoCompaniaId() }, productoCompania)
+            if (resultado !== null) {
                 LoggerAPI.info(`Se ha actualizado el productocompania exitosamente`)
                 return new Boolean(true);
-             }
-             else{
+            }
+            else {
                 LoggerAPI.warn(`No se ha actualizado el productocompania`)
                 return new Boolean(false)
-             }
+            }
         } catch (error) {
             LoggerAPI.warn(`Se ha producido un error al actualizar un productoCompania error; ${error}`)
             throw error;
@@ -103,21 +118,21 @@ productoCompaniaRepositorio = Conexion.getRepository(ProductoCompania);
      * Metodo que elimina un ProductoCompania por su ID.
      */
     async eliminarProductoCompaniaById(productoCompaniaId: Number): Promise<Boolean> {
-    LoggerAPI.info("Se ha iniciado el proceso para eliminar un producto compania por id ")
-    try {  
-         const  resultado=await this.productoCompaniaRepositorio.delete({productoCompaniaId})
-         if(resultado.affected>0){
-            LoggerAPI.info("Se ha eliminado el producto compania exitosamente")
-            return new Boolean(true)
-         }else{
-            LoggerAPI.warn("No se ha eliminado el producto compania")
-            return new Boolean(false)
-         }
-    } catch (error) {
-        LoggerAPI.warn(`Se ha producido un error al eliminar un productocompania por id error; ${error}`)
-       throw error; 
+        LoggerAPI.info("Se ha iniciado el proceso para eliminar un producto compania por id ")
+        try {
+            const resultado = await this.productoCompaniaRepositorio.delete({ productoCompaniaId })
+            if (resultado.affected > 0) {
+                LoggerAPI.info("Se ha eliminado el producto compania exitosamente")
+                return new Boolean(true)
+            } else {
+                LoggerAPI.warn("No se ha eliminado el producto compania")
+                return new Boolean(false)
+            }
+        } catch (error) {
+            LoggerAPI.warn(`Se ha producido un error al eliminar un productocompania por id error; ${error}`)
+            throw error;
+        }
     }
-      }
 
 
 }
