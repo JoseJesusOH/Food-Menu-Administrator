77f72d0333fc955ed488689af7564757e8e0bce8
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-23 18:13:50 -0700

    Actualización de Interfaz de Acceso a Datos DireccionDAO - FMA API Perfiles
    Se realiza la actualiazacion de la interfaz correspondiente, en esta se actualizan los return de cada metodo para adecvuarlos a lo requerido en TypeORM.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/direccion.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/direccion.dao.ts
@@ -12,33 +12,33 @@ export interface DireccionIDAO {
      * Método para obtener todas las direcciones
      * @return Un arreglo de objetos Direccion
      */
-    getDirecciones(): Direccion[];
+    getDirecciones(): Promise<Direccion[]>;
 
     /** 
      * Método para obtener una direccion por su ID
-     * @param direccionID ID de la direccion a buscar
+     * @param direccionId ID de la direccion a buscar
      * @return Un objeto Direccion si se encuentra, de lo contrario retorna null
      */
-    getDireccionByID(direccionID: Number): Direccion;
+    getDireccionById(direccionId: Number): Promise<Direccion>;
 
     /** 
      * Método para agregar una nueva direccion
      * @param direccion Objeto Direccion a agregar
      * @return true si la operación fue exitosa, de lo contrario false
      */
-    agregarDireccion(direccion: Direccion): Boolean;
+    agregarDireccion(direccion: Direccion): Promise<Boolean>;
 
     /** 
      * Método para eliminar una direccion por su ID
-     * @param direccionID ID de la direccion a eliminar
+     * @param direccionId ID de la direccion a eliminar
      * @return true si la operación fue exitosa, de lo contrario false
      */
-    eliminarDireccion(direccionID: Number): Boolean;
+    eliminarDireccion(direccionId: Number): Promise<Boolean>;
 
     /** 
      * Método para actualizar una direccion existente
      * @param direccion Objeto Direccion con los datos actualizados
      * @return true si la operación fue exitosa, de lo contrario false
      */
-    actualizarDireccion(direccion: Direccion): Boolean;
+    actualizarDireccion(direccion: Direccion): Promise<Boolean>;
 }
