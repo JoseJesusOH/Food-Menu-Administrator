a9a2e168b627a22d5b7ced7fef1a477532ed977d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-23 18:35:49 -0700

    Implmentación del metodo GetDirecciones - FMA API Perfiles
    Se realiza la implementación del metodo para obtener todas las direccion en DB ,  se manajan errores respectivos y logs correspondientes.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/direccion.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/direccion.dao.impl.ts
@@ -3,44 +3,58 @@
  */
 import { Direccion } from "@entity/direccion.entity"
 import {DireccionIDAO} from "@data.dao/direccion.dao"
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
 /**
  * Implementación correspondiente al acceso a datos de Direccion
  */
 export class DireccionDAO implements DireccionIDAO{
-
+     direccionRepiositorio=Conexion.getRepository(Direccion)
     /**
      * Metodo para obtener todas las direcciones del sistema
      */
-    getDirecciones(): Direccion[] {
-        throw new Error("Method not implemented.");
+    async getDirecciones(): Promise<Direccion[]> {
+        LoggerAPI.info("Se inicia el metodo para obtener todas las direcciones en DB")
+        try {
+            const resultados = await this.direccionRepiositorio.find();
+            if(resultados.length>0){
+                LoggerAPI.info(`Se han encontrado un total de ${resultados.length} direcciones`)
+                return resultados;
+            }else{
+                LoggerAPI.warn("No se han encontrado direcciones en DB")
+                return []
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 
     /**
      * Metodo para obtener la direccion por id
      */
-    getDireccionByID(direccionID: Number): Direccion {
+    getDireccionById(direccionId: Number): Promise<Direccion> {
         throw new Error("Method not implemented.");
     }
 
     /**
      * Metodo para agregar una direccion
      */
-    agregarDireccion(direccion: Direccion): Boolean {
+    agregarDireccion(direccion: Direccion): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
 
     /**
      * Metodo para eliminar una direccion
      */
-    eliminarDireccion(direccionID: Number): Boolean {
+    async eliminarDireccion(direccionId: Number): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
 
     /**
      * Metodo para actualizar una direccion
      */
-    actualizarDireccion(direccion: Direccion): Boolean {
+    actualizarDireccion(direccion: Direccion): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
     
