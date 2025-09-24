43ce4c8c7ffae0283511d028e3863915c73ada56
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-23 18:18:59 -0700

    Actualización de Interfaz de Acceso a Datos PersonaDAO - FMA API Perfiles
    Se realiza la actualiazacion de la interfaz correspondiente, en esta se actualizan los return de cada metodo para adecvuarlos a lo requerido en TypeORM.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/persona.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/persona.dao.ts
@@ -10,32 +10,32 @@ export interface PersonaIDAO{
     * Metodo para obtener todas las personas
     * @return {Persona[]} Retorna un arreglo de personas
     */
-    getPersonas():Persona[];
+    getPersonas():Promise<Persona[]>;
     /**
      * Metodo para obtener una persona por su ID
-     * @param personaID ID de la persona
+     * @param personaId ID de la persona
      * @return {Persona} Retorna la persona que coincide con el ID
      */
-    getPersonaByID(personaID:Number):Persona;
+    getPersonaById(personaId:Number):Promise<Persona>;
     
         /** 
          * Metodo para agregar una nueva persona
          * @param persona Persona a agregar
          * @return {Boolean} Retorna true si se agrego correctamente, false si no
          */
-    agregarPersona(persona:Persona):Boolean;
+    agregarPersona(persona:Persona):Promise<Boolean>;
     
     /**
      * Metodo para eliminar una persona por su ID
      * @param personaID ID de la persona a eliminar
      * @return {Boolean} Retorna true si se elimino correctamente, false si no
      */
-    eliminarPersonaByID(personaID:Number):Boolean;
+    eliminarPersonaByID(personaID:Number):Promise<Boolean>;
     
     /**
      * Metodo para actualizar una persona
      * @param persona Persona a actualizar
      * @return {Boolean} Retorna true si se actualizo correctamente, false si no
      */
-    actualizarPersona(persona:Persona):Boolean;
+    actualizarPersona(persona:Persona):Promise<Boolean>;
 }
