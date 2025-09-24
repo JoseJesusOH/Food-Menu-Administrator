6ab1facff5d73b59868d913d6815051d7315b04b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-23 18:14:35 -0700

    Actualización de Interfaz de Acceso a Datos PermisoDAO - FMA API Perfiles
    Se realiza la actualiazacion de la interfaz correspondiente, en esta se actualizan los return de cada metodo para adecvuarlos a lo requerido en TypeORM.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso.dao.ts
@@ -10,33 +10,33 @@ export interface PermisoIDAO{
      * Metodo para obtener todos los permisos
      * @return Array de permisos
      */
-    getPermisos():Permiso[];
+    getPermisos(): Promise<Permiso[]>;
 
      /**
       *  Metodo para obtener un permiso por su ID
-      * @param permisoID ID del permiso a buscar
+      * @param permisoId ID del permiso a buscar
       * @return Permiso encontrado
       */
-    getPermisoByID(permisoID:Number):Permiso;
+    getPermisoById(permisoId:Number): Promise<Permiso>;
 
     /**
      * Metodo para agregar un permiso
      * @param permiso Permiso a agregar
      * @return Booleano que indica si se agrego el permiso
      */
-    agregarPermiso(permiso:Permiso):Boolean;
+    agregarPermiso(permiso:Permiso): Promise<Boolean>;
 
     /**
      * Metodo para eliminar un permiso por su ID
-     * @param permisoID ID del permiso a eliminar
+     * @param permisoId ID del permiso a eliminar
      * @return Booleano que indica si se elimino el permiso
      */
-    eliminarPermisoByID(permisoID:Number):Boolean;
+    eliminarPermisoById(permisoId:Number): Promise<Boolean>;
     
     /**
      * Metodo para actualizar un permiso
      * @param permiso Permiso a actualizar
      * @return Booleano que indica si se actualizo el permiso
      */
-    actualizarPermiso(permiso:Permiso):Boolean;
+    actualizarPermiso(permiso:Permiso): Promise<Boolean>;
 }
