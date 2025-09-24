21922130d4b7876d0c69be0871408527d84b2ec7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-23 18:19:23 -0700

    Actualización de Interfaz de Acceso a Datos UsuarioCategoriaDAO - FMA API Perfiles
     Se realiza la actualiazacion de la interfaz correspondiente, en esta se actualizan los return de cada metodo para adecvuarlos a lo requerido en TypeORM.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/usuario-categoria.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/usuario-categoria.dao.ts
@@ -10,31 +10,31 @@ export interface UsuarioCategoriaIDAO{
      * Metodo para obtener todas las categorias de usuario
      * @return {UsuarioCategoria[]} Retorna un arreglo de categorias de usuario
      */
-    getUsuarioCategorias():UsuarioCategoria[];
+    getUsuarioCategorias():Promise<UsuarioCategoria[]>;
 
     /**
      * Metodo para obtener una categoria de usuario por su ID
-     * @param usuarioCategoriaID ID de la categoria de usuario
+     * @param usuarioCategoriaId ID de la categoria de usuario
      * @return {UsuarioCategoria} Retorna la categoria de usuario que coincide con el ID
      */
-    getUsuarioCategoriaByID(usuarioCategoriaID:Number):UsuarioCategoria;
+    getUsuarioCategoriaById(usuarioCategoriaId:Number):Promise<UsuarioCategoria>;
 
     /**
      * Metodo para agregar una nueva categoria de usuario
      * @param usuarioCategoria Categoria de usuario a agregar
      * @return {Boolean} Retorna true si se agrego correctamente, false si no
      */
-    agregarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Boolean;
+    agregarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Promise<Boolean>;
     /**
      * Metodo para eliminar una categoria de usuario por su ID
      * @param usuarioCategoria ID de la categoria de usuario a eliminar
      * @return {Boolean} Retorna true si se elimino correctamente, false si no
      */
-    eliminarUsuarioCategoriaByID(usuarioCategoria:Number):Boolean;
+    eliminarUsuarioCategoriaByID(usuarioCategoria:Number):Promise<Boolean>;
     /**
      * Metodo para actualizar una categoria de usuario
      * @param usuarioCategoria Categoria de usuario a actualizar
      * @return {Boolean} Retorna true si se actualizo correctamente, false si no
      */
-    actualizarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Boolean;
+    actualizarUsuarioCategoria(usuarioCategoria:UsuarioCategoria):Promise<Boolean>;
 }
