87c429ce534aabcba76a0c6da91d588da7493461
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-24 17:49:56 -0700

    Actualizacion de interface AlimentoServicie - FMA API Productos
    Se realiza la actualización de la interface se agregan los comentarioss respectivos a las importaciones, exportaciones  e interface.

--- API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
+++ API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
@@ -1,5 +1,17 @@
+/**
+ * Importaciones del DTO requerido
+ */
 import {AlimentoDTO} from "@dto/alimento.dto"
+/**
+ * Interfaz que implementara los servicios de Alimento
+ */
 interface IAlimentoService{
- 
+   obtenerAlimentos():AlimentoDTO;
+   agregarAlimento(alimentoDTO: AlimentoDTO): AlimentoDTO;
+   eliminar(alimentoId: Number): Boolean;
+   obtenerAlimentoById(alimentoId:Number):AlimentoDTO;
 }
+/**
+ * Exportacion de la interfaz
+ */
 export {IAlimentoService}
