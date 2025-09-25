40dd85d60f06591a3e5f4b6edbebab02dbb7354a
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-24 17:54:08 -0700

    Actualización AlimentoService Interface - FMA API Productos
    Se realiza la actualización de los comentarios respectivos de la Interface con respecto a los metodos de cada servicio especificando parametros y retornos.

--- API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
+++ API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
@@ -6,9 +6,28 @@ import {AlimentoDTO} from "@dto/alimento.dto"
  * Interfaz que implementara los servicios de Alimento
  */
 interface IAlimentoService{
+    /**
+     * Servicio para obtener todos los alimentos 
+     * @returns Alimentos del Sistema 
+     */
    obtenerAlimentos():AlimentoDTO;
+   /**
+    * Servicio para agregar un alimento
+    * @param alimentoDTO Alimento a Registrar
+    * @returns True si el alimento fue registrado correctamente , false si este no fue registrado
+    */
    agregarAlimento(alimentoDTO: AlimentoDTO): AlimentoDTO;
+    /**
+     * Servicio para la eliminación de un alimento
+     * @param alimentoId ID Alimento a eliminar
+     * @returns True si el alimento fue eliminado, false en caso contrario
+     */
    eliminar(alimentoId: Number): Boolean;
+   /**
+    * Servicio para obtener un alimento por ID
+    * @param alimentoId ID Alimento a Obtener
+    * @returns Alimento encontrado, null en caso contrario
+    */
    obtenerAlimentoById(alimentoId:Number):AlimentoDTO;
 }
 /**
