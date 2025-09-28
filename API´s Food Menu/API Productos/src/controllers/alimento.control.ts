bf2c7d8a370f0b1df28c45ae594fe73935dc1d5f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-27 22:00:46 -0700

    Comentarios de metodos en AlimentoControl - FMA API Productos
    Se realiza los comentarios de los metodos de AlimentoControl.

--- API´s Food Menu/API Productos/src/controllers/alimento.control.ts
+++ API´s Food Menu/API Productos/src/controllers/alimento.control.ts
@@ -1,15 +1,41 @@
 class AlimentoControl {
+    /**
+     * Crea un nuevo alimento en la base de datos.
+     * Normalmente toma los datos del alimento desde req.body
+     * y responde con el alimento creado o un mensaje de éxito.
+     */
     agregarAlimento = async (req, res, next) => {
     }
+
+    /**
+     * Actualiza un alimento existente.
+     * Suele recibir el id del alimento en req.params.id y
+     * los nuevos datos en req.body.
+     */
     actualizarAlimento = async (req, res, next) => {
     }
+
+    /**
+     * Obtiene todos los alimentos.
+     * No suele recibir parámetros (salvo filtros opcionales en req.query)
+     * y responde con un array de alimentos.
+     */
     obtenerAlimentos = async (req, res, next) => {
     }
+
+    /**
+     * Obtiene un alimento específico por su id.
+     * El id vendría en req.params.id.
+     */
     obtenerAlimentoById = async (req, res, next) => {
     }
 
+    /**
+     * Elimina un alimento de la base de datos.
+     * El id del alimento se suele recibir en req.params.id.
+     */
     eliminarAlimento = async (req, res, next) => {
     }
-
 }
+
 export { AlimentoControl }
