8d053c0ef3f1bd5e8268517a8282ac9ffd32b704
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 22:23:16 -0700

    Implementacion de metodos Abstractos de AlimentoIServicio - FMA API Productos
    Se realiza la implementación de los metodos de la interfaz AlimentoIServicio en AlimentoServicio.

--- API´s Food Menu/API Productos/src/services/implementations/alimento.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/alimento.servicio.impl.ts
@@ -1,3 +1,18 @@
-class AlimentoServicio{
+
+import { AlimentoDTO } from "@dto/alimento.dto";
+import {AlimentoIServicio } from "@service.dao/alimento.servicio"
+class AlimentoServicio implements AlimentoIServicio{
+    obtenerAlimentos(): AlimentoDTO {
+        throw new Error("Method not implemented.");
+    }
+    agregarAlimento(alimentoDTO: AlimentoDTO): AlimentoDTO {
+        throw new Error("Method not implemented.");
+    }
+    eliminar(alimentoId: Number): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    obtenerAlimentoById(alimentoId: Number): AlimentoDTO {
+        throw new Error("Method not implemented.");
+    }
     
 }
