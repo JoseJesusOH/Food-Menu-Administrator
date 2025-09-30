5a9d397d4d9ad5d2ed53921b264d31b88455daab
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 22:31:50 -0700

    Implementación de Interfaz CompaniaIServicio - FMA API Productos
    Se realiza la implementacion de la CompaniaIServicio en CompaniaServicio.

--- API´s Food Menu/API Productos/src/services/implementations/compania.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/compania.servicio.impl.ts
@@ -1,3 +1,18 @@
-class ProveedorServicio{
+    
+import { CompaniaDTO } from "@dto/compania.dto";
+import {CompaniaIServicio } from "@service.dao/compania.servicio"
+class CompaniaServicio implements CompaniaIServicio{
+    getCompanias(): CompaniaDTO {
+        throw new Error("Method not implemented.");
+    }
+    agregarCompania(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCompania(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    getCompaniaById(): CompaniaDTO {
+        throw new Error("Method not implemented.");
+    }
     
 }
