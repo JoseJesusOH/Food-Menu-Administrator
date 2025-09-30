b7c45f1f7a8d73a2cd409c53874cc53ca8167f02
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 22:34:28 -0700

    Implementacion de interfaz ProductoIServicio - FMA API Productos
    Se realiza implementacion de la interfaz ProductoIServicio en ProductoServicio.

--- API´s Food Menu/API Productos/src/services/implementations/producto.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/producto.servicio.impl.ts
@@ -1,3 +1,17 @@
-class ProductoServicio{
+import { ProductoDTO } from "@dto/producto.dto";
+import { ProductoIService } from "@service.dao/producto.servicio";
+class ProductoServicio implements ProductoIService{
+    getProductos(): ProductoDTO[] {
+        throw new Error("Method not implemented.");
+    }
+    getProductoById(): ProductoDTO {
+        throw new Error("Method not implemented.");
+    }
+    agregarProducto(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProdcto(): Boolean {
+        throw new Error("Method not implemented.");
+    }
     
 }
