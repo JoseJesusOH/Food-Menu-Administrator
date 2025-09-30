105edc5faa874dc587051b324477d0df52f56982
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 22:27:49 -0700

    Implementación de interfaz en CategoriaServicio - FMA API Productos
    Se realiza la implementacion correspondiente de la interfaz.

--- API´s Food Menu/API Productos/src/services/implementations/categoria.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/categoria.servicio.impl.ts
@@ -1,3 +1,17 @@
-class CategoriaServicio{
+import { CategoriaDTO } from "@dto/categoria.dto";
+import {CategoriaIServicio} from "@service.dao/categoria.servicio"
+class CategoriaServicio implements CategoriaIServicio{
+    getCategorias(): CategoriaDTO {
+        throw new Error("Method not implemented.");
+    }
+    agregarCategoria(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCategoria(): Boolean {
+        throw new Error("Method not implemented.");
+    }
+    obtenerCategoriaByID(): CategoriaDTO {
+        throw new Error("Method not implemented.");
+    }
     
 }
