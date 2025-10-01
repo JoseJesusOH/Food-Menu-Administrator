91974bf5faa5b02676da0aa554f3c5f87d72ffca
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-01 15:49:46 -0700

    Creacion de Categoria Ruta y Ruta de GetCategorias - FMA API Productos
    Se implementa route para establecer el metodo get para obtener todas las categorias.

--- API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
@@ -1,3 +1,10 @@
-class CategoriaRuta{
-    
-}
+import { CategoriaControl } from "@controller/categoria.control";
+
+const express = require("express");
+
+const router = express.Router();
+const categoriaControl = new CategoriaControl()
+// Home page route.
+router.get("/",categoriaControl.obtenerCategorias);
+
+module.exports = router;
