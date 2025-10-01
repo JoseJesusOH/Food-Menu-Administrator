6cd3b5be288232e5be3d4899581dc509a199afd5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-01 14:04:33 -0700

    Acutalizacion implementacion AlimentoServicio - FMA API Productos
    Se realiza la actualizacion respectiva de los retorno de la implementacion de AlimentoServicio para los retorno y actualizaciones de la interfaz pasada.

--- API´s Food Menu/API Productos/src/services/implementations/alimento.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/alimento.servicio.impl.ts
@@ -2,16 +2,16 @@
 import { AlimentoDTO } from "@dto/alimento.dto";
 import {AlimentoIServicio } from "@service.dao/alimento.servicio"
 class AlimentoServicio implements AlimentoIServicio{
-    obtenerAlimentos(): AlimentoDTO {
+    obtenerAlimentos(): Promise<AlimentoDTO> {
         throw new Error("Method not implemented.");
     }
-    agregarAlimento(alimentoDTO: AlimentoDTO): AlimentoDTO {
+    agregarAlimento(alimentoDTO: AlimentoDTO): Promise<AlimentoDTO> {
         throw new Error("Method not implemented.");
     }
-    eliminar(alimentoId: Number): Boolean {
+    eliminar(alimentoId: Number): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    obtenerAlimentoById(alimentoId: Number): AlimentoDTO {
+    obtenerAlimentoById(alimentoId: Number): Promise<AlimentoDTO> {
         throw new Error("Method not implemented.");
     }
     
