a3eec248988054949d2c8f02ea4947ca3942a930
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-01 14:02:08 -0700

    Actualizacion Interface AlimentoServicio - FMA API Productos
    Se realiza actualizacion de la interfaz con respecto a los retorno para la devolucion de promesas.

--- API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
+++ API´s Food Menu/API Productos/src/services/interfaces/alimento.servicio.ts
@@ -10,25 +10,25 @@ interface AlimentoIServicio{
      * Servicio para obtener todos los alimentos 
      * @returns Alimentos del Sistema 
      */
-   obtenerAlimentos():AlimentoDTO;
+   obtenerAlimentos():Promise<AlimentoDTO>;
    /**
     * Servicio para agregar un alimento
     * @param alimentoDTO Alimento a Registrar
     * @returns True si el alimento fue registrado correctamente , false si este no fue registrado
     */
-   agregarAlimento(alimentoDTO: AlimentoDTO): AlimentoDTO;
+   agregarAlimento(alimentoDTO: AlimentoDTO): Promise<AlimentoDTO>;
     /**
      * Servicio para la eliminación de un alimento
      * @param alimentoId ID Alimento a eliminar
      * @returns True si el alimento fue eliminado, false en caso contrario
      */
-   eliminar(alimentoId: Number): Boolean;
+   eliminar(alimentoId: Number): Promise<Boolean>;
    /**
     * Servicio para obtener un alimento por ID
     * @param alimentoId ID Alimento a Obtener
     * @returns Alimento encontrado, null en caso contrario
     */
-   obtenerAlimentoById(alimentoId:Number):AlimentoDTO;
+   obtenerAlimentoById(alimentoId:Number):Promise<AlimentoDTO>;
 }
 /**
  * Exportacion de la interfaz
