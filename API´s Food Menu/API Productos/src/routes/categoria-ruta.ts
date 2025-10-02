6081d8a065b6e964e1e9d10c3bddd7b0efd1b45c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-02 13:12:45 -0700

    Actualizacion CategoriaRuta - FMA API Productos
    Se realiza la aqwctualizacion de CategoriaRuta, para añadir comentarios respectivos de cada seccion.

--- API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
@@ -1,10 +1,18 @@
+/**
+ * Importacion de Control Respectivo
+ */
 import { CategoriaControl } from "@controller/categoria.control";
 
+//Instancias  
 const express = require("express");
-
 const router = express.Router();
+
+//Categoria Control 
 const categoriaControl = new CategoriaControl()
-// Home page route.
+
+//Manejo de Router
 router.get("/",categoriaControl.obtenerCategorias);
 router.get("/categoria",categoriaControl.obtenerCategorias);
+
+//Exportacion Router
 module.exports = router;
