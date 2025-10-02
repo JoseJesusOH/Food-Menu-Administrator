56892d4a7c8b7288cd86df15e2af683a63c0fdf0
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-02 15:18:17 -0700

    Creacion del DTO Centro Productivo - FMA API Producto
    Se realiza la creacion del DTO para el centro productivo.

---
+++ API´s Food Menu/API Productos/src/dtos/centro-productivo.dto.ts
@@ -0,0 +1,8 @@
+import { CompaniaDTO } from "@dto/compania.dto";
+export class CentroProductivoDTO {
+   centroProductivoUuid: String;
+   nombre: String
+   direccion: String
+   telefono: String
+   compania: CompaniaDTO;
+}
