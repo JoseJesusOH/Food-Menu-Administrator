2e3f4ecbe4f5d5ccfcd3802c5410339adeacb41c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-03 16:28:51 -0700

    Actualizacion Categoria Ruta - FMA API Productos
    Se separan los metodos.

--- API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/categoria-ruta.ts
@@ -12,9 +12,13 @@ const categoriaControl = new CategoriaControl()
 
 //Manejo de Router
 router.get("/",categoriaControl.obtenerCategorias);
+
 router.get("/categoria",categoriaControl.obtenerCategoriaByUuid);
+
 router.post("/",categoriaControl.agregarCategoria);
+
 router.delete("/",categoriaControl.eliminarCategoria)
+
 router.put("/",categoriaControl.actualizarCategoria)
 
 //Exportacion Router
