68b48c345c7f358deb470a0ce8089bebdeb6428b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-03 16:53:59 -0700

    Creacion del modulo de rutas Alimentos - FMA API Productos
    Se realiza la creacion del router correspondiente para el manejo de los servicios a exponer de las entidades de productos.

--- API´s Food Menu/API Productos/src/routes/alimento-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/alimento-ruta.ts
@@ -1,5 +1,17 @@
-import { Router } from "express";
-class AlimentoRouter{
-   
-}
-export {AlimentoRouter}
+/**
+ * Importacion de Control Respectivo
+ */
+
+import { AlimentoControl } from "@controller/alimento.control";
+
+//Instancias  
+const express = require("express");
+const router = express.Router();
+
+const alimentoControl = new AlimentoControl
+
+//Manejo de Router
+router.get("/",alimentoControl.obtenerAlimentos);
+
+//Exportacion Router
+module.exports = router;
