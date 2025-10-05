f294d46600b0c9d6109356df045d3ed89b0f0a37
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-04 22:02:59 -0700

    Actualizacion Alimento Ruta  - FMA API Producto

--- API´s Food Menu/API Productos/src/routes/alimento-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/alimento-ruta.ts
@@ -15,6 +15,7 @@ router.get("/",alimentoControl.obtenerAlimentos);
 router.get("/:id",alimentoControl.obtenerAlimentoById);
 router.delete("/:id",alimentoControl.eliminarAlimento);
 router.post("/",alimentoControl.crearAlimento);
+router.put("/:id",alimentoControl.actualizarAlimento);
 
 //Exportacion Router
 module.exports = router;
