00b02c3e894eca3b5016ab9086e841d2e6791e04
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-05 16:55:17 -0700

    Actualizacion  Producto Ruta - FMA API Productos
    Se realza la creacion de las rtutas correspondientes a los servicios esperados.

--- API´s Food Menu/API Productos/src/routes/producto-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/producto-ruta.ts
@@ -1,3 +1,19 @@
-class ProductoRuta{
-    
-}
+/**
+ * Importacion de Control Respectivo
+ */
+
+import { ProductoControl } from "@controller/producto.control";
+
+//Instancias  
+const express = require("express");
+const router = express.Router();
+
+const productoControl = new ProductoControl()
+//Manejo de Router
+router.get("/",productoControl.obtenerCompanias);
+router.get("/:id",productoControl.obtenerCompaniaById);
+router.delete("/:id",productoControl.eliminarCompania);
+router.post("/",productoControl.crearCompania);
+router.put("/:id",productoControl.actualizarCompania);
+//Exportacion Router
+module.exports = router;
