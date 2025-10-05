006844d338d81f0fc1f7b1ec79dc65f9a50e55c5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-04 22:06:04 -0700

    Actualizacion CompaniaRuta  - FMA API Producto

--- API´s Food Menu/API Productos/src/routes/compania-ruta.ts
+++ API´s Food Menu/API Productos/src/routes/compania-ruta.ts
@@ -11,5 +11,6 @@ const router = express.Router();
 const companiaControl = new CompaniaControl()
 //Manejo de Router
 router.get("/",companiaControl.obtenerCompanias);
+router.get("/:id",companiaControl.obtenerCompaniaById);
 //Exportacion Router
 module.exports = router;
