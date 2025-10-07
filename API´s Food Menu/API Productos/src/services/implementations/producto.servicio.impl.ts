ba559bf0de46f449caad8cc8b02d3b62ac216fdb
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-06 21:33:26 -0700

    Acutalizacion de metodo obtener Productos - FMA API Productos
    Se realiza la implementacion del metodo para obtener productos en servicio.

--- API´s Food Menu/API Productos/src/services/implementations/producto.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/producto.servicio.impl.ts
@@ -1,8 +1,12 @@
 // Importación del Data Transfer Object (DTO) para la entidad Producto
+import { ProductoIDAO } from "@data.dao/producto.dao";
+import { ProductoDAO } from "@data.impl/producto.dao.impl";
 import { ProductoDTO } from "@dto/producto.dto";
 
 // Importación de la interfaz de servicio que define los métodos a implementar
 import { ProductoIService } from "@service.dao/producto.servicio";
+import { LoggerAPI } from "@utility/logger";
+import { instanceToInstance } from "class-transformer";
 
 /**
  * Clase que implementa la interfaz ProductoIService.
@@ -12,30 +16,44 @@ import { ProductoIService } from "@service.dao/producto.servicio";
  * Actualmente los métodos lanzan un error indicando que no han sido implementados.
  */
 class ProductoServicio implements ProductoIService {
-    
+    productoDAO : ProductoIDAO = new ProductoDAO();
     /** Obtiene la lista de productos disponibles. */
-    getProductos(): Promise<ProductoDTO[]> {
-        throw new Error("Method not implemented.");
+    async getProductos(): Promise<ProductoDTO[]> {
+        LoggerAPI.info("Se inicia el metodo para obtener Productos");
+        try {
+             const productos= await this.productoDAO.getProductos();
+             if(productos.length>0){
+                let productosDTO:ProductoDTO []=[]
+                productosDTO=instanceToInstance(productos);
+                return productos;
+             }else{
+                LoggerAPI.warn("No se han encontrado producto")
+                return [];
+             }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener los productos: " + error);
+            throw error;
+        }
     }
 
     /** Busca un producto por su UUID único. */
     getProductoByUuid(productoUuid: String): Promise<ProductoDTO> {
-        throw new Error("Method not implemented.");
+        
     }
 
     /** Agrega un nuevo producto al sistema. */
     agregarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+        
     }
 
     /** Elimina un producto a partir de su UUID. */
     eliminarProdcto(productoUuid: String): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+       
     }
 
     /** Actualiza la información de un producto existente. */
     actualizarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+        
     }
 }
 
