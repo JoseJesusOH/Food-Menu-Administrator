98cd2556077df67c9f3b34392accb0a7c80a1491
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-07 22:19:41 -0700

    Creacion del metodo eliminar Categoria en CategoriaServicio - FMA API Productos
    Se realiza la creacion del metodo para eliminar la categoria en la implementacion del servicio.

--- API´s Food Menu/API Productos/src/services/implementations/categoria.servicio.impl.ts
+++ API´s Food Menu/API Productos/src/services/implementations/categoria.servicio.impl.ts
@@ -12,7 +12,7 @@ import { CategoriaIServicio } from "@service.dao/categoria.servicio"
 import { CategoriaIDAO } from "@data.dao/categoria.dao";
 import { CategoriaDAO } from "@data.impl/categoria.dao.impl";
 import { LoggerAPI } from "@utility/logger";
-import { instanceToInstance,ClassTransformOptions, plainToInstance } from "class-transformer";
+import { instanceToInstance, ClassTransformOptions, plainToInstance } from "class-transformer";
 import { Categoria } from "@entity/categoria.entity";
 /**
  * Servicio encargado de la lógica de negocio relacionada con Categorías.
@@ -32,8 +32,8 @@ class CategoriaServicio implements CategoriaIServicio {
                 return [];
             } else {
                 LoggerAPI.info(`Se han encontrado un total de ${categorias.length}`)
-                let categoriasDTO=[];
-                categoriasDTO=plainToInstance(CategoriaDTO,categorias)
+                let categoriasDTO = [];
+                categoriasDTO = plainToInstance(CategoriaDTO, categorias)
                 return categoriasDTO;
             }
         } catch (error) {
@@ -45,17 +45,17 @@ class CategoriaServicio implements CategoriaIServicio {
     /**
      * Agrega una nueva categoría al sistema.
      */
-   async agregarCategoria(categoriaDTO: CategoriaDTO): Promise<Boolean> {
+    async agregarCategoria(categoriaDTO: CategoriaDTO): Promise<Boolean> {
         LoggerAPI.info("Se inicia el metodo para agregar una nueva categoria");
         try {
-            let categoria=new Categoria ();
-            categoria=plainToInstance(Categoria,categoriaDTO)
-            let resultado= await this.categoriaDao.agregarCategoria(categoria);
-            if(resultado){
+            let categoria = new Categoria();
+            categoria = plainToInstance(Categoria, categoriaDTO)
+            let resultado = await this.categoriaDao.agregarCategoria(categoria);
+            if (resultado) {
                 LoggerAPI.info(`Se ha agregado una nueva categoria con exito`);
                 return true;
             }
-            else{
+            else {
                 LoggerAPI.warn(`No se ha podido agregar la nueva categoria`);
                 return false;
             }
@@ -68,16 +68,16 @@ class CategoriaServicio implements CategoriaIServicio {
      * Actualiza una  categoría al sistema.
      */
     async actualizarCategoria(categoriaDTO: CategoriaDTO): Promise<Boolean> {
-         LoggerAPI.info("Se inicia el metodo para actualizar una nueva categoria"); 
-          try {
-            let categoria=new Categoria ();
-            categoria=plainToInstance(Categoria,categoriaDTO)
-            let resultado= await this.categoriaDao.actualizarCategoria(categoria);
-            if(resultado){
+        LoggerAPI.info("Se inicia el metodo para actualizar una nueva categoria");
+        try {
+            let categoria = new Categoria();
+            categoria = plainToInstance(Categoria, categoriaDTO)
+            let resultado = await this.categoriaDao.actualizarCategoria(categoria);
+            if (resultado) {
                 LoggerAPI.info(`Se ha actualizado una nueva categoria con exito`);
                 return true;
             }
-            else{
+            else {
                 LoggerAPI.warn(`No se ha podido actualizar la nueva categoria`);
                 return false;
             }
@@ -89,15 +89,34 @@ class CategoriaServicio implements CategoriaIServicio {
     /**
      * Elimina una categoría existente a partir de su UUID.
      */
-    eliminarCategoria(categoriaUuid: String): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+    async eliminarCategoria(categoriaUuid: String): Promise<Boolean> {
+        LoggerAPI.info("Se inicia el metodo para eliminar una nueva categoria");
+        try {
+            let categoria: Categoria = await this.categoriaDao.getCategoriaByUuid(categoriaUuid);
+            if (!categoria) {
+                LoggerAPI.warn(`No se ha encontrado la categoria con el UUID: ${categoriaUuid}`);
+                return false;
+            }
+            let resultado = await this.categoriaDao.eliminarCategoriaById(categoria.categoriaId);
+            if (resultado) {
+                LoggerAPI.info(`Se ha eliminado una nueva categoria con exito`);
+                return true;
+            }
+            else {
+                LoggerAPI.warn(`No se ha podido eliminar la nueva categoria`);
+                return false;
+            }
+        } catch (error) {
+            LoggerAPI.error(`Error al eliminar una nueva categoria: ${error}`);
+            throw error;
+        }
     }
 
     /**
      * Busca y devuelve una categoría específica por su UUID.
      */
-    async getCategoriaByUuid(categoriaUuid:String): Promise<CategoriaDTO> {
-              LoggerAPI.info("Se inicia metodo de servicio para obtener las Categoria")
+    async getCategoriaByUuid(categoriaUuid: String): Promise<CategoriaDTO> {
+        LoggerAPI.info("Se inicia metodo de servicio para obtener las Categoria")
         try {
             const categoria = await this.categoriaDao.getCategoriaByUuid(categoriaUuid)
             if (!categoria) {
@@ -105,8 +124,8 @@ class CategoriaServicio implements CategoriaIServicio {
                 return null;
             } else {
                 LoggerAPI.info(`Se han encontrado una categoria con el UUID  ${categoriaUuid}`)
-                let categoriaDTO =new CategoriaDTO();
-                categoriaDTO=plainToInstance(CategoriaDTO,categoria)
+                let categoriaDTO = new CategoriaDTO();
+                categoriaDTO = plainToInstance(CategoriaDTO, categoria)
                 return categoriaDTO;
             }
         } catch (error) {
@@ -116,4 +135,4 @@ class CategoriaServicio implements CategoriaIServicio {
     }
 }
 
-export {CategoriaServicio}
+export { CategoriaServicio }
