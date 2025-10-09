5bcd7e4f0fae97f1cbc80377a7ee1caa1a23df6f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-08 23:23:35 -0700

    Implementacion del metodo obtener Companias en CompaniaControl - FMA API Producto
    Se realiza la implementacion del metodo para obtener companias,ademas del manejo adecuado de respuestas que se esperan.

--- API´s Food Menu/API Productos/src/controllers/compania.control.ts
+++ API´s Food Menu/API Productos/src/controllers/compania.control.ts
@@ -4,66 +4,78 @@ import { CompaniaServicio } from "@service.impl/compania.servicio.impl"
 import { plainToInstance } from "class-transformer"
 import { LoggerAPI } from "@utility/logger"
 class CompaniaControl {
-  companiaServicio: CompaniaIServicio =new CompaniaServicio()
-    /**
- * Crea una nueva compañía.
- * Normalmente toma los datos de la compañía desde req.body
- * y responde con la compañía creada o un mensaje de éxito.
+  companiaServicio: CompaniaIServicio = new CompaniaServicio()
+  /**
+* Crea una nueva compañía.
+* Normalmente toma los datos de la compañía desde req.body
+* y responde con la compañía creada o un mensaje de éxito.
+*/
+  agregarCompania = async (req, res, next) => {
+    LoggerAPI.info("Se inicia el control respectivo para agregar una compania");
+    try {
+      let compania = plainToInstance(Compania, req.body);
+      const companiaResult = await this.companiaServicio.agregarCompania(compania)
+      if (companiaResult) {
+        return res.status(200).send({ message: `La compania ha sido registrada` })
+      } else {
+        return res.status(404).send({ message: "No se ha ingresado la compania" })
+      }
+    } catch (error) {
+      LoggerAPI.warn(`Se ha presentado un error en agregar la compania  ${error}`)
+      return res.status(500).send({ message: "Error interno del servidor" });
+    }
+  }
+  /**
+ * Elimina una compañía existente.
+ * El id de la compañía se recibe en req.params.id.
  */
-    agregarCompania = async (req, res, next) => {
-      LoggerAPI.info("Se inicia el control respectivo para agregar una compania");
-      try {
-         let compania= plainToInstance(Compania, req.body);
-          const companiaResult = await this.companiaServicio.agregarCompania(compania)
-          if (companiaResult) {
-              return res.status(200).send({ message: `La compania ha sido registrada` })
-          } else {
-              return res.status(404).send({ message: "No se ha ingresado la compania" })
-          }
-      } catch (error) {
-          LoggerAPI.warn(`Se ha presentado un error en agregar la compania  ${error}`)
-          return res.status(500).send({ message: "Error interno del servidor" });
+  eliminarCompania = async (req, res, next) => {
+    LoggerAPI.info("Se inicia el control respectivo para eliminar una compania");
+    try {
+      const { companiaUuid } = req.params;
+      const companiaResult = await this.companiaServicio.eliminarCompania(companiaUuid)
+      if (companiaResult) {
+        return res.status(200).send({ message: `La compania ha sido eliminada` })
+      } else {
+        return res.status(404).send({ message: "No se ha eliminado la compania" })
       }
+    } catch (error) {
+      LoggerAPI.warn(`Se ha presentado un error en eliminar la compania  ${error}`)
+      return res.status(500).send({ message: "Error interno del servidor" });
     }
-    /**
-   * Elimina una compañía existente.
-   * El id de la compañía se recibe en req.params.id.
-   */
-    eliminarCompania = async (req, res, next) => {
-      LoggerAPI.info("Se inicia el control respectivo para eliminar una compania");
-      try {
-          const {companiaUuid} = req.params;
-          const companiaResult = await this.companiaServicio.eliminarCompania(companiaUuid)
-          if (companiaResult) {
-              return res.status(200).send({ message: `La compania ha sido eliminada` })
-          } else {
-              return res.status(404).send({ message: "No se ha eliminado la compania" })
-          } 
-      } catch (error) {
-          LoggerAPI.warn(`Se ha presentado un error en eliminar la compania  ${error}`)
-          return res.status(500).send({ message: "Error interno del servidor" });
+  }
+  /**
+* Obtiene todas las compañías.
+* Suele responder con un array de compañías.
+* Puede aceptar filtros opcionales desde req.query.
+*/
+  obtenerCompanias = async (req, res, next) => {
+    LoggerAPI.info("Se inicia el control respectivo para el retorno de companias");
+    try {
+      const companias = await this.companiaServicio.getCompanias();
+      if (companias) {
+        return res.status(200).send({ companias: companias })
+      } else {
+        return res.status(404).send({ message: "No se han encontrado companias" })
       }
+    } catch (error) {
+      LoggerAPI.warn(`Se ha presentado un error en obtener las companias  ${error}`)
+      return res.status(500).send({ message: "Error interno del servidor" });
     }
-    /**
-  * Obtiene todas las compañías.
-  * Suele responder con un array de compañías.
-  * Puede aceptar filtros opcionales desde req.query.
-  */
-    obtenerCompanias = async (req, res, next) => {
-    }
+  }
 
-    /**
-     * Obtiene una compañía específica por su id.
-     * El id vendría en req.params.id.
-     */
-    obtenerCompaniaById = async (req, res, next) => {
-    }
-    /**
-  * Actualiza una compañía existente.
-  * Suele recibir el id de la compañía en req.params.id
-  * y los nuevos datos en req.body.
-  */
-    actualizarCompania = async (req, res, next) => {
-    }
+  /**
+   * Obtiene una compañía específica por su id.
+   * El id vendría en req.params.id.
+   */
+  obtenerCompaniaById = async (req, res, next) => {
+  }
+  /**
+* Actualiza una compañía existente.
+* Suele recibir el id de la compañía en req.params.id
+* y los nuevos datos en req.body.
+*/
+  actualizarCompania = async (req, res, next) => {
+  }
 }
 export { CompaniaControl }
