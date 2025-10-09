92c98c13792effb6149f69b13f937ec32b8470c5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-08 17:23:11 -0700

    Actualizacion de retorno en interface de CompaniaServicio - FMA API Productos
    Se realiza la actualizacion respectiva para los retorno de boolean a Boolean en los metodos respectivos no actualizados anteriormente.

--- API´s Food Menu/API Productos/src/services/interfaces/compania.servicio.ts
+++ API´s Food Menu/API Productos/src/services/interfaces/compania.servicio.ts
@@ -19,21 +19,21 @@ interface CompaniaIServicio {
    * @param {CompaniaDTO} companiaDTO - Objeto que contiene los datos de la compañía a agregar.
    * @returns {Promise<boolean>} Una promesa que indica si la operación fue exitosa.
    */
-  agregarCompania(companiaDTO: CompaniaDTO): Promise<boolean>;
+  agregarCompania(companiaDTO: CompaniaDTO): Promise<Boolean>;
 
   /**
    * Actualiza los datos de una compañía existente.
    * @param {CompaniaDTO} companiaDTO - Objeto que contiene los datos actualizados de la compañía.
    * @returns {Promise<boolean>} Una promesa que indica si la actualización fue exitosa.
    */
-  actualizarCompania(companiaDTO: CompaniaDTO): Promise<boolean>;
+  actualizarCompania(companiaDTO: CompaniaDTO): Promise<Boolean>;
 
   /**
    * Elimina una compañía por su UUID.
    * @param {string} companiaUuid - Identificador único de la compañía a eliminar.
    * @returns {Promise<boolean>} Una promesa que indica si la eliminación fue exitosa.
    */
-  eliminarCompania(companiaUuid: string): Promise<boolean>;
+  eliminarCompania(companiaUuid: string): Promise<Boolean>;
 
   /**
    * Obtiene los datos de una compañía por su UUID.
