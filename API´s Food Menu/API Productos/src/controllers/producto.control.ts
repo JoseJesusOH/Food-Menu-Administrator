839c998bd69fe56c64a8e034f9490e92cf466d8c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-09 16:23:00 -0700

    Implementacion de metodo agregar producto en ProductoControl - FMA API Producto
    Se realiza la implementacion del metodo par agregar un producto en ProductoControl.

--- API´s Food Menu/API Productos/src/controllers/producto.control.ts
+++ API´s Food Menu/API Productos/src/controllers/producto.control.ts
@@ -1,10 +1,36 @@
+import { ProductoIDAO } from "@data.dao/producto.dao"
+import { ProductoDAO } from "@data.impl/producto.dao.impl"
+import { ProductoIService } from "@service.dao/producto.servicio";
+import { ProductoServicio } from "@service.impl/producto.servicio.impl";
+import { plainToInstance } from "class-transformer";
+import { Logger } from "winston";
+import { Producto } from "@entity/producto.entity";
+import { LoggerAPI } from "@utility/logger";
 class ProductoControl {
+    productoServicio: ProductoIService=new ProductoServicio();
         /**
      * Crea un nuevo producto.
      * Normalmente toma los datos del producto desde req.body
      * y responde con el producto creado o un mensaje de éxito.
      */
     agregarProducto = async (req, res, next) => {
+      Logger.info("Se inicio el proceso de agregar un nuevo prducto en control"); 
+      try {
+         const producto= plainToInstance(Producto,req.body);
+         const result = await this.productoServicio.agregarProducto(producto);
+         if(result){
+            LoggerAPI.info("Se agrego un nuevo producto en control"); 
+            return res.status(201).json({message:"Producto Agregado",producto:result});
+         }
+         else{
+            LoggerAPI.error("No se pudo agregar el producto en control"); 
+            return res.status(400).json({message:"No se pudo agregar el producto"});
+         }
+      } catch (error) {
+         LoggerAPI.error("Error al agregar el producto en control: "+error); 
+         return res.status(500).json({message:"Error al agregar el producto"});
+      }
+
     }
         /**
      * Elimina un producto existente.
