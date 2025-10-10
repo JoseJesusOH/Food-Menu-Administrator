36116aa808cf18b91d36aec77768d2f58a137667
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-09 22:30:56 -0700

    Implementacion de metodo obtener usuario en UsuarioDAO - FMA API Perfiles
    Se realiza la implementacion del metodo para obtener los usuarios de la base de datos.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario.dao.impl.ts
@@ -4,21 +4,38 @@
  */
 import { Usuario } from "@entity/usuario.entity";
 import { UsuarioIDAO } from "@data.dao/usuario.dao";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 /**
  * Implementacion corrrespondiente del acceso a datos de usuario
  */
 export class UsuarioDAO implements UsuarioIDAO {
+
+    usuarioRepositorio = Conexion.getRepository(Usuario);
     /**
      * Metodo para obtener la lista de usuarios del sistema
      */
-    async getUsuarios():  Promise<Usuario[]> {
-        throw new Error("Method not implemented.");
+    async getUsuarios(): Promise<Usuario[]> {
+        LoggerAPI.info("Se inicia el metodo para obtener usuario en UsuarioDAO")
+        try {
+            let usuario = await this.usuarioRepositorio.find();
+            if (usuario.length > 0) {
+                LoggerAPI.info("Se encontraron usuarios en el sistema")
+                return usuario;
+            } else {
+                LoggerAPI.warn("No se encontraron usuarios en el sistema")
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error en la consulta de usuarios en el sistema: " + error)
+            throw error;
+        }
     }
 
     /**
      * Metodo para obtener determinado usuario del sistema por ID
      */
-    async getUsuarioByID(usuarioID: Number): Promise<Usuario> {
+    async getUsuarioById(usuarioId: Number): Promise<Usuario> {
         throw new Error("Method not implemented.");
     }
 
@@ -36,7 +53,7 @@ export class UsuarioDAO implements UsuarioIDAO {
     /**
      * Metodo para eliminar un usuario del sistema por su id respectivo
      */
-    async eliminarUsuarioByID(usuarioID: Number): Promise<Boolean> {
+    async eliminarUsuarioById(usuarioID: Number): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
 
