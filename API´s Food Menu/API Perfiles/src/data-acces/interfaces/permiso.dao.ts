6a063fe271a4b831cbecabdab03eb76ec0a44c7e
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-10 22:31:44 -0700

    Actualizacion General de PermisoDAO Interface GetPermisoByUuid - FMA API Pefiles
    Se escribe el metodo GetPermisoByUuid en la interface derivado del proyecto.

--- API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso.dao.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/interfaces/permiso.dao.ts
@@ -1,42 +1,48 @@
 /**
  * Importacion de la entidad Permiso
  */
-import {Permiso} from "@entity/permiso.entity"
+import { Permiso } from "@entity/permiso.entity"
 /** 
  * Interface que define los metodos del DAO de Permiso
  */
-export interface PermisoIDAO{
-    /**
-     * Metodo para obtener todos los permisos
-     * @return Array de permisos
-     */
-    getPermisos(): Promise<Permiso[]>;
+export interface PermisoIDAO {
+  /**
+   * Metodo para obtener todos los permisos
+   * @return Array de permisos
+   */
+  getPermisos(): Promise<Permiso[]>;
 
-     /**
-      *  Metodo para obtener un permiso por su ID
-      * @param permisoId ID del permiso a buscar
-      * @return Permiso encontrado
-      */
-    getPermisoById(permisoId:Number): Promise<Permiso>;
+  /**
+   *  Metodo para obtener un permiso por su ID
+   * @param permisoId ID del permiso a buscar
+   * @return Permiso encontrado
+   */
+  getPermisoById(permisoId: Number): Promise<Permiso>;
 
-    /**
-     * Metodo para agregar un permiso
-     * @param permiso Permiso a agregar
-     * @return Booleano que indica si se agrego el permiso
-     */
-    agregarPermiso(permiso:Permiso): Promise<Boolean>;
+  /**
+   * Metodo para obtener un permiso por su UUID
+   * @param permisoUuid UUID del permiso a buscar
+   * @return Permiso encontrado
+   */
+  getPermisoByUuid(permisoUuid: String): Promise<Permiso>;
+  /**
+   * Metodo para agregar un permiso
+   * @param permiso Permiso a agregar
+   * @return Booleano que indica si se agrego el permiso
+   */
+  agregarPermiso(permiso: Permiso): Promise<Boolean>;
 
-    /**
-     * Metodo para eliminar un permiso por su ID
-     * @param permisoId ID del permiso a eliminar
-     * @return Booleano que indica si se elimino el permiso
-     */
-    eliminarPermisoById(permisoId:Number): Promise<Boolean>;
-    
-    /**
-     * Metodo para actualizar un permiso
-     * @param permiso Permiso a actualizar
-     * @return Booleano que indica si se actualizo el permiso
-     */
-    actualizarPermiso(permiso:Permiso): Promise<Boolean>;
+  /**
+   * Metodo para eliminar un permiso por su ID
+   * @param permisoId ID del permiso a eliminar
+   * @return Booleano que indica si se elimino el permiso
+   */
+  eliminarPermisoById(permisoId: Number): Promise<Boolean>;
+
+  /**
+   * Metodo para actualizar un permiso
+   * @param permiso Permiso a actualizar
+   * @return Booleano que indica si se actualizo el permiso
+   */
+  actualizarPermiso(permiso: Permiso): Promise<Boolean>;
 }
