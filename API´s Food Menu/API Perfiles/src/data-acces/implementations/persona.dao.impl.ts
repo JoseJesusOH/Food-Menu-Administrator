3f411947cd23f32634c323c03a5222c3a9f90b7f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-10 22:25:47 -0700

    Implementacion del metodo eliminar en PersonaDAO Implementacion - FMA API Perfiles
    Se realiza la implementacion del metodo para eliminar una persona en el sistema, manejando errores.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
@@ -2,13 +2,13 @@
  * Importaciones requeridas para el metodo el modelo Persona e interface PersonaIDAO
  */
 import { Persona } from "@entity/persona.entity";
-import {PersonaIDAO} from "@data.dao/persona.dao"
+import { PersonaIDAO } from "@data.dao/persona.dao"
 import { Conexion } from "@utility/conexion";
 import { LoggerAPI } from "@utility/logger";
 /**
  * Clase correspondiente al acceso a datos de Persona
  */
-export class PersonaDAO implements PersonaIDAO{
+export class PersonaDAO implements PersonaIDAO {
     personaRepositorio = Conexion.getRepository(Persona);
     /**
      * Metodo para obtener todas las personas del sistema
@@ -33,7 +33,7 @@ export class PersonaDAO implements PersonaIDAO{
     /**
      * Metodo para obtener una persona del sistema por id 
      */
-   async  getPersonaById(personaId: Number): Promise<Persona> {
+    async getPersonaById(personaId: Number): Promise<Persona> {
         LoggerAPI.info("Se inicia el metodo para obtener persona por ID en PersonaDAO")
         try {
             let persona = await this.personaRepositorio.findOneBy({ personaId: Number(personaId) });
@@ -68,14 +68,14 @@ export class PersonaDAO implements PersonaIDAO{
         } catch (error) {
             LoggerAPI.error("Error en la consulta de persona por UUID en el sistema: " + error)
             throw error;
-        }   
+        }
     }
     /**
      * Metodo para agregar una persona al sistema
      */
     async agregarPersona(persona: Persona): Promise<Boolean> {
-           LoggerAPI.info("Se inicia el metodo para agregar persona en PersonaDAO")
-        try { 
+        LoggerAPI.info("Se inicia el metodo para agregar persona en PersonaDAO")
+        try {
             let result = await this.personaRepositorio.save(persona);
             if (result) {
                 LoggerAPI.info("Se agrego la persona en el sistema")
@@ -87,15 +87,29 @@ export class PersonaDAO implements PersonaIDAO{
             }
         } catch (error) {
             LoggerAPI.error("Error en el agregado de persona en el sistema: " + error)
-             throw error;
-        }   
-    } 
+            throw error;
+        }
+    }
 
     /**
      * Metodo para eliminar una persona del sistema
      */
-    eliminarPersonaByID(personaID: Number): Boolean {
-        throw new Error("Method not implemented.");
+    async eliminarPersonaById(personaId: Number): Promise<Boolean> {
+        LoggerAPI.info("Se inicia el metodo para eliminar persona por ID en PersonaDAO")
+        try {
+            let result = await this.personaRepositorio.delete({ personaId: Number(personaId) });
+            if (result.affected && result.affected > 0) {
+                LoggerAPI.info("Se elimino la persona en el sistema")
+                return true;
+            }
+            else {
+                LoggerAPI.warn("No se elimino la persona en el sistema")
+                return false;
+            }
+        } catch (error) {
+            LoggerAPI.error("Error en la eliminacion de persona por ID en el sistema: " + error)
+            throw error;
+        }
     }
 
     /**
@@ -104,5 +118,5 @@ export class PersonaDAO implements PersonaIDAO{
     actualizarPersona(persona: Persona): Boolean {
         throw new Error("Method not implemented.");
     }
-    
+
 }
