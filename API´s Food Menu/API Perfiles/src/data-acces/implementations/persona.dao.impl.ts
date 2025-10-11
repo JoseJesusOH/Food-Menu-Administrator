b89a98f7e4ca4a2092a42e96e218f8d80aa5cde7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-10 22:16:29 -0700

    Implementacion del metodo GetPersonas en PersonaDAO Implementacion - FMA API Perfiles
    Se realiza la implementacion del metodo para obtener las personas en la base de datos.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/persona.dao.impl.ts
@@ -3,16 +3,32 @@
  */
 import { Persona } from "@entity/persona.entity";
 import {PersonaIDAO} from "@data.dao/persona.dao"
-
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 /**
  * Clase correspondiente al acceso a datos de Persona
  */
 export class PersonaDAO implements PersonaIDAO{
+    personaRepositorio = Conexion.getRepository(Persona);
     /**
      * Metodo para obtener todas las personas del sistema
      */
-    getPersonas(): Persona[] {
-        throw new Error("Method not implemented.");
+    async getPersonas(): Promise<Persona[]> {
+        LoggerAPI.info("Se inicia el metodo para obtener personas en PersonaDAO")
+        try {
+            let personas = await this.personaRepositorio.find();
+            if (personas.length > 0) {
+                LoggerAPI.info("Se encontraron personas en el sistema")
+                return personas;
+            }
+            else {
+                LoggerAPI.warn("No se encontraron personas en el sistema")
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error en la consulta de personas en el sistema: " + error)
+            throw error;
+        }
     }
     /**
      * Metodo para obtener una persona del sistema por id 
