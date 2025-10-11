9db391f658d2e177042e3f32967b6a380b5663f2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-10 22:46:33 -0700

    Implementacion del metodo GetUsuarioCategorias en UsuarioCategoriaDAO Impl - FMA API Perfiles
    Se realiza la implementacion del metodo para obtener la relacion de usuario - categoria en la API Perfiles.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario-categoria.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/usuario-categoria.dao.impl.ts
@@ -4,16 +4,32 @@
  */
 import { UsuarioCategoria } from "@entity/usuario-categoria.entity";
 import {UsuarioCategoriaIDAO} from "@data.dao/usuario-categoria.dao"
-
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 /**
  * Implementación de UsuarioCateforiaDAO 
  */
 export class UsuarioCategoriaDAO implements UsuarioCategoriaIDAO{
+    usuarioCategoria=Conexion.getRepository(UsuarioCategoria);
     /**
      * Metodo para obtener las relaciones de usuario con respecto a categoria
      */
-    getUsuarioCategorias(): UsuarioCategoria[] {
-        throw new Error("Method not implemented.");
+    async getUsuarioCategorias(): Promise<UsuarioCategoria[]> {
+        LoggerAPI.info("Se inicia el metodo getUsuarioCategorias de la clase UsuarioCategoriaDAO");
+        try {
+            let result = await this.usuarioCategoria.find();
+            if (result.length > 0) {
+                LoggerAPI.info("Se encontraron relaciones de usuario con categoria en el sistema");
+                return result;
+            }
+            else {
+                LoggerAPI.info("No se encontraron relaciones de usuario con categoria en el sistema");
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener las relaciones de usuario con categoria del sistema: " + error);
+            throw error;
+        }
     }
 
     /**
