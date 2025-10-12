0d2f497cd2ce8bbe863709a73b44551ee98a26eb
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-11 20:47:44 -0700

    Implementacion de metodo obtener PermisosCategorias DAO Implementacion - FMA API Perfiles
    Se realiza la implementacion de metodo para obtener permisos categorias.

--- API´s Food Menu/API Perfiles/src/data-acces/implementations/permiso-categoria.dao.impl.ts
+++ API´s Food Menu/API Perfiles/src/data-acces/implementations/permiso-categoria.dao.impl.ts
@@ -3,16 +3,33 @@
  */
 import { PermisoCategoria } from "@entity/permiso-categoria.entity";
 import {PermisoCategoriaIDAO} from "@data.dao/permiso-categoria.dao"
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
 /**
  * Implementacion del acceso a datos de PermisoCategoriaDAO
  */
 export class PermisoCategoriaDAO implements PermisoCategoriaIDAO{
+     permisoCategoriaRepository=Conexion.getRepository(PermisoCategoria);
     /**
      * Metodo para obtener todos las relacionas de permisos con respecto a categoria
      */
-    getPermisoCategorias(): PermisoCategoria[] {
-        throw new Error("Method not implemented.");
+    async getPermisoCategorias(): Promise<PermisoCategoria[]> {
+        LoggerAPI.info("Inicio - getPermisoCategorias - PermisoCategoriaDAO");
+        try {
+            let permisosCategorias: PermisoCategoria[] = await this.permisoCategoriaRepository.find(); 
+            if(permisosCategorias.length === 0){  
+                LoggerAPI.warn("No se encontraron permisos categorias - getPermisoCategorias - PermisoCategoriaDAO");
+             return [];
+            }
+            else{
+                LoggerAPI.info("Fin - getPermisoCategorias - PermisoCategoriaDAO");
+                return permisosCategorias;
+            }
+        } catch (error) {
+            LoggerAPI.error("Error - getPermisoCategorias - PermisoCategoriaDAO", error);
+            throw new Error("Error al obtener los permisos categorias");
+        }
     }
     /**
      * Obtiene las relacion con respecti al id 
