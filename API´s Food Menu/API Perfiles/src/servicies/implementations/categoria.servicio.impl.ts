538d94a89ab43a91339b3aeab0b8ad1cee171744
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-14 11:04:55 -0700

    Implementacion del metodo obtener categorias en CategoriaServicio - FMA API Perfiles
    Implementaicon del metodo para obtencion de las categorias.

--- API´s Food Menu/API Perfiles/src/servicies/implementations/categoria.servicio.impl.ts
+++ API´s Food Menu/API Perfiles/src/servicies/implementations/categoria.servicio.impl.ts
@@ -1,9 +1,26 @@
 import { CategoriaDTO } from "@dto/categoria.dto";
+import { Categoria } from "@entity/categoria.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 import { CategoriaIServicio } from "servicies/interfaces/categoria.servicio";
 
 export class CategoriaServicio implements CategoriaIServicio{
-    getCategorias(): Promise<CategoriaDTO[]> {
-        throw new Error("Method not implemented.");
+    categoriaRepositorio=Conexion.getRepository(Categoria);
+    async getCategorias(): Promise<CategoriaDTO[]> {
+        LoggerAPI.info("Se inicia metodo para obtener categorias en ServicioCategoria")
+        try{
+              let resultado= await this.categoriaRepositorio.find();
+              if(resultado.length>0){
+                LoggerAPI.info(`Se han encontrado un total de ${resultado.length} categorias`)
+                 return resultado;
+              }
+              else{
+                LoggerAPI.warn(`No se han encontrado categorias.`)
+                return []
+              }
+        }catch(error){
+            LoggerAPI.warn(`Se ha producido error al obtener categorias error; ${error}`)
+        }
     }
     getCategoriaById(categoriaId: Number): Promise<CategoriaDTO> {
         throw new Error("Method not implemented.");
