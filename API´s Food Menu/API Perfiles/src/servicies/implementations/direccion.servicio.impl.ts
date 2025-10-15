db571d4784908ec1025eb9697eeafeba3bc009c9
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-14 22:04:12 -0700

    Implementacion de metodo GetDireccionById en DireccionServicio - FMA API Perfiles
    Se realiza la implementacion de metodo, analizan errores y logs respectivos.

--- API´s Food Menu/API Perfiles/src/servicies/implementations/direccion.servicio.impl.ts
+++ API´s Food Menu/API Perfiles/src/servicies/implementations/direccion.servicio.impl.ts
@@ -6,6 +6,7 @@ import { plainToInstance } from "class-transformer";
 import { DireccionDAO } from "@data.impl/direccion.dao.impl";
 
 import { DireccionIDAO } from "@data.dao/direccion.dao";
+import { dir } from "console";
 export class DireccionServicio implements DireccionIServicio {
     direcionDAO: DireccionIDAO = new DireccionDAO
     async getDirecciones(): Promise<DireccionDTO[]> {
@@ -25,8 +26,21 @@ export class DireccionServicio implements DireccionIServicio {
         }
     }
 
-    getDireccionById(direcionId: Number): Promise<DireccionDTO> {
-        throw new Error("Method not implemented.");
+    async getDireccionById(direcionId: Number): Promise<DireccionDTO> {
+        LoggerAPI.info("Se ha iniciado el metodo en DireccionServicio para obtener Direccion por ID")
+        try {
+             let result=await this.direcionDAO.getDireccionById(direcionId)
+             if(result){
+                LoggerAPI.info(`Se ha encontrado la direccion con ID ${direcionId}`)
+                return result;
+             }else{
+                LoggerAPI.info(`No se ha encontrado la direccion con ID ${direcionId}`)
+                return null;
+             }
+        } catch (error) {
+            LoggerAPI.warn(`Se ha producido error en DireccionServicio metodo GetDireccionById error; ${error}`)
+            throw error;
+        }
     }
     getDireccionByUuid(direccionUuid: String): Promise<DireccionDTO> {
         throw new Error("Method not implemented.");
