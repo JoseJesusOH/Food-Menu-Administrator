4f0e2315284bb9e61a750aa56c512ae44364fd1c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-15 23:26:43 -0700

    Correccion de return en interface PersonaServicio - FMA API Perfiles
    Correccion de metodos y el retorno especifico.

--- API´s Food Menu/API Perfiles/src/servicies/interfaces/persona.servicio.ts
+++ API´s Food Menu/API Perfiles/src/servicies/interfaces/persona.servicio.ts
@@ -2,8 +2,8 @@ import { PersonaDTO } from "@dto/persona.dto";
 
 export interface PersonaIServicio{
     getPersonas():Promise<PersonaDTO[]>;
-    getPersonaById(personaId:Number):Promise<Boolean>;
-    getPersonaByUuid(personaUuid:String):Promise<Boolean>;
+    getPersonaById(personaId:Number):Promise<PersonaDTO>;
+    getPersonaByUuid(personaUuid:String):Promise<PersonaDTO>;
     agregarPersona(personaDTO:PersonaDTO):Promise<Boolean>;
     actualizarPersona(personaDTO:PersonaDTO):Promise<Boolean>;
     eliminarPersonaById(personaId:Number):Promise<Boolean>;
