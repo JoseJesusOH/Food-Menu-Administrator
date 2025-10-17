c62715f88b8f77512d9f349f0968d2317808b5d7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-16 22:17:26 -0700

    Implementacion del metodo obtener Direccion en DireccionControl - FMA API Perfiles
    Implementacion de metodos y manejo de errores.

--- API´s Food Menu/API Perfiles/src/controllers/direccion.control.ts
+++ API´s Food Menu/API Perfiles/src/controllers/direccion.control.ts
@@ -22,5 +22,23 @@ class DireccionControl {
             return res.status(500).send({ message: "Error interno del servidor" })
         }
     }
+    
+    obtenerDirecciones = async (req, res, next) => {
+        LoggerAPI.info("Se inicia el control respectivo para obtener las direcciones")
+        try {
+            const direcciones = await this.direccionServicio.getDirecciones()
+
+            if (direcciones && direcciones.length > 0) {
+                return res.status(200).send(direcciones)
+            } else {
+                return res.status(404).send({ message: "No se han encontrado direcciones registradas" })
+            }
+
+        } catch (error) {
+            LoggerAPI.warn(`Se ha presentado un error al obtener las direcciones: ${error}`)
+            return res.status(500).send({ message: "Error interno del servidor" })
+        }
+    }
+
 }
 export default { DireccionControl }
