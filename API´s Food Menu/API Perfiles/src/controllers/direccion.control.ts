96b73303a5cc7bf4677a16b95a372e272e60a9bc
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-16 22:14:43 -0700

    Implementacion de metodo AgregarDirecion en DireccionControl - FMA API Perfiles
    Implementacion del metodo.

---
+++ API´s Food Menu/API Perfiles/src/controllers/direccion.control.ts
@@ -0,0 +1,26 @@
+import { Direccion } from "@entity/direccion.entity"
+import { LoggerAPI } from "@utility/logger"
+import { plainToInstance } from "class-transformer"
+import { DireccionServicio } from "servicies/implementations/direccion.servicio.impl"
+import { DireccionIServicio } from "servicies/interfaces/direccion.servicio"
+
+class DireccionControl {
+    direccionServicio: DireccionIServicio = new DireccionServicio()
+    agregarDireccion = async (req, res, next) => {
+        LoggerAPI.info("Se inicia el control respectivo para agregar una dirección")
+        try {
+            let direccion = plainToInstance(Direccion, req.body)
+            const direccionResult = await this.direccionServicio.agregarDireccion(direccion)
+            if (direccionResult) {
+                return res.status(200).send({ message: `La dirección ha sido registrada` })
+            } else {
+                return res.status(404).send({ message: "No se ha ingresado la dirección" })
+            }
+
+        } catch (error) {
+            LoggerAPI.warn(`Se ha presentado un error en agregar la dirección: ${error}`)
+            return res.status(500).send({ message: "Error interno del servidor" })
+        }
+    }
+}
+export default { DireccionControl }
