3e06f74325eb79cb6f8d01f2256b0472bf4dac6c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-17 22:49:10 -0700

    Implementacion de routas en CategoriaRuta - FMA API Perfiles
    Se realiza la definicion de rutas y exportacion de modulo.

---
+++ API´s Food Menu/API Perfiles/src/routes/categoria.route.ts
@@ -0,0 +1,25 @@
+/**
+ * Importacion de Control Respectivo
+ */
+import { CategoriaControl } from "@controller/categoria.control";
+
+//Instancias  
+const express = require("express");
+const router = express.Router();
+
+//Categoria Control 
+const categoriaControl = new CategoriaControl()
+
+//Manejo de Router
+router.get("/",categoriaControl.obtenerCategorias);
+
+router.get("/categoria",categoriaControl.obtenerCategoriaByUuid);
+
+router.post("/",categoriaControl.agregarCategoria);
+
+router.delete("/",categoriaControl.eliminarCategoria)
+
+router.put("/",categoriaControl.actualizarCategoria)
+
+//Exportacion Router
+module.exports = router;
