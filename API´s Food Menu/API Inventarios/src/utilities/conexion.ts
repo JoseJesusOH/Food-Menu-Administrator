b259d0ba3757b3ac1589ce90dd4b5b3f2b85bb88
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-19 23:00:07 -0700

    Actualizacion de modulo conexion DB - FMA API Inventarios
    Se realiza la actualizacion respectiva y configuracion inicial de la conexion.

---
+++ API´s Food Menu/API Inventarios/src/utilities/conexion.ts
@@ -0,0 +1,21 @@
+
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+const Conexion = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "api_inventarios",
+    synchronize: true,
+    logging: false,
+    entities: [
+        
+    ],
+    subscribers: [],
+    migrations: [],
+})
+
+export {Conexion};
