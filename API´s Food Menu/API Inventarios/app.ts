95c2d4a899c6a1d28f221f2195694407cc7cbd26
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-19 23:03:36 -0700

    Configuracion APP - FMA API Inventarios
    Configuracion de arranque de la APP.

---
+++ API´s Food Menu/API Inventarios/app.ts
@@ -0,0 +1,15 @@
+/**
+ * Importaciones Requeridas Servidor y modulo de Pruebas
+ */
+import { servidorStart } from "./src/servidor";
+import { pruebas } from "./src/pruebas";
+
+/**
+ * Llamada al metodo para iniciar los servidores express
+ */
+servidorStart();
+
+/**
+ * Llamada al metodo para ejecucion de pruebas
+ */
+//pruebas();
