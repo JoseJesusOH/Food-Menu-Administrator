88314db0ee33c9dde944253a94db3da8abc55fc2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-19 23:04:12 -0700

    Configuracion de servidor Express - FMA API Inventarios
    Se realiza la configuraicon inicial del servidor, puerto y routes.

---
+++ API´s Food Menu/API Inventarios/src/servidor.ts
@@ -0,0 +1,47 @@
+/**
+ * Importacion de la conexion
+ */
+import { Conexion } from "@utility/conexion"
+
+/**
+ * Variables requeridas 
+ */
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+
+
+/**
+ * Metodo que inializa el servidor express y manda a llamar la inicializacion 
+ * de la base de datos
+ */
+async function servidorStart() {
+  try {
+    await inicializarBaseDatos();
+    // Iniciar servidor
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (error) {
+  }
+}
+/**
+ * Metodo paras inicializar la base de datos
+ */
+async function inicializarBaseDatos() {
+  try {
+    await Conexion.initialize()
+    console.log("Base de Datos iniciliazada")
+  } catch (error) {
+    console.error("Erorr durante inicializacion DB:", error)
+  }
+}
+
+/**
+ * Exportacion del metodo servidorStart
+ */
+export {servidorStart};
