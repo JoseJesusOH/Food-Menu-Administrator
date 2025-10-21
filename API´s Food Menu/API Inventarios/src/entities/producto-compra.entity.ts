525f2640cedf57072ca75729076f5db03b563ea2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-20 22:05:23 -0700

    Actualizacion entidad ProductoCompra - FMA API Inventarios
    Se realiza la actualizacion respectiva de la entidad para el manejo con TypeORM y Class-Transformer.

--- API´s Food Menu/API Inventarios/src/entities/producto-compra.entity.ts
+++ API´s Food Menu/API Inventarios/src/entities/producto-compra.entity.ts
@@ -5,47 +5,72 @@
 
 import { Compra } from "@entity/compra.entity";
 import { Producto } from "@entity/producto.entity";
+/**
+ * Importaciones TypeORM 
+ */
+import "reflect-metadata"
+import {
+    Entity, Column, PrimaryGeneratedColumn, Generated, OneToOne, OneToMany,
+    JoinColumn
+} from "typeorm"
 
+/**
+ * Expose expe las propiedades para la transformacion en classs transformer mientras
+ * Exclude excluye esos parametros.
+ */
+import { Exclude, Expose, ClassTransformOptions } from "class-transformer";
 /**
  * Representa el detalle de un producto dentro de una compra.
  * 
  * Esta clase vincula un producto con una compra específica, 
  * almacenando la cantidad comprada, el precio base y el total.
  */
+@Entity("producto_compra")
 export class ProductoCompra {
     /**
      * Identificador numérico del registro de producto-compra.
      */
+    @PrimaryGeneratedColumn({ name: "producto_compra_id" })
+    @Exclude()
     productoCompraId: Number;
 
     /**
      * Identificador único universal (UUID) del registro de producto-compra.
      */
+    @Column({ name: "producto_compra_uuid" })
+    @Generated("uuid")
     productoCompraUuid: String;
 
     /**
      * Cantidad de unidades del producto compradas.
      */
+    @Column({ name: "cantidad" })
     cantidad: Number;
 
     /**
      * Monto total correspondiente al producto en la compra.
      */
+    @Column({ name: "total" })
     total: Number;
 
     /**
      * Precio base (unitario) del producto al momento de la compra.
      */
+    @Column({ name: "precio_base" })
     precioBase: Number;
 
     /**
      * Compra a la que pertenece este registro.
      */
+    @OneToOne(() => Compra)
+    @JoinColumn({ name: "compra_id", foreignKeyConstraintName: "producto_compra_compra_IDFK" })
     compra: Compra;
 
     /**
      * Producto asociado a esta compra.
      */
+    @OneToOne(() => Producto)
+    @JoinColumn({ name: "producto_id", foreignKeyConstraintName: "producto_compra_producto_IDFK" })
     producto: Producto;
 
     /**
