c3384c6a12daaf2c72e700a81a54d8775ab37d8a
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-20 22:48:56 -0700

    Implementacion de interface CompraDAO  - FMA API Inventarios
    Implementacion en CompraDAO Implementacion de la interface respectiva.

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/compra.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/compra.dao.impl.ts
@@ -1,3 +1,24 @@
-class CompraDAO{
+import { CompraIDAO } from "@data.dao/compra.dao";
+import { Compra } from "@entity/compra.entity";
+
+class CompraDAO implements CompraIDAO{
+    getCompras(): Promise<Compra[]> {
+        throw new Error("Method not implemented.");
+    }
+    getCompraById(compraId: Number): Promise<Compra> {
+        throw new Error("Method not implemented.");
+    }
+    getCompraByUuid(compraUuid: String): Promise<Compra> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCompraById(compraId: Number): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    actualizarCompra(compra: Compra): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    agregarCompra(compra: Compra): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
     
 }
