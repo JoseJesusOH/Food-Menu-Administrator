35405a5abedf39ac405b87d788bcca6bd6b6066d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-20 22:51:32 -0700

    Implementacion de metodo GetCompras en CompraDAO - FMA API Inventarios
    Se realiza la implementacion de metodo para obtener las compras registradas y manejo de errores.

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/compra.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/compra.dao.impl.ts
@@ -1,9 +1,24 @@
 import { CompraIDAO } from "@data.dao/compra.dao";
 import { Compra } from "@entity/compra.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class CompraDAO implements CompraIDAO{
-    getCompras(): Promise<Compra[]> {
-        throw new Error("Method not implemented.");
+class CompraDAO implements CompraIDAO {
+    compraRepositorio = Conexion.getRepository(Compra)
+    async getCompras(): Promise<Compra[]> {
+        try {
+            let compras = await this.compraRepositorio.find({ relations: ["sucursal"] });
+            if (compras.length > 0) {
+                LoggerAPI.info("Se obtuvieron todas las compras", { total: compras.length });
+                return compras;
+            } else {
+                LoggerAPI.info("No se obtuvieron compras", { total: compras.length });
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener las compras", error);
+            throw error;
+        }
     }
     getCompraById(compraId: Number): Promise<Compra> {
         throw new Error("Method not implemented.");
@@ -20,5 +35,5 @@ class CompraDAO implements CompraIDAO{
     agregarCompra(compra: Compra): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
 }
