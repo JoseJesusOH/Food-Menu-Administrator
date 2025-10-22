e1ec80bdfb76d28cbdb9937143cb9d8d2f1edd0e
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-21 23:30:52 -0700

    Implementacion de metodo GetSucursales en SucursalDAO - FMA API Inventarios

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/sucursal.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/sucursal.dao.impl.ts
@@ -1,9 +1,24 @@
 import { SucursalIDAO } from "@data.dao/sucursal.dao";
 import { Sucursal } from "@entity/sucursal.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class SucursalDAO implements SucursalIDAO{
-    getSucursales(): Promise<Sucursal[]> {
-        throw new Error("Method not implemented.");
+class SucursalDAO implements SucursalIDAO {
+    sucursalRepositorio = Conexion.getRepository(Sucursal);
+    async getSucursales(): Promise<Sucursal[]> {
+        try {
+            const sucursales = await this.sucursalRepositorio.find();
+            if (sucursales.length > 0) {
+                LoggerAPI.info("Se obtuvieron todas las sucursales", { total: sucursales.length });
+                return sucursales;
+            } else {
+                LoggerAPI.info("No se obtuvieron sucursales", { total: sucursales.length });
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener las sucursales", error);
+            throw error;
+        }
     }
     getSucursalById(sucursalId: Number): Promise<Sucursal> {
         throw new Error("Method not implemented.");
@@ -20,5 +35,5 @@ class SucursalDAO implements SucursalIDAO{
     agregarSucursal(sucursal: Sucursal): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
 }
