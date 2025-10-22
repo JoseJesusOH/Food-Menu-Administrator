b43d62397208d1c696821f5475c6f050188a222c
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-21 23:35:58 -0700

    Implementacion de metodos ProductoDAO - FMA API Inventarios

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/producto.dao.impl.ts
@@ -1,3 +1,24 @@
-class ProductoDAO{
+import { ProductoIDAO } from "@data.dao/producto.dao";
+import { Producto } from "@entity/producto.entity";
+
+class ProductoDAO implements ProductoIDAO{
+    getProductos(): Promise<Producto[]> {
+        throw new Error("Method not implemented.");
+    }
+    getProductoById(productoId: Number): Promise<Producto> {
+        throw new Error("Method not implemented.");
+    }
+    getProductoByUuid(productoUuid: String): Promise<Producto> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProductoById(productoId: Number): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    actualizarProducto(producto: Producto): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    agregarProducto(producto: Producto): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
     
 }
