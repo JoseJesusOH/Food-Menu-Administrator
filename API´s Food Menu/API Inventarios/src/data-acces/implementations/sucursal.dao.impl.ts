04a41f2ae813804cf29bdbd39225edeb78fcdc05
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-21 23:29:25 -0700

    Implementacion de metodos SucursalDAO - FMA API Inventarios

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/sucursal.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/sucursal.dao.impl.ts
@@ -1,3 +1,24 @@
-class SucursalDAO{
+import { SucursalIDAO } from "@data.dao/sucursal.dao";
+import { Sucursal } from "@entity/sucursal.entity";
+
+class SucursalDAO implements SucursalIDAO{
+    getSucursales(): Promise<Sucursal[]> {
+        throw new Error("Method not implemented.");
+    }
+    getSucursalById(sucursalId: Number): Promise<Sucursal> {
+        throw new Error("Method not implemented.");
+    }
+    getSucursalByUuid(sucursalUuid: String): Promise<Sucursal> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarSucursalById(sucursalId: Number): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    actualizarSucursal(sucursal: Sucursal): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    agregarSucursal(sucursal: Sucursal): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
     
 }
