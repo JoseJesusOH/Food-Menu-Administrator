2009fad83b0e1a5ed9beb22df1ac6692e8a44cc5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-22 22:10:49 -0700

    Implementacion de metodo GetProductosStock en ProductoStockDAO - FMA API Inventarios
    Implementacion de metodos GetProductoStock.

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/producto-stock.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/producto-stock.dao.impl.ts
@@ -1,10 +1,26 @@
 import { ProductoStockIDAO } from "@data.dao/producto-stock.dao";
 import { ProductoStock } from "@entity/producto-stock.entity";
 import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class ProductoStockDAO implements ProductoStockIDAO{
-    getProductosStock(): Promise<ProductoStock[]> {
-        throw new Error("Method not implemented.");
+class ProductoStockDAO implements ProductoStockIDAO {
+    productoStockRepositorio = Conexion.getRepository(ProductoStock);
+
+    async getProductosStock(): Promise<ProductoStock[]> {
+        try {
+            const productosStock = await this.productoStockRepositorio.find({ relations: ["producto", "sucursal"] });
+
+            if (productosStock.length > 0) {
+                LoggerAPI.info("Se obtuvieron todos los productos con su stock", { total: productosStock.length });
+                return productosStock;
+            } else {
+                LoggerAPI.info("No se encontraron productos con stock", { total: productosStock.length });
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener los productos con stock", error);
+            throw error;
+        }
     }
     getProductoStockById(productoStockId: Number): Promise<ProductoStock> {
         throw new Error("Method not implemented.");
