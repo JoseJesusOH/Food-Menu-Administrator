fa5e9c1a25333455b1597e06ab58c61c8d945835
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-22 22:31:08 -0700

    Implementacion de metodo GetProductoCompras en ProductoCompraDAO - FMA API Inventarios
    Se realiza implementacion de metodo y validacion de errores respectivos.

--- API´s Food Menu/API Inventarios/src/data-acces/implementations/producto-compra.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/data-acces/implementations/producto-compra.dao.impl.ts
@@ -1,9 +1,25 @@
 import { ProductoCompraIDAO } from "@data.dao/producto-compra.dao";
 import { ProductoCompra } from "@entity/producto-compra.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class ProductoCompraDAO implements ProductoCompraIDAO{
-    getProductosCompra(): Promise<ProductoCompra[]> {
-        throw new Error("Method not implemented.");
+class ProductoCompraDAO implements ProductoCompraIDAO {
+    productoCompraRepositorio = Conexion.getRepository(ProductoCompra)
+    async getProductosCompra(): Promise<ProductoCompra[]> {
+        try {
+            const productosCompra = await this.productoCompraRepositorio.find({ relations: ["producto", "compra"] });
+
+            if (productosCompra.length > 0) {
+                LoggerAPI.info("Se obtuvieron todos los productos de compra", { total: productosCompra.length });
+                return productosCompra;
+            } else {
+                LoggerAPI.info("No se encontraron productos de compra", { total: productosCompra.length });
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error("Error al obtener los productos de compra", error);
+            throw error;
+        }
     }
     getProductoCompraById(productoCompraId: Number): Promise<ProductoCompra> {
         throw new Error("Method not implemented.");
@@ -20,5 +36,5 @@ class ProductoCompraDAO implements ProductoCompraIDAO{
     agregarProductoCompra(productoCompra: ProductoCompra): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
 }
