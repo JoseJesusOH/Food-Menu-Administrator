9fdf4db4ff85672ca03be9bc6d6c4c1683e4f895
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-23 21:29:41 -0700

    Implementacion de metodo GetCompras en CompraService - FMA API Inventarios

--- API´s Food Menu/API Inventarios/src/services/implementations/compra.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/services/implementations/compra.dao.impl.ts
@@ -1,9 +1,28 @@
+import { CompraIDAO } from "@data.dao/compra.dao";
+import { CompraDAO } from "@data.impl/compra.dao.impl";
 import { CompraDTO } from "@dto/compra.dto";
+import { Compra } from "@entity/compra.entity";
 import { CompraIServicio } from "@service.dao/compra.dao";
+import { LoggerAPI } from "@utility/logger";
+import { instanceToInstance, instanceToPlain, plainToInstance } from "class-transformer";
 
-class CompraServicio implements CompraIServicio{
-    getCompras(): Promise<CompraDTO[]> {
-        throw new Error("Method not implemented.");
+class CompraServicio implements CompraIServicio {
+    compraDAO: CompraIDAO = new CompraDAO();
+    async getCompras(): Promise<CompraDTO[]> {
+        LoggerAPI.info("Se inicia servicio para obtener las compras");
+        try {
+            const compras = await this.compraDAO.getCompras();
+            if (compras.length > 0) {
+                const comprasDTO = plainToInstance(CompraDTO, compras);
+                return comprasDTO;
+            } else {
+                LoggerAPI.warn("No se han encontrado compras en el sistema.");
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.warn(`Se produjo un error al obtener las compras: ${error}`);
+            throw error;
+        }
     }
     agregarCompra(compra: CompraDTO): Promise<Boolean> {
         throw new Error("Method not implemented.");
