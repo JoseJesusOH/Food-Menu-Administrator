b72815508745b49a494ae6012d0cb5e0574620ae
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-23 21:27:50 -0700

    Implementacion de metodos interface en CompraService - FMA API Inventarios

--- API´s Food Menu/API Inventarios/src/services/implementations/compra.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/services/implementations/compra.dao.impl.ts
@@ -1,48 +1,21 @@
-/**
- * Importación del DTO de Compra.
- * Representa la estructura de datos que se transfiere entre capas.
- */
 import { CompraDTO } from "@dto/compra.dto";
+import { CompraIServicio } from "@service.dao/compra.dao";
 
-/**
- * Interfaz que define los métodos del servicio de Compras.
- * Todos los servicios que implementen esta interfaz deben cumplir con estas operaciones.
- */
-interface CompraIServicio {
-
-    /**
-     * Obtiene todas las compras registradas en el sistema.
-     * @return Arreglo de objetos CompraDTO que representan todas las compras.
-     */
-    getCompras(): Promise<CompraDTO[]>;
-
-    /**
-     * Registra una nueva compra a partir de los datos proporcionados.
-     * @param compra Datos de la compra a registrar.
-     * @return Boolean indicando si la operación fue exitosa o no.
-     */
-    agregarCompra(compra: CompraDTO): Promise<Boolean>;
-
-    /**
-     * Actualiza una compra existente con los datos proporcionados.
-     * @param compra Datos de la compra a actualizar.
-     * @return Boolean indicando si la operación fue exitosa o no.
-     */
-    actualizarCompra(compra: CompraDTO): Promise<Boolean>;
-
-    /**
-     * Elimina una compra utilizando su identificador único (UUID).
-     * @param compraUuid Identificador único de la compra.
-     * @return Boolean indicando si la operación de eliminación fue exitosa o no.
-     */
-    eliminarCompra(compraUuid: string): Promise<Boolean>;
-
-    /**
-     * Obtiene una compra específica a partir de su UUID.
-     * @param compraUuid Identificador único de la compra.
-     * @return Objeto CompraDTO correspondiente a la compra buscada.
-     */
-    getCompraByUuid(compraUuid: string): Promise<CompraDTO>;
+class CompraServicio implements CompraIServicio{
+    getCompras(): Promise<CompraDTO[]> {
+        throw new Error("Method not implemented.");
+    }
+    agregarCompra(compra: CompraDTO): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    actualizarCompra(compra: CompraDTO): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarCompra(compraUuid: string): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    getCompraByUuid(compraUuid: string): Promise<CompraDTO> {
+        throw new Error("Method not implemented.");
+    }
+    
 }
-
-export { CompraIServicio };
