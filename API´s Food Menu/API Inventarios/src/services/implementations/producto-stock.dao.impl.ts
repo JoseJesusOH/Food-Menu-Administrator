79f46cfa15fa2b2f5dc6be9e9cff3e77ef25f32b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-24 22:07:44 -0700

    Implementacion de metodo AgregarProductoStock en ProductoStockService - FMA API Inventarios
    Implementacion de metodo agregar, analisis errores, y logs respectivo.

--- API´s Food Menu/API Inventarios/src/services/implementations/producto-stock.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/services/implementations/producto-stock.dao.impl.ts
@@ -1,6 +1,7 @@
 import { ProductoStockIDAO } from "@data.dao/producto-stock.dao";
 import { ProductoStockDAO } from "@data.impl/producto-stock.dao.impl";
 import { ProductoDTO } from "@dto/producto.dto";
+import { ProductoStock } from "@entity/producto-stock.entity";
 import { ProductoIServicio } from "@service.dao/producto.dao";
 import { LoggerAPI } from "@utility/logger";
 import { plainToInstance } from "class-transformer";
@@ -26,18 +27,37 @@ class ProductoStockService implements ProductoIServicio {
         }
     }
 
-    agregarProducto(producto: ProductoDTO): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+    async agregarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
+        LoggerAPI.info("Se inicia servicio para registrar un nuevo producto en el sistema.");
+        try {
+            // Convertimos el DTO a una entidad de Producto
+            const productoStock = plainToInstance(ProductoStock, productoDTO);
+
+            // Llamamos al DAO para realizar el registro
+            const productoCreado = await this.productoStockDAO.agregarProductoStock(productoStock);
+
+            if (productoCreado) {
+                LoggerAPI.info("El producto fue registrado correctamente en el sistema.");
+                return true;
+            } else {
+                LoggerAPI.warn("No se pudo registrar el producto en el sistema.");
+                return false;
+            }
+        } catch (error) {
+            LoggerAPI.error(`Error al agregar un nuevo producto: ${error}`);
+            throw error;
+        }
     }
+
 
     actualizarProducto(producto: ProductoDTO): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
     eliminarProducto(productoUuid: string): Promise<Boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
     getProductoByUuid(productoUuid: string): Promise<ProductoDTO> {
         throw new Error("Method not implemented.");
     }
