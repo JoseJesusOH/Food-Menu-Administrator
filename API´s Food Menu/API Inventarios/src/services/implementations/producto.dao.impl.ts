538f0cb937708dd4c770a51c6c1925f07c24c31d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-24 22:24:47 -0700

    Implementacion de metodo GetProductos en ProductoService - FMA API Inventarios
    Implementacion de metodo y logs respectivos.

--- API´s Food Menu/API Inventarios/src/services/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Inventarios/src/services/implementations/producto.dao.impl.ts
@@ -1,9 +1,31 @@
+import { ProductoIDAO } from "@data.dao/producto.dao";
+import { ProductoDAO } from "@data.impl/producto.dao.impl";
 import { ProductoDTO } from "@dto/producto.dto";
 import { ProductoIServicio } from "@service.dao/producto.dao";
+import { LoggerAPI } from "@utility/logger";
+import { plainToInstance } from "class-transformer";
 
 class ProductoService implements ProductoIServicio{
-    getProductos(): Promise<ProductoDTO[]> {
-        throw new Error("Method not implemented.");
+    productoDAO: ProductoIDAO = new ProductoDAO();
+async getProductos(): Promise<ProductoDTO[]> {
+        LoggerAPI.info("Se inicia servicio para obtener los productos registrados en el sistema.");
+        try {
+            // Se obtiene la lista de productos desde el DAO
+            const productos = await this.productoDAO.getProductos();
+
+            if (productos.length > 0) {
+                // Se transforman las entidades en objetos DTO
+                const productosDTO = plainToInstance(ProductoDTO, productos);
+                LoggerAPI.info(`Se han obtenido ${productosDTO.length} productos del sistema.`);
+                return productosDTO;
+            } else {
+                LoggerAPI.warn("No se encontraron productos registrados en el sistema.");
+                return [];
+            }
+        } catch (error) {
+            LoggerAPI.error(`Error al obtener los productos: ${error}`);
+            throw error;
+        }
     }
     agregarProducto(producto: ProductoDTO): Promise<Boolean> {
         throw new Error("Method not implemented.");
