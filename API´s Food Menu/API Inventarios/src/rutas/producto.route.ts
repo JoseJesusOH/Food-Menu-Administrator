b5dd34318637e2fd904b3b270d48a25b2c5ca6e1
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-26 21:03:16 -0700

    Implementacion de Ruta ObtenerProductos en ProductoRoute - FMA API Inventarios
    Imlementacion de ruta.

---
+++ API´s Food Menu/API Inventarios/src/rutas/producto.route.ts
@@ -0,0 +1,20 @@
+/**
+ * Importacion de Control Respectivo
+ */
+
+import { ProductoControl } from "@controller/producto.control";
+
+
+
+//Instancias  
+const express = require("express");
+const router = express.Router();
+
+const productoControl = new ProductoControl
+
+//Manejo de Router
+router.get("/",productoControl.obtenerProductos)
+
+//Exportacion Router
+module.exports = router;
+
