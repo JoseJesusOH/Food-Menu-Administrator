9abbcb747399754a3818f4493ad24b5188a83227
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-27 23:47:14 -0700

    Acrtualizacion de entidad Usuario - FMA API Ventas
    Actualizacion de entidad, atributos y metodos.

--- API´s Food Menu/API Ventas/entities/usuario.entity.ts
+++ API´s Food Menu/API Ventas/entities/usuario.entity.ts
@@ -1,5 +1,38 @@
-export class Usuario {
-    private usuarioID: Number;
-    private usuarioUUID: String;
-    private nombre: String;
+ class Usuario {
+     usuarioID: Number;
+     usuarioUUID: String;
+     nombre: String;
+
+    
+    /** Obtiene el ID del usuario. */
+    getUsuarioID(): number {
+        return this.usuarioID;
+    }
+
+    /** Establece el ID del usuario. */
+    setUsuarioID(value: number): void {
+        this.usuarioID = value;
+    }
+
+    /** Obtiene el UUID del usuario. */
+    getUsuarioUUID(): string {
+        return this.usuarioUUID;
+    }
+
+    /** Establece el UUID del usuario. */
+    setUsuarioUUID(value: string): void {
+        this.usuarioUUID = value;
+    }
+
+    /** Obtiene el nombre del usuario. */
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    /** Establece el nombre del usuario. */
+    setNombre(value: string): void {
+        this.nombre = value;
+    }
 }
+
+export {Usuario}
