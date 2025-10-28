6b1c1dd4a17c280298d05a44f1601e7ddbc861d5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-27 23:46:44 -0700

    Actualizacion de entidad Producto - FMA API Ventas
    Actualizacion de entidad, atributos y metodos.

--- API´s Food Menu/API Ventas/entities/producto.entity.ts
+++ API´s Food Menu/API Ventas/entities/producto.entity.ts
@@ -1,6 +1,48 @@
-export class Producto{
-    private productoID: Number;
-    private productoUUID: String;
-    private nombre:String;
-    private precio: Number;
+class Producto{
+     productoID: Number;
+     productoUUID: String;
+     nombre:String;
+     precio: Number;
+
+       /** Obtiene el ID del producto. */
+    getProductoID(): number {
+        return this.productoID;
+    }
+
+    /** Establece el ID del producto. */
+    setProductoID(value: number): void {
+        this.productoID = value;
+    }
+
+    /** Obtiene el UUID del producto. */
+    getProductoUUID(): string {
+        return this.productoUUID;
+    }
+
+    /** Establece el UUID del producto. */
+    setProductoUUID(value: string): void {
+        this.productoUUID = value;
+    }
+
+    /** Obtiene el nombre del producto. */
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    /** Establece el nombre del producto. */
+    setNombre(value: string): void {
+        this.nombre = value;
+    }
+
+    /** Obtiene el precio del producto. */
+    getPrecio(): number {
+        return this.precio;
+    }
+
+    /** Establece el precio del producto. */
+    setPrecio(value: number): void {
+        this.precio = value;
+    }
 }
+
+export {Producto}
