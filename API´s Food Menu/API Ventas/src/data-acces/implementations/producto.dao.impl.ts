6d30dc471879ecbad7f0fa5794fda6175a7a19a9
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-29 21:06:44 -0700

    Implementacion de metodo GetProductoById en ProductoDAO - FMA API Ventas

--- API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
@@ -3,31 +3,47 @@ import { Producto } from "@entity/producto.entity";
 import { Conexion } from "@utility/conexion";
 import { LoggerAPI } from "@utility/logger";
 
-class ProductoDAO implements ProductoIDAO{
-      productoRepositorio = Conexion.getRepository(Producto);
+class ProductoDAO implements ProductoIDAO {
+    productoRepositorio = Conexion.getRepository(Producto);
 
-async getProductos(): Promise<Producto[]> {
-    LoggerAPI.info("Se inicia la búsqueda de productos en la base de datos.");
+    async getProductos(): Promise<Producto[]> {
+        LoggerAPI.info("Se inicia la búsqueda de productos en la base de datos.");
 
-    try {
-      const productos = await this.productoRepositorio.find();
+        try {
+            const productos = await this.productoRepositorio.find();
 
-      if (!productos || productos.length === 0) {
-        LoggerAPI.warn("No se han encontrado productos en el sistema.");
-        return [];
-      } else {
-        LoggerAPI.info(`Se han obtenido los productos exitosamente. Total: ${productos.length}`);
-        return productos;
-      }
+            if (!productos || productos.length === 0) {
+                LoggerAPI.warn("No se han encontrado productos en el sistema.");
+                return [];
+            } else {
+                LoggerAPI.info(`Se han obtenido los productos exitosamente. Total: ${productos.length}`);
+                return productos;
+            }
 
-    } catch (error) {
-      LoggerAPI.warn(`Error al obtener los productos. Detalle del error: ${error}`);
-      throw error;
+        } catch (error) {
+            LoggerAPI.warn(`Error al obtener los productos. Detalle del error: ${error}`);
+            throw error;
+        }
     }
+async getProductoById(productoId: Number): Promise<Producto> {
+  LoggerAPI.info(`Se inicia la obtención del producto con ID: ${productoId}`);
+
+  try {
+    const producto = await this.productoRepositorio.findOneBy({ productoId: productoId.valueOf() });
+
+    if (producto !== null) {
+      LoggerAPI.info(`Se ha obtenido correctamente el producto con ID ${productoId}`);
+      return producto;
+    } else {
+      LoggerAPI.warn(`No se encontró un producto con el ID ${productoId}`);
+      return null;
+    }
+
+  } catch (error) {
+    LoggerAPI.warn(`Error al buscar el producto por ID. Detalle del error: ${error}`);
+    throw error;
   }
-    getProductoByID(productoID: number): Producto {
-        throw new Error("Method not implemented.");
-    }
+}
     getProductoByUUID(productoUUID: string): Producto {
         throw new Error("Method not implemented.");
     }
@@ -40,5 +56,5 @@ async getProductos(): Promise<Producto[]> {
     eliminarProductoByID(productoID: number): boolean {
         throw new Error("Method not implemented.");
     }
-    
+
 }
