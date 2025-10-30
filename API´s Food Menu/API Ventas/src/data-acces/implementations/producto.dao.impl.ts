f6355e25ef3086f97e41f7915715a47cdd3f5290
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-29 21:02:43 -0700

    Implementaicon metodo GetProductos en ProductoDAO - FMA API Ventas

--- API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
@@ -1,10 +1,30 @@
 import { ProductoIDAO } from "@data.dao/producto.dao";
 import { Producto } from "@entity/producto.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
 class ProductoDAO implements ProductoIDAO{
-    getProductos(): Producto[] {
-        throw new Error("Method not implemented.");
+      productoRepositorio = Conexion.getRepository(Producto);
+
+async getProductos(): Promise<Producto[]> {
+    LoggerAPI.info("Se inicia la búsqueda de productos en la base de datos.");
+
+    try {
+      const productos = await this.productoRepositorio.find();
+
+      if (!productos || productos.length === 0) {
+        LoggerAPI.warn("No se han encontrado productos en el sistema.");
+        return [];
+      } else {
+        LoggerAPI.info(`Se han obtenido los productos exitosamente. Total: ${productos.length}`);
+        return productos;
+      }
+
+    } catch (error) {
+      LoggerAPI.warn(`Error al obtener los productos. Detalle del error: ${error}`);
+      throw error;
     }
+  }
     getProductoByID(productoID: number): Producto {
         throw new Error("Method not implemented.");
     }
