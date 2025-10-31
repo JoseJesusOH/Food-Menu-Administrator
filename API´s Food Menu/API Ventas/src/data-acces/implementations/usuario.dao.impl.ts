15f2070e5df321ac06313113f7bb9b941bfb3dda
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-30 21:57:48 -0700

    Implementacion de metodo GetUsuarios en UsuarioDAO - FMA API Ventas
    Implementacion y validacion de resultados.

--- API´s Food Menu/API Ventas/src/data-acces/implementations/usuario.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/usuario.dao.impl.ts
@@ -1,9 +1,27 @@
 import { UsuarioIDAO } from "@data.dao/usuario.dao";
 import { Usuario } from "@entity/usuario.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
+class UsuarioDAO implements UsuarioIDAO {
+    usuarioRepositorio = Conexion.getRepository(Usuario);
+    async getUsuarios(): Promise<Usuario[]> {
+        LoggerAPI.info("Se inicia la búsqueda de usuarios en la base de datos.");
 
-class UsuarioDAO implements UsuarioIDAO{
-    getUsuarios(): Usuario[] {
-        throw new Error("Method not implemented.");
+        try {
+            const usuarios = await this.usuarioRepositorio.find();
+
+            if (!usuarios || usuarios.length === 0) {
+                LoggerAPI.warn("No se han encontrado usuarios en el sistema.");
+                return [];
+            } else {
+                LoggerAPI.info(`Se han obtenido los usuarios exitosamente. Total: ${usuarios.length}`);
+                return usuarios;
+            }
+
+        } catch (error) {
+            LoggerAPI.warn(`Error al obtener los usuarios. Detalle del error: ${error}`);
+            throw error;
+        }
     }
     getUsuarioByID(usuarioID: number): Usuario {
         throw new Error("Method not implemented.");
@@ -20,5 +38,5 @@ class UsuarioDAO implements UsuarioIDAO{
     eliminarUsuarioByID(usuarioID: number): boolean {
         throw new Error("Method not implemented.");
     }
-    
+
 }
