ada4b9ddc9d33b1c8e71ab22999c8467ee1828b6
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-30 21:40:37 -0700

    Actualizaicon general de formato en ProductoDAO - FMA API Ventas
    Actualizacion de las lineas y formato del codigo respectivo.

--- API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/producto.dao.impl.ts
@@ -4,73 +4,73 @@ import { Conexion } from "@utility/conexion";
 import { LoggerAPI } from "@utility/logger";
 
 class ProductoDAO implements ProductoIDAO {
-    productoRepositorio = Conexion.getRepository(Producto);
+  productoRepositorio = Conexion.getRepository(Producto);
 
-    async getProductos(): Promise<Producto[]> {
-        LoggerAPI.info("Se inicia la búsqueda de productos en la base de datos.");
+  async getProductos(): Promise<Producto[]> {
+    LoggerAPI.info("Se inicia la búsqueda de productos en la base de datos.");
 
-        try {
-            const productos = await this.productoRepositorio.find();
+    try {
+      const productos = await this.productoRepositorio.find();
 
-            if (!productos || productos.length === 0) {
-                LoggerAPI.warn("No se han encontrado productos en el sistema.");
-                return [];
-            } else {
-                LoggerAPI.info(`Se han obtenido los productos exitosamente. Total: ${productos.length}`);
-                return productos;
-            }
+      if (!productos || productos.length === 0) {
+        LoggerAPI.warn("No se han encontrado productos en el sistema.");
+        return [];
+      } else {
+        LoggerAPI.info(`Se han obtenido los productos exitosamente. Total: ${productos.length}`);
+        return productos;
+      }
 
-        } catch (error) {
-            LoggerAPI.warn(`Error al obtener los productos. Detalle del error: ${error}`);
-            throw error;
-        }
+    } catch (error) {
+      LoggerAPI.warn(`Error al obtener los productos. Detalle del error: ${error}`);
+      throw error;
     }
-async getProductoById(productoId: Number): Promise<Producto> {
-  LoggerAPI.info(`Se inicia la obtención del producto con ID: ${productoId}`);
-
-  try {
-    const producto = await this.productoRepositorio.findOneBy({ productoId: productoId.valueOf() });
-
-    if (producto !== null) {
-      LoggerAPI.info(`Se ha obtenido correctamente el producto con ID ${productoId}`);
-      return producto;
-    } else {
-      LoggerAPI.warn(`No se encontró un producto con el ID ${productoId}`);
-      return null;
-    }
-
-  } catch (error) {
-    LoggerAPI.warn(`Error al buscar el producto por ID. Detalle del error: ${error}`);
-    throw error;
   }
-}
-async getProductoByUuid(productoUuid: String): Promise<Producto> {
-  LoggerAPI.info(`Se inicia la obtención del producto con UUID: ${productoUuid}`);
+  async getProductoById(productoId: Number): Promise<Producto> {
+    LoggerAPI.info(`Se inicia la obtención del producto con ID: ${productoId}`);
 
-  try {
-    const producto = await this.productoRepositorio.findOneBy({ productoUuid: productoUuid.toString() });
+    try {
+      const producto = await this.productoRepositorio.findOneBy({ productoId: productoId.valueOf() });
 
-    if (producto !== null) {
-      LoggerAPI.info(`Se ha obtenido correctamente el producto con UUID ${productoUuid}`);
-      return producto;
-    } else {
-      LoggerAPI.warn(`No se encontró un producto con el UUID ${productoUuid}`);
-      return null;
+      if (producto !== null) {
+        LoggerAPI.info(`Se ha obtenido correctamente el producto con ID ${productoId}`);
+        return producto;
+      } else {
+        LoggerAPI.warn(`No se encontró un producto con el ID ${productoId}`);
+        return null;
+      }
+
+    } catch (error) {
+      LoggerAPI.warn(`Error al buscar el producto por ID. Detalle del error: ${error}`);
+      throw error;
     }
-
-  } catch (error) {
-    LoggerAPI.warn(`Error al buscar el producto por UUID. Detalle del error: ${error}`);
-    throw error;
   }
-}
-    agregarProducto(producto: Producto): boolean {
-        throw new Error("Method not implemented.");
+  async getProductoByUuid(productoUuid: String): Promise<Producto> {
+    LoggerAPI.info(`Se inicia la obtención del producto con UUID: ${productoUuid}`);
+
+    try {
+      const producto = await this.productoRepositorio.findOneBy({ productoUuid: productoUuid.toString() });
+
+      if (producto !== null) {
+        LoggerAPI.info(`Se ha obtenido correctamente el producto con UUID ${productoUuid}`);
+        return producto;
+      } else {
+        LoggerAPI.warn(`No se encontró un producto con el UUID ${productoUuid}`);
+        return null;
+      }
+
+    } catch (error) {
+      LoggerAPI.warn(`Error al buscar el producto por UUID. Detalle del error: ${error}`);
+      throw error;
     }
-    actualizarProducto(producto: Producto): boolean {
-        throw new Error("Method not implemented.");
-    }
-    eliminarProductoByID(productoID: number): boolean {
-        throw new Error("Method not implemented.");
-    }
+  }
+  agregarProducto(producto: Producto): boolean {
+    throw new Error("Method not implemented.");
+  }
+  actualizarProducto(producto: Producto): boolean {
+    throw new Error("Method not implemented.");
+  }
+  eliminarProductoByID(productoID: number): boolean {
+    throw new Error("Method not implemented.");
+  }
 
 }
