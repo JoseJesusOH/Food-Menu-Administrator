befbc31ba51d7e31d3dad0fee40884d27f334600
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-31 23:36:24 -0700

    Implementacion parcial de metodo GetVentaUsuarioByIdVenta - FMA API Ventas

--- API´s Food Menu/API Ventas/src/data-acces/implementations/venta-usuario.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/venta-usuario.dao.impl.ts
@@ -1,10 +1,33 @@
 import { VentaUsuarioIDAO } from "@data.dao/venta-usuario.dao";
 import { VentaUsuario } from "@entity/venta-usuario.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class VentaUsuarioDAO implements VentaUsuarioIDAO{
-    getVentasUsuarioByIDVenta(ventaID: number): VentaUsuario[] {
-        throw new Error("Method not implemented.");
+class VentaUsuarioDAO implements VentaUsuarioIDAO {
+    ventaUsuarioRepositorio = Conexion.getRepository(VentaUsuario)
+    async getVentasUsuarioByIdVenta(ventaId: number): Promise<VentaUsuario[]> {
+        /* LoggerAPI.info(`Se inicia la búsqueda de usuarios asociados a la venta con ID: ${ventaId}`);
+ 
+         try {
+             const ventaUsuarios = await this.ventaUsuarioRepositorio.findBy({
+                 ventaId: ventaId,
+             });
+ 
+             if (!ventaUsuarios || ventaUsuarios.length === 0) {
+                 LoggerAPI.warn(`No se encontraron usuarios asociados a la venta con ID ${ventaId}`);
+                 return [];
+             } else {
+                 LoggerAPI.info(`Se obtuvieron correctamente ${ventaUsuarios.length} usuarios asociados a la venta con ID ${ventaId}`);
+                 return ventaUsuarios;
+             }
+ 
+         } catch (error) {
+             LoggerAPI.warn(`Error al obtener los usuarios asociados a la venta con ID ${ventaId}. Detalle del error: ${error}`);
+             throw error;
+         }
+         */
     }
+
     getVentaUsuarioByIDVenta(ventaID: number): VentaUsuario {
         throw new Error("Method not implemented.");
     }
@@ -20,5 +43,5 @@ class VentaUsuarioDAO implements VentaUsuarioIDAO{
     eliminarVentaUsuarioByID(ventaUsuarioID: number): boolean {
         throw new Error("Method not implemented.");
     }
-    
+
 }
