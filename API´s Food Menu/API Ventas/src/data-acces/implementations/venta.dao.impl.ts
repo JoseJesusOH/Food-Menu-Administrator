c9ec291ab303751434735996cb20666ea660194d
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-31 23:47:09 -0700

    Implementacion de metodo GetVentas en VentaDAO - FMA API Ventas

--- API´s Food Menu/API Ventas/src/data-acces/implementations/venta.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/data-acces/implementations/venta.dao.impl.ts
@@ -1,10 +1,31 @@
 import { VentaIDAO } from "@data.dao/venta.dao";
 import { Venta } from "@entity/venta.entity";
+import { Conexion } from "@utility/conexion";
+import { LoggerAPI } from "@utility/logger";
 
-class VentaDAO implements VentaIDAO{
-    getVentas(): Venta[] {
-        throw new Error("Method not implemented.");
+class VentaDAO implements VentaIDAO {
+    ventaRepositorio = Conexion.getRepository(Venta)
+    async getVentas(): Promise<Venta[]> {
+        LoggerAPI.info("Se inicia la búsqueda de todas las ventas en la base de datos");
+
+
+        try {
+            const ventas = await this.ventaRepositorio.find();
+
+            if (!ventas || ventas.length === 0) {
+                LoggerAPI.warn("No se encontraron ventas registradas en el sistema");
+                return [];
+            } else {
+                LoggerAPI.info(`Se obtuvieron correctamente ${ventas.length} ventas`);
+                return ventas;
+            }
+
+        } catch (error) {
+            LoggerAPI.warn(`Error al obtener las ventas. Detalle del error: ${error}`);
+            throw error;
+        }
     }
+
     getVentaByID(ventaID: number): Venta {
         throw new Error("Method not implemented.");
     }
@@ -20,5 +41,5 @@ class VentaDAO implements VentaIDAO{
     eliminarVentaByID(ventaID: number): boolean {
         throw new Error("Method not implemented.");
     }
-    
+
 }
