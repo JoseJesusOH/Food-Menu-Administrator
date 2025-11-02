d17a481d4bf6fa5d0b0ba98d74eab25213daa856
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-01 21:58:03 -0700

    Actualizacion entidad Usuario para uso de TypeORM - FMA API Ventas

--- API´s Food Menu/API Ventas/src/entities/usuario.entity.ts
+++ API´s Food Menu/API Ventas/src/entities/usuario.entity.ts
@@ -1,38 +1,49 @@
- class Usuario {
-     usuarioId: Number;
-     usuarioUuid: String;
-     nombre: String;
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { VentaUsuario } from "@entity/venta-usuario.entity";
 
-    
-    /** Obtiene el ID del usuario. */
-    getUsuarioId(): Number {
-        return this.usuarioId;
-    }
+@Entity("usuario")
+class Usuario {
+  @PrimaryGeneratedColumn({ name: "usuario_id" })
+  usuarioId: Number;
 
-    /** Establece el ID del usuario. */
-    setUsuarioId(value: Number): void {
-        this.usuarioId = value;
-    }
+  @Column({ name: "usuario_uuid", type: "uuid", unique: true })
+  usuarioUuid: String;
 
-    /** Obtiene el UUID del usuario. */
-    getUsuarioUuid(): String {
-        return this.usuarioUuid;
-    }
+  @Column({ name: "nombre", type: "varchar", length: 100 })
+  nombre: String;
 
-    /** Establece el UUID del usuario. */
-    setUsuarioUuid(value: String): void {
-        this.usuarioUuid = value;
-    }
+  @OneToMany(() => VentaUsuario, (ventaUsuario) => ventaUsuario.usuario)
+  ventaUsuarios: VentaUsuario[];
 
-    /** Obtiene el nombre del usuario. */
-    getNombre(): String {
-        return this.nombre;
-    }
+  /** Obtiene el ID del usuario. */
+  getUsuarioId(): Number {
+    return this.usuarioId;
+  }
 
-    /** Establece el nombre del usuario. */
-    setNombre(value: String): void {
-        this.nombre = value;
-    }
+  /** Establece el ID del usuario. */
+  setUsuarioId(value: Number): void {
+    this.usuarioId = value;
+  }
+
+  /** Obtiene el UUID del usuario. */
+  getUsuarioUuid(): String {
+    return this.usuarioUuid;
+  }
+
+  /** Establece el UUID del usuario. */
+  setUsuarioUuid(value: String): void {
+    this.usuarioUuid = value;
+  }
+
+  /** Obtiene el nombre del usuario. */
+  getNombre(): String {
+    return this.nombre;
+  }
+
+  /** Establece el nombre del usuario. */
+  setNombre(value: String): void {
+    this.nombre = value;
+  }
 }
 
-export {Usuario}
+export { Usuario };
