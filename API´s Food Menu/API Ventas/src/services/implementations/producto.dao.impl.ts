dfca33174041a5a9a378c6bcafbbc700aad69789
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-02 21:35:21 -0800

    Creacion de Metodos Interface ProductoService - FMA API Ventas

---
+++ API´s Food Menu/API Ventas/src/services/implementations/producto.dao.impl.ts
@@ -0,0 +1,21 @@
+import { ProductoDTO } from "@dto/producto.dto";
+import { ProductoIService } from "@service.dao/producto.dao";
+
+class ProductoService implements ProductoIService {
+    getProductos(): Promise<ProductoDTO[]> {
+        throw new Error("Method not implemented.");
+    }
+    getProductoByUuid(productoUuid: String): Promise<ProductoDTO> {
+        throw new Error("Method not implemented.");
+    }
+    agregarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarProdcto(productoUuid: String): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+    actualizarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
+        throw new Error("Method not implemented.");
+    }
+
+}
