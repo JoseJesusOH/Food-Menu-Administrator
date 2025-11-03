ae7ba33c41b7ea68d33bdf96d9d57128471c1fc6
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-02 21:40:04 -0800

    Implementacion de metodo GetProductoByUuid en ProductoService - FMA API Ventas

--- API´s Food Menu/API Ventas/src/services/implementations/producto.dao.impl.ts
+++ API´s Food Menu/API Ventas/src/services/implementations/producto.dao.impl.ts
@@ -27,10 +27,23 @@ class ProductoService implements ProductoIService {
             throw error;
         }
     }
-
-
-    getProductoByUuid(productoUuid: String): Promise<ProductoDTO> {
-        throw new Error("Method not implemented.");
+async getProductoByUuid(productoUuid: String): Promise<ProductoDTO> {
+        LoggerAPI.info(`Se inicia servicio para obtener el producto con UUID: ${productoUuid}`);
+        try {
+            const producto = await this.productoDAO.getProductoByUuid(productoUuid);
+            
+            if (producto) {
+                const productoDTO = plainToInstance(ProductoDTO, producto);
+                LoggerAPI.info(`Producto encontrado: ${productoDTO.nombre ?? "sin nombre"}`);
+                return productoDTO;
+            } else {
+                LoggerAPI.warn(`No se encontró un producto con UUID: ${productoUuid}`);
+                return null;
+            }
+        } catch (error) {
+            LoggerAPI.warn(`Error al obtener el producto con UUID ${productoUuid}: ${error}`);
+            throw error;
+        }
     }
     agregarProducto(productoDTO: ProductoDTO): Promise<Boolean> {
         throw new Error("Method not implemented.");
