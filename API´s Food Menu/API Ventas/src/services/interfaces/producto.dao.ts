1a599a11d88d0db9f9202457c096fa02acdd6177
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-02 21:35:50 -0800

    Implementacion de metodos en ProductoService - FMA API Ventas

---
+++ API´s Food Menu/API Ventas/src/services/interfaces/producto.dao.ts
@@ -0,0 +1,11 @@
+import { ProductoDTO } from "@dto/producto.dto"
+
+interface ProductoIService {
+    getProductos(): Promise<ProductoDTO[]>;
+    getProductoByUuid(productoUuid: String): Promise<ProductoDTO>;
+    agregarProducto(productoDTO: ProductoDTO): Promise<Boolean>;
+    eliminarProdcto(productoUuid: String): Promise<Boolean>;
+    actualizarProducto(productoDTO: ProductoDTO): Promise<Boolean>;
+}
+
+export { ProductoIService }
