2509750d4eee4b47baaaa5ea2dca8098c35965a3
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-03 22:45:35 -0800

    Implementacion de metodo AgregarProducto en ProductoControl - FMA API Ventas

---
+++ API´s Food Menu/API Ventas/src/controllers/producto.control.ts
@@ -0,0 +1,32 @@
+import { ProductoIDAO } from "@data.dao/producto.dao"
+import { ProductoDAO } from "@data.impl/producto.dao.impl"
+import { plainToInstance } from "class-transformer";
+import { Logger } from "winston";
+import { Producto } from "@entity/producto.entity";
+import { LoggerAPI } from "@utility/logger";
+import { ProductoIService } from "@service.dao/producto.dao";
+import { ProductoService } from "@service.impl/producto.dao.impl";
+class ProductoControl {
+    productoServicio: ProductoIService=new ProductoService();
+    agregarProducto = async (req, res, next) => {
+      LoggerAPI.info("Se inicio el proceso de agregar un nuevo prducto en control"); 
+      try {
+         const producto= plainToInstance(Producto,req.body);
+         const result = await this.productoServicio.agregarProducto(producto);
+         if(result){
+            LoggerAPI.info("Se agrego un nuevo producto en control"); 
+            return res.status(201).json({message:"Producto Agregado",producto:result});
+         }
+         else{
+            LoggerAPI.error("No se pudo agregar el producto en control"); 
+            return res.status(400).json({message:"No se pudo agregar el producto"});
+         }
+      } catch (error) {
+         LoggerAPI.error("Error al agregar el producto en control: "+error); 
+         return res.status(500).json({message:"Error al agregar el producto"});
+      }
+
+    }
+
+}
+export { ProductoControl }
