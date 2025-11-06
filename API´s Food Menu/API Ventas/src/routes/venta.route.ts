2cd330648fb521f3cc06372fb0550d2ce58d9a66
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-05 23:27:58 -0800

    Implementacion de Route ObtenerVentaByUuid en VentaRoute - FMA API Ventas

--- API´s Food Menu/API Ventas/src/routes/venta.route.ts
+++ API´s Food Menu/API Ventas/src/routes/venta.route.ts
@@ -5,6 +5,7 @@ const express = require("express");
 const router = express.Router();
 const ventaControl = new VentaControl();
 router.get("/", ventaControl.obtenerVentas);
+router.get("/:ventaUuid", ventaControl.obtenerVentaByUuid); 
 
 //Exportacion Router
 module.exports = router;
