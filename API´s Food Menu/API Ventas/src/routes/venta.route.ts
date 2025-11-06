67c64884a8c30bfc6a93c359c082a87d7d00940f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-05 23:29:42 -0800

    Implementacion de Ruta EliminarVenta en VentaRoute - FMA API Ventas

--- API´s Food Menu/API Ventas/src/routes/venta.route.ts
+++ API´s Food Menu/API Ventas/src/routes/venta.route.ts
@@ -7,7 +7,7 @@ const ventaControl = new VentaControl();
 router.get("/", ventaControl.obtenerVentas);
 router.get("/:ventaUuid", ventaControl.obtenerVentaByUuid); 
 router.post("/", ventaControl.agregarVenta);
-router.put("/:ventaUuid", ventaControl.actualizarVenta);
-
+router.put("/:ventaUuid", ventaControl.actualizarVenta)
+router.delete("/:ventaUuid", ventaControl.eliminarVenta);
 //Exportacion Router
 module.exports = router;
