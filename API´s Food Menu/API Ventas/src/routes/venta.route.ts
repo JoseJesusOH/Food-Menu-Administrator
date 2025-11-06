28dda72fe81b3caf1dd469d615dd0b615effb585
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-05 23:29:10 -0800

    implementacion de ruta ActualizarVenta en VentaRoute - FMA API Ventas

--- API´s Food Menu/API Ventas/src/routes/venta.route.ts
+++ API´s Food Menu/API Ventas/src/routes/venta.route.ts
@@ -7,6 +7,7 @@ const ventaControl = new VentaControl();
 router.get("/", ventaControl.obtenerVentas);
 router.get("/:ventaUuid", ventaControl.obtenerVentaByUuid); 
 router.post("/", ventaControl.agregarVenta);
+router.put("/:ventaUuid", ventaControl.actualizarVenta);
 
 //Exportacion Router
 module.exports = router;
