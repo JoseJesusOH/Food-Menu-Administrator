29b95d58b1ca558189b33b422848f2c93b418918
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-11-05 23:24:24 -0800

    Implementacion de Ruta ObtenerVentas en VentaRoute - FMA API Ventas

---
+++ API´s Food Menu/API Ventas/src/routes/venta.route.ts
@@ -0,0 +1,10 @@
+import { VentaControl } from "@controller/venta.control";
+
+
+const express = require("express");
+const router = express.Router();
+const ventaControl = new VentaControl();
+router.get("/", ventaControl.obtenerVentas);
+
+//Exportacion Router
+module.exports = router;
